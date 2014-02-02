8c7cc25509a4a7a69352b75bd90d34ee9463c76f
Darko Fabijan <darko@renderedtext.com>
2014-02-02 14:20:33 +0100

    Fix example router.

--- lib/phoenix/examples/router.ex
+++ lib/phoenix/examples/router.ex
@@ -2,7 +2,7 @@ defmodule Router do
   use Phoenix.Router, port: 4000
 
   get "pages/:page", Phoenix.Controllers.Pages, :show, as: :page
-  get "files/*path", Phoenix.Controllers.Files, :show, as: :ah_this_is_very_long
+  get "files/*path", Phoenix.Controllers.Files, :show, as: :file
   get "profiles/user-:id", Phoenix.Controllers.Users, :show
 
   resources "users", Phoenix.Controllers.Users do
