833c442358525d3c33e112ae41176175007650dd
Chris McCord <chris@chrismccord.com>
2014-02-15 12:01:35 -0500

    Use pattern matching for route validity.

--- lib/phoenix/router/errors.ex
+++ lib/phoenix/router/errors.ex
@@ -1,17 +1,13 @@
 defmodule Phoenix.Router.Errors do
 
+  def ensure_valid_path!(<<"/" <> _rest>>), do: nil
   def ensure_valid_path!(path) do
-    message = """
+    raise ArgumentError, message: """
     Path must start with slash.
     Change path from:
     #{path}
     to
     /#{path}
     """
-
-    unless String.at(path, 0) == "/" do
-      raise(ArgumentError, message: message)
-    end
   end
-
 end
