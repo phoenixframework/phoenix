2b1556cd0e71bce5ebf19dd043a97978ad29edef
Akash Manohar J <akash@akash.im>
2014-04-23 22:36:34 +0530

    Update Elixir version in template mix.exs to '~> 0.13.0'

--- template/mix.exs
+++ template/mix.exs
@@ -4,7 +4,7 @@ defmodule <%= application_module %>.Mixfile do
   def project do
     [ app: :<%= application_name %>,
       version: "0.0.1",
-      elixir: "~> 0.13.0-dev",
+      elixir: "~> 0.13.0",
       deps: deps ]
   end
 
