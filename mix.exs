52f0c716ef3db6aca76d99bb245be7d5d81e4667
Chris McCord <chris@chrismccord.com>
2014-05-06 09:08:41 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Phoenix.Mixfile do
   def project do
     [
       app: :phoenix,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 0.13.0",
       deps: deps(Mix.env),
       package: [
--- template/mix.exs
+++ template/mix.exs
@@ -4,7 +4,7 @@ defmodule <%= application_module %>.Mixfile do
   def project do
     [ app: :<%= application_name %>,
       version: "0.0.1",
-      elixir: "~> 0.13.0",
+      elixir: "~> 0.13.1",
       deps: deps ]
   end
 
@@ -20,9 +20,8 @@ defmodule <%= application_module %>.Mixfile do
   # { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
   defp deps do
     [
-      {:phoenix, "0.2.1"},
+      {:phoenix, "0.2.2"},
       {:cowboy, github: "extend/cowboy", override: true, ref: "05024529679d1d0203b8dcd6e2932cc2a526d370"},
-      {:jazz, github: "meh/jazz"}
     ]
   end
 end
