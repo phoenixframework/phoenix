ee69351a3255b5a19cb20abae89df109d36415b0
Chris McCord <chris@chrismccord.com>
2014-05-25 11:44:52 -0400

    Fix Mix.project deprecations

--- lib/phoenix/project.ex
+++ lib/phoenix/project.ex
@@ -1,7 +1,7 @@
 defmodule Phoenix.Project do
 
   def module_root do
-    project_name = Keyword.get Mix.project, :app
+    project_name = Keyword.get Mix.Project.config, :app
 
     binary_to_atom(Mix.Utils.camelize(to_string(project_name)))
   end
