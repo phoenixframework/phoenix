f09b04e46e13967cf66c6d23453f878741f6573e
Chris McCord <chris@chrismccord.com>
2014-05-29 13:33:20 -0400

    Fix app configuration failing to merge base app config into Env config

--- lib/phoenix/config/app.ex
+++ lib/phoenix/config/app.ex
@@ -1,23 +1,13 @@
 defmodule Phoenix.Config.App do
-  defmacro __using__(_) do
-    quote do
-      @before_compile unquote(__MODULE__)
-      use ExConf.Config, env_var: "MIX_ENV"
-    end
-  end
+  use ExConf.Config, env_var: "MIX_ENV"
 
-  defmacro __before_compile__(_env) do
-    quote do
+  config :router, port: 4000,
+                  ssl: true,
+                  # Full error reports are disabled
+                  consider_all_requests_local: false
 
-      config :router, port: 4000,
-                      ssl: true,
-                      # Full error reports are disabled
-                      consider_all_requests_local: false
+  config :plugs, code_reload: false,
+                 serve_static_assets: true
 
-      config :plugs, code_reload: false,
-                     serve_static_assets: true
-
-      config :logger, level: :error
-    end
-  end
+  config :logger, level: :error
 end
