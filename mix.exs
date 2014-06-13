5551f49416e5731205d1c43f72ce23f846f62e43
Chris McCord <chris@chrismccord.com>
2014-06-13 11:27:02 -0400

    Bump verson to 0.2.7 to fix #108

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Phoenix.Mixfile do
   def project do
     [
       app: :phoenix,
-      version: "0.2.6",
+      version: "0.2.7",
       elixir: "~> 0.14.0",
       deps: deps(Mix.env),
       package: [
