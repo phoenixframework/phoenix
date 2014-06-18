2cb9110b27b23640bac758db02dae5de091dd122
Johnny Winn <dev+nurugger07@hashrocket.com>
2014-06-17 23:07:33 -0400

    Bump Inflex version

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Phoenix.Mixfile do
     [
       {:cowboy, "~> 0.10.0", github: "extend/cowboy", optional: true},
       {:plug, "0.5.0"},
-      {:inflex, "0.2.0"},
+      {:inflex, "0.2.3"},
       {:ex_conf, "0.1.2"},
       {:jazz, "0.1.1"}
     ]
