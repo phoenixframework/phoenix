ec8bad159538a0a8ea8ba8c2a9238e7e4b3cc3ea
Chris McCord <chris@chrismccord.com>
2014-07-04 18:46:19 -0400

    Add Controller plug tests and fix double plugging of :action

--- test/phoenix/controller_test.exs
+++ test/phoenix/controller_test.exs
@@ -23,11 +23,78 @@ defmodule Phoenix.Router.ControllerTest do
     end
   end
 
+  defmodule PlugController do
+    use Phoenix.Controller
+
+    plug :first_plug
+    plug :second_plug
+    plug :authenticate
+
+    def authenticate(conn = %Conn{private: %{phoenix_action: :restricted}}, _) do
+      if conn.params["username"] == "superadmin" do
+        conn
+      else
+        halt!(conn)
+      end
+    end
+    def authenticate(conn, _), do: conn
+
+    def first_plug(conn, _) do
+      stack = conn.assigns[:stack] || []
+      Conn.assign(conn, :stack, stack ++ [:first_plug])
+    end
+
+    def second_plug(conn, _) do
+      stack = conn.assigns[:stack] || []
+      Conn.assign(conn, :stack, stack ++ [:second_plug])
+    end
+
+    def index(conn, _params) do
+      stack = conn.assigns[:stack] || []
+      conn
+      |> Conn.assign(:stack, stack ++ [:action])
+      |> text("")
+    end
+
+    def restricted(conn, _params) do
+      text conn, ""
+    end
+  end
+
+  defmodule ManuallyPluggedActionController do
+    use Phoenix.Controller
+
+    plug :first_plug
+    plug :action
+    plug :second_plug
+
+    def first_plug(conn, _) do
+      stack = conn.assigns[:stack] || []
+      Conn.assign(conn, :stack, stack ++ [:first_plug])
+    end
+
+    def second_plug(conn, _) do
+      stack = conn.assigns[:stack] || []
+      Conn.assign(conn, :stack, stack ++ [:second_plug])
+    end
+
+    def index(conn, _params) do
+      stack = conn.assigns[:stack] || []
+      conn
+      |> Conn.assign(:stack, stack ++ [:action])
+      |> text("")
+    end
+  end
+
+
   defmodule Router do
     use Phoenix.Router
     get "/users/not_found_301", RedirController, :redir_301
     get "/users/not_found_302", RedirController, :redir_302
     get "/atom/:status", AtomStatusController, :atom
+    get "/plugs/standard", PlugController, :index
+    get "/plugs/standard/restricted", PlugController, :restricted
+    get "/plugs/manual", ManuallyPluggedActionController, :index
   end
 
   test "redirect without status performs 302 redirect do url" do
@@ -91,4 +158,25 @@ defmodule Phoenix.Router.ControllerTest do
     conn = %Conn{params: %{}, req_headers: [{"accept", "somethingcrazy/abc"}]}
     assert Controller.response_content_type(conn) == "text/html"
   end
+
+  test "plug chain invokes :action plug last by default" do
+    conn = simulate_request(Router, :get, "plugs/standard")
+    assert conn.status == 200
+    assert conn.assigns[:stack] == [:first_plug, :second_plug, :action]
+  end
+
+  test "plug chain invokes :action in order of explicity plugging" do
+    conn = simulate_request(Router, :get, "plugs/manual")
+    assert conn.status == 200
+    assert conn.assigns[:stack] == [:first_plug, :action, :second_plug]
+  end
+
+  test "halt! stops plug chain" do
+    conn = simulate_request(Router, :get, "plugs/standard/restricted?username=superadmin")
+    assert conn.status == 200
+
+    conn = simulate_request(Router, :get, "plugs/standard/restricted?username=bob")
+    assert conn.status == 400
+  end
+
 end
