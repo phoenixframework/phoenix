4b74e631ab1fb264ff63bbdd838f27888ed1f142
Chris McCord <chris@chrismccord.com>
2014-07-16 13:22:15 -0400

    Point project template at github repo

--- template/mix.exs
+++ template/mix.exs
@@ -23,7 +23,7 @@ defmodule <%= application_module %>.Mixfile do
   # { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
   defp deps do
     [
-      {:phoenix, "0.3.1"},
+      {:phoenix, github: "phoenixframework/phoenix"},
       {:cowboy, "~> 0.10.0", github: "extend/cowboy", optional: true}
     ]
   end
