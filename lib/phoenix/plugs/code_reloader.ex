18e109256857652f23317d96cc6a4d61e7a8447d
Chris McCord <chris@chrismccord.com>
2014-07-16 13:03:21 -0400

    raise if Mix.Task isn't loaded and show helpful instructions. Close #190

--- lib/phoenix/plugs/code_reloader.ex
+++ lib/phoenix/plugs/code_reloader.ex
@@ -3,8 +3,15 @@ defmodule Phoenix.Plugs.CodeReloader do
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    Mix.Task.reenable "compile.elixir"
-    Mix.Task.run "compile.elixir"
+    if Code.ensure_loaded?(Mix.Task) do
+      Mix.Task.reenable "compile.elixir"
+      Mix.Task.run "compile.elixir"
+    else
+      raise """
+      If you want to use the code reload plug in production or inside an escript,
+      add :mix to your list of dependencies or disable code reloading"
+      """
+    end
 
     conn
   end
