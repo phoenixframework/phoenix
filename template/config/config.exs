9f8f873893ff6ac881cdf42f7b0ec2ef1c946ec2
Chris McCord <chris@chrismccord.com>
2014-07-17 21:30:52 -0400

    Fix template config missing commas

--- template/config/config.exs
+++ template/config/config.exs
@@ -4,11 +4,11 @@ config :phoenix, <%= application_module %>.Router,
   port: System.get_env("PORT"),
   ssl: false,
   code_reload: false,
-  cookies: true
+  cookies: true,
   session_key: "_<%= Mix.Utils.underscore(application_module) %>_key",
   session_secret: "<%= session_secret %>"
 
-config :phoenix, :logger
+config :phoenix, :logger,
   level: :error
 
 
--- template/config/dev.exs
+++ template/config/dev.exs
@@ -4,11 +4,11 @@ config :phoenix, <%= application_module %>.Router,
   port: System.get_env("PORT") || 4000,
   ssl: false,
   code_reload: true,
-  cookies: true
+  cookies: true,
   session_key: "_<%= Mix.Utils.underscore(application_module) %>_key",
   session_secret: "<%= session_secret %>"
 
-config :phoenix, :logger
+config :phoenix, :logger,
   level: :debug
 
 
