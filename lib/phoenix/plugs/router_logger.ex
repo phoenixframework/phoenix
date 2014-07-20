0900f0b3443b38b76dc401072ca0d6f75dc425c4
Jason S <jason@rokkincat.com>
2014-07-19 20:49:24 -0500

    Adding timing and date to request

--- lib/phoenix/plugs/router_logger.ex
+++ lib/phoenix/plugs/router_logger.ex
@@ -9,13 +9,44 @@ defmodule Phoenix.Plugs.RouterLogger do
   def init(opts), do: opts
 
   def call(conn, level) do
-    log(conn, level)
-
-    conn
+    before_stamp = :os.timestamp()
+    Plug.Conn.register_before_send(conn, before_send(before_stamp, level))
   end
 
-  defp log(conn, level) when level in [:debug, :info, :error] do
-    IO.puts "#{conn.method}: #{inspect conn.path_info}"
+  defp before_send(before_stamp, level) when level in [:debug, :info, :error] do
+    fn conn ->
+      {_, _, before_micro} = before_stamp
+      {_, _, after_micro} = :os.timestamp()
+      diff = after_micro - before_micro
+      before = localtime_ms(before_stamp) |> format_time
+
+      #if we micro gets too big then use ms
+      resp_time =  if diff > 1000 do
+        "#{div(diff, 1000)}ms"
+      else
+        "#{diff}µs"
+      end
+
+      IO.puts "#{before} resp_time=#{resp_time} status=#{conn.status} #{conn.method}: #{inspect conn.path_info}"
+      conn
+    end
   end
-  defp log(_conn, _), do: nil
+  defp before_send(_, _), do: fn conn -> conn end
+
+  defp localtime_ms() do
+    localtime_ms(:os.timestamp())
+  end
+  #Source https://gist.github.com/dergraf/2216802GOOGLE_PLACES 
+  defp localtime_ms(now = {_, _, micro}) do
+    {date, {hours, minutes, meconds}} = :calendar.now_to_local_time(now)
+    {date, {hours, minutes, meconds, div(micro, 1000) |> rem(1000)}}
+  end
+
+  defp format_time({{yy, mm, dd}, {hh, mi, ss, ms}}) do
+    [pad(yy), ?-, pad(mm), ?-, pad(dd), ?\s, pad(hh), ?:, pad(mi), ?:, pad(ss), ?:, pad(ms)]
+  end
+
+  defp pad(int) when int < 10, do: [?0, Integer.to_string(int)]
+  defp pad(int), do: Integer.to_string(int)
+
 end
