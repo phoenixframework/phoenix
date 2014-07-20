41b7efe4608f6bef3a74ad548e7e6facac89a4a1
Chris McCord <chris@chrismccord.com>
2014-07-20 12:14:26 -0400

    Add tests for assigning Connection layout and stauts

--- test/phoenix/controller/connection_test.exs
+++ test/phoenix/controller/connection_test.exs
@@ -41,4 +41,17 @@ defmodule Phoenix.Router.ConnectionTest do
     )
     assert Connection.response_content_type(conn) == "text/html"
   end
+
+  test "layout/2 assigns the private layout" do
+    conn = Connection.layout(%Conn{}, "print")
+    assert Connection.layout(conn) == "print"
+  end
+
+  test "layout/1 retrieves the layout, defaulting to application" do
+    assert Connection.layout(%Conn{}) == "application"
+  end
+
+  test "status/1 returns the conn.satus" do
+    assert Connection.status(%Conn{}, 404).status == 404
+  end
 end
