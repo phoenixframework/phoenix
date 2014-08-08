1ed64398bcba7b1fb90356dcd5c2c3579a1588b6
Glenn Rempe <glenn@rempe.us>
2014-08-07 20:32:56 -0700

    Fix broken link in generated comments.

--- template/lib/application_name.ex
+++ template/lib/application_name.ex
@@ -1,7 +1,7 @@
 defmodule <%= application_module %> do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/Application.Behaviour.html
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
     <%= application_module %>.Supervisor.start_link
