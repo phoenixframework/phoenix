7722f297460ce7165793fa4608175ab72269293c
Chris McCord <chris@chrismccord.com>
2014-08-20 18:28:34 -0400

    Clean up docs

--- template/config/prod.exs
+++ template/config/prod.exs
@@ -1,15 +1,13 @@
 use Mix.Config
 
-"""
-NOTE: To get SSL working, you will need to set:
-
-    ssl: true,
-    keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-    certfile: System.get_env("SOME_APP_SSL_CERT_PATH"),
-
-Where those two env variables point to a file on disk
-for the key and cert
-"""
+# NOTE: To get SSL working, you will need to set:
+#
+#     ssl: true,
+#     keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
+#     certfile: System.get_env("SOME_APP_SSL_CERT_PATH"),
+#
+# Where those two env variables point to a file on disk
+# for the key and cert
 
 config :phoenix, <%= application_module %>.Router,
   port: System.get_env("PORT"),
