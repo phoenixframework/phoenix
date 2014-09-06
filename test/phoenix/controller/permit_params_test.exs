085822531e9ad80926889a5c7c09a604ded75bc4
Gabriel Jaldon <gjaldon85@gmail.com>
2014-09-07 01:08:43 +0800

    Namespace modules in test

--- test/phoenix/controller/permit_params_test.exs
+++ test/phoenix/controller/permit_params_test.exs
@@ -1,30 +1,32 @@
-defmodule MyApp.PermitParamsController do
-  use Phoenix.Controller
-
-  plug :permit_params, ["first_name", "last_name"]
-
-  def test(conn, params), do: conn
-end
-
-defmodule MyApp.Router do
-  use Phoenix.Router
-
-  post "/permit/test", MyApp.PermitParamsController, :test
-end
-
 defmodule Phoenix.Controller.PermitParamsTest do
   use ExUnit.Case
   use PlugHelper
 
+  defmodule PermitParams.Controller do
+    use Phoenix.Controller
+
+    plug :permit_params, ["first_name", "last_name"]
+
+    def test(conn, _params) do
+      conn
+    end
+  end
+
+  defmodule PermitParams.Router do
+    use Phoenix.Router
+
+    post "/permit/test", PermitParams.Controller, :test
+  end
+
   test "permits all whitelisted params" do
     params = %{first_name: "Foo", last_name: "Bar"}
-    conn = simulate_request(MyApp.Router, :post, "permit/test", params)
+    conn = simulate_request(PermitParams.Router, :post, "permit/test", params)
     assert conn.params == %{"first_name" => "Foo", "last_name" => "Bar"}
   end
 
   test "blocks unpermitted params" do
     params = %{first_name: "Foo", username: "foobar", aka: "baz"}
-    conn = simulate_request(MyApp.Router, :post, "permit/test", params)
+    conn = simulate_request(PermitParams.Router, :post, "permit/test", params)
     assert conn.params == %{"first_name" => "Foo"}
   end
 end
