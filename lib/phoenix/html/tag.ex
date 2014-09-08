6af187b97a4229fc32adf4b0a0dc7959feb923cc
Sonny Scroggin <scrogson@gmail.com>
2014-09-08 12:46:51 -0500

    Ensure that keys are strings

--- lib/phoenix/html/tag.ex
+++ lib/phoenix/html/tag.ex
@@ -37,13 +37,15 @@ defmodule Phoenix.HTML.Tag do
   @doc false
   defp tag_attrs([]), do: ""
   defp tag_attrs(attrs) do
-    Enum.map_join attrs, fn {k,v} -> ~s( #{k}="#{Safe.to_string(v)}") end
+    Enum.map_join attrs, fn {k,v} ->
+      " " <> k <> "=" <> "\"" <> Safe.to_string(v) <> "\""
+    end
   end
 
   @doc false
   defp nested_attrs(attr, dict, acc) do
-      attr_name = :"#{attr}-#{dasherize(k)}"
     Enum.reduce dict, acc, fn {k,v}, acc ->
+      attr_name = "#{attr}-#{dasherize(k)}"
       case is_list(v) do
         true  -> nested_attrs(attr_name, v, acc)
         false -> [{attr_name, v}|acc]
@@ -58,17 +60,17 @@ defmodule Phoenix.HTML.Tag do
   defp build_attrs(_tag, [], acc),
     do: acc |> Enum.sort |> tag_attrs
   defp build_attrs(:form, [{:method,v}|t], acc),
-    do: build_attrs(:form, t, [{:method, v}|acc])
+    do: build_attrs(:form, t, [{"method", v}|acc])
   defp build_attrs(tag, [{k,v}|t], acc) when is_list(v),
     do: build_attrs(tag, t, nested_attrs(k, v, acc))
   defp build_attrs(tag, [{k,v}|t], acc) when k in @data_attrs,
-    do: build_attrs(tag, t, [{:"data-#{k}", v}|acc])
+    do: build_attrs(tag, t, [{"data-#{k}", v}|acc])
   defp build_attrs(tag, [{k,v}|t], acc) when v == true,
-    do: build_attrs(tag, t, [{k,k}|acc])
+    do: build_attrs(tag, t, [{to_string(k),k}|acc])
   defp build_attrs(tag, [{:method,v}|t], acc),
-    do: build_attrs(tag, t, [{:"data-method", v}, {:rel, "nofollow"}|acc])
+    do: build_attrs(tag, t, [{"data-method", v}, {"rel", "nofollow"}|acc])
   defp build_attrs(tag, [{k,v}|t], acc),
-    do: build_attrs(tag, t, [{k,v}|acc])
+    do: build_attrs(tag, t, [{to_string(k),v}|acc])
 
   @doc false
   defp dasherize(value) when is_atom(value), do: dasherize(Atom.to_string(value))
