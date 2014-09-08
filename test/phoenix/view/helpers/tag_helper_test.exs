151ac38ec7a6d9c0de83fb6ef6a54529b97accea
Sonny Scroggin <scrogson@gmail.com>
2014-09-08 03:04:22 -0500

    Use `~s(...)` instead of manually escaping

--- test/phoenix/view/helpers/tag_helper_test.exs
+++ test/phoenix/view/helpers/tag_helper_test.exs
@@ -6,18 +6,18 @@ defmodule Phoenix.View.Helpers.TagHelperTest do
   test "tag" do
     assert tag(:br) == "<br>"
     assert tag(:br, false) == "<br />"
-    assert tag(:input, type: "text", name: "user_id") == "<input name=\"user_id\" type=\"text\">"
-    assert tag(:input, name: "\"<3\"") == "<input name=\"&quot;&lt;3&quot;\">"
-    assert tag(:input, data: [toggle: "dropdown"]) == "<input data-toggle=\"dropdown\">"
-    assert tag(:input, data: [toggle: [target: "#parent"]]) == "<input data-toggle-target=\"#parent\">"
-    assert tag(:audio, autoplay: true) == "<audio autoplay=\"autoplay\">"
+    assert tag(:input, type: "text", name: "user_id") == ~s(<input name="user_id" type="text">)
+    assert tag(:input, name: ~s("<3")) == ~s(<input name="&quot;&lt;3&quot;">)
+    assert tag(:input, data: [toggle: "dropdown"]) == ~s(<input data-toggle="dropdown">)
+    assert tag(:input, data: [toggle: [target: "#parent"]]) == ~s(<input data-toggle-target="#parent">)
+    assert tag(:audio, autoplay: true) == ~s(<audio autoplay="autoplay">)
   end
 
   test "content_tag" do
     assert content_tag(:form, [action: "/users", remote: true], do: tag(:input, name: "user[name]")) ==
-      "<form action=\"/users\" data-remote=\"true\"><input name=\"user[name]\"></form>"
-    assert content_tag(:p, "Hello", class: "dark") == "<p class=\"dark\">Hello</p>"
-    assert content_tag(:p, do: "Hello") == "<p>Hello</p>"
-    assert content_tag(:p, [class: "dark"], do: "Hello") == "<p class=\"dark\">Hello</p>"
+      ~s(<form action="/users" data-remote="true"><input name="user[name]"></form>)
+    assert content_tag(:p, "Hello", class: "dark") == ~s(<p class="dark">Hello</p>)
+    assert content_tag(:p, "Hello") == "<p>Hello</p>"
+    assert content_tag(:p, [class: "dark"], do: "Hello") == ~s(<p class="dark">Hello</p>)
   end
 end
