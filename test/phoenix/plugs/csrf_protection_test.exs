9c276117db502c360ec4a4f1de219ba6c7cfc000
Gabriel Jaldon <gjaldon85@gmail.com>
2014-09-16 23:58:21 +0800

    Assert that conn has not halted

--- test/phoenix/plugs/csrf_protection_test.exs
+++ test/phoenix/plugs/csrf_protection_test.exs
@@ -42,44 +42,62 @@ defmodule Phoenix.Controller.CsrfProtectionTest do
   end
 
   test "unprotected requests are always valid" do
-    simulate_request_without_token(:get, "/") |> CsrfProtection.call([])
-    simulate_request_without_token(:options, "/") |> CsrfProtection.call([])
-    simulate_request_without_token(:connect, "/") |> CsrfProtection.call([])
-    simulate_request_without_token(:trace, "/") |> CsrfProtection.call([])
-    simulate_request_without_token(:head, "/") |> CsrfProtection.call([])
+    conn = simulate_request_without_token(:get, "/") |> CsrfProtection.call([])
+    assert conn.halted  == false
+
+    conn = simulate_request_without_token(:options, "/") |> CsrfProtection.call([])
+    assert conn.halted  == false
+
+    conn = simulate_request_without_token(:connect, "/") |> CsrfProtection.call([])
+    assert conn.halted == false
+
+    conn = simulate_request_without_token(:trace, "/") |> CsrfProtection.call([])
+    assert conn.halted == false
+
+    conn = simulate_request_without_token(:head, "/") |> CsrfProtection.call([])
+    assert conn.halted == false
   end
 
   test "protected requests with valid token in params are allowed except DELETE" do
     old_conn = simulate_request(:get, "/")
     params = %{csrf_token: "hello123"}
 
-    conn(:post, "/", params) |> recycle_data(old_conn) |> CsrfProtection.call([])
-    conn(:put, "/", params) |> recycle_data(old_conn) |> CsrfProtection.call([])
-    conn(:patch, "/", params) |> recycle_data(old_conn) |> CsrfProtection.call([])
+    conn = conn(:post, "/", params) |> recycle_data(old_conn) |> CsrfProtection.call([])
+    assert conn.halted == false
+
+    conn = conn(:put, "/", params) |> recycle_data(old_conn) |> CsrfProtection.call([])
+    assert conn.halted == false
+
+    conn = conn(:patch, "/", params) |> recycle_data(old_conn) |> CsrfProtection.call([])
+    assert conn.halted == false
   end
 
   test "protected requests with valid token in header are allowed" do
     old_conn = simulate_request(:get, "/")
 
-    conn(:post, "/")
+    conn = conn(:post, "/")
     |> recycle_data(old_conn)
     |> put_req_header("X-CSRF-Token", "hello123")
     |> CsrfProtection.call([])
+    assert conn.halted == false
 
-    conn(:put, "/")
+    conn = conn(:put, "/")
     |> recycle_data(old_conn)
     |> put_req_header("X-CSRF-Token", "hello123")
     |> CsrfProtection.call([])
+    assert conn.halted == false
 
-    conn(:patch, "/")
+    conn = conn(:patch, "/")
     |> recycle_data(old_conn)
     |> put_req_header("X-CSRF-Token", "hello123")
     |> CsrfProtection.call([])
+    assert conn.halted == false
 
-    conn(:delete, "/")
+    conn = conn(:delete, "/")
     |> recycle_data(old_conn)
     |> put_req_header("X-CSRF-Token", "hello123")
     |> CsrfProtection.call([])
+    assert conn.halted == false
   end
 
   test "csrf_token is generated when it isn't available" do
