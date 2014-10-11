92037c759347f2d68ae1e754c8d1f55ba780f881
Gabriel Jaldon <gjaldon85@gmail.com>
2014-10-11 16:07:24 +0800

    Use call/4 in flash_test

--- test/phoenix/plugs/controller_logger_test.exs
+++ test/phoenix/plugs/controller_logger_test.exs
@@ -15,8 +15,8 @@ defmodule Phoenix.ControllerLoggerTest do
 
   test "verify logger" do
     {_conn, [_plug_log, header, accept, parameters, _plug_log_2]} = simulate_request_with_logging(Router, :get, "/")
-    assert String.contains?(header, "[debug] Processing by Phoenix.ControllerLoggerTest.LoggerController.index")
-    assert accept == "  Accept: text/html"
-    assert parameters == "  Parameters: %{}"
+    assert header =~ "[debug] Processing by Phoenix.ControllerLoggerTest.LoggerController.index"
+    assert accept =~ "Accept: text/html"
+    assert parameters =~ "Parameters: %{}"
   end
 end
