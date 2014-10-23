ce7a42739b301c03fd7db2ae8b37f7e90de0e35c
Takayuki Matsubara <takayuki.1229@gmail.com>
2014-10-23 22:40:07 +0900

    Add parameter filtering for logging

--- lib/phoenix/plugs/controller_logger.ex
+++ lib/phoenix/plugs/controller_logger.ex
@@ -4,6 +4,16 @@ defmodule Phoenix.Plugs.ControllerLogger do
 
   @moduledoc """
   Plug to handle request logging at the controller level.
+
+
+  ## Configuration
+
+  Filter out sensitive parameters in the logs, such as passwords.
+  The parameters can be added via the `:filter_parameters` option:
+
+      config :phoenix, :filter_parameters, ["password", "secret"]
+
+  Phoenix's default is `["password"]`.
   """
 
   def init(opts), do: opts
@@ -12,10 +22,29 @@ defmodule Phoenix.Plugs.ControllerLogger do
     Logger.debug fn ->
       module = conn |> controller_module |> inspect
       action = conn |> action_name |> Atom.to_string
+      filtered_parameters = conn.params |> filter_parameters
 
       ["Processing by ", module, ?., action, ?/, ?2, ?\n,
-        "  Parameters: ", inspect(conn.params)]
+        "  Parameters: ", inspect(filtered_parameters)]
     end
     conn
   end
+
+  defp filter_parameters(params) do
+    filter_parameters = Application.get_env(:phoenix, :filter_parameters)
+    Enum.into params, %{}, fn {k, v} ->
+      if String.contains?(String.downcase(k), filter_parameters) do
+        {k, "FILTERED"}
+      else
+        {k, filter_values(v)}
+      end
+    end
+  end
+
+  defp filter_values(%{} = map),
+    do: filter_parameters(map)
+  defp filter_values([_|_] = list),
+    do: Enum.map(list, fn (item) -> if is_map(item), do: filter_parameters(item), else: item end)
+  defp filter_values(other),
+    do: other
 end
--- test/phoenix/plugs/controller_logger_test.exs
+++ test/phoenix/plugs/controller_logger_test.exs
@@ -9,11 +9,43 @@ defmodule Phoenix.ControllerLoggerTest do
   end
 
   test "logs controller, action, format and parameters" do
-    {_conn, [header, parameters]} = capture_log fn ->
-      conn = conn(:get, "/", foo: "bar", format: "html") |> fetch_params
-      LoggerController.call(conn, LoggerController.init(:index))
-    end
+    {_conn, [header, parameters]} = call_controller_with_params(foo: "bar", format: "html")
     assert header =~ "[debug] Processing by Phoenix.ControllerLoggerTest.LoggerController.index/2"
     assert parameters =~ "Parameters: %{\"foo\" => \"bar\", \"format\" => \"html\"}"
+  end
+
+  test "filter parameter" do
+    filter_parameters = Application.get_env(:phoenix, :filter_parameters)
+    try do
+      Application.put_env(:phoenix, :filter_parameters, filter_parameters ++ ["secret"])
+      {_conn, [_, parameters]} = call_controller_with_params(
+        password: "should_not_be_show", secret: "should_not_be_show"
+      )
+      assert parameters =~ "Parameters: %{\"password\" => \"FILTERED\", \"secret\" => \"FILTERED\"}"
+    after
+      Application.put_env(:phoenix, :filter_parameters, filter_parameters)
+    end
+  end
+
+  test "filter parameter when a Map has secret key" do
+    {_conn, [_, parameters]} = call_controller_with_params(
+      foo: "bar", map: %{ password: "should_not_be_show" }
+    )
+    assert parameters =~ "Parameters: %{\"foo\" => \"bar\", \"map\" => %{\"password\" => \"FILTERED\"}}"
+  end
+
+  test "filter parameter when a List has a Map" do
+    {_conn, [_, parameters]} = call_controller_with_params(
+      foo: "bar", list: [ %{ password: "should_not_be_show"} ]
+    )
+    assert parameters =~ "Parameters: %{\"foo\" => \"bar\", \"list\" => [%{\"password\" => \"FILTERED\"}]}"
+  end
+
+  defp call_controller_with_params(params) do
+    capture_log fn ->
+      conn = conn(:get, "/", foo: "bar", format: "html") |> fetch_params
+      conn = conn(:get, "/", params) |> fetch_params
+      LoggerController.call(conn, LoggerController.init(:index))
+    end
   end
 end
