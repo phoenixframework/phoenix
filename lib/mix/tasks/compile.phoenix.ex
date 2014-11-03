bfa2631ceb816a88f4f33ee4257ff700ac983445
Chris McCord <chris@chrismccord.com>
2014-11-03 12:54:31 -0500

    Only use code reloader compiler if enabled

--- lib/mix/tasks/compile.phoenix.ex
+++ lib/mix/tasks/compile.phoenix.ex
@@ -8,7 +8,14 @@ defmodule Mix.Tasks.Compile.Phoenix do
 
   def run(_args) do
     Application.ensure_all_started(:phoenix)
+    if Application.get_env(:phoenix, :code_reloader) do
+      reload
+    else
+      :noop
+    end
+  end
 
+  defp reload do
     case Phoenix.CodeReloader.touch do
       [] -> :noop
       _  -> :ok
