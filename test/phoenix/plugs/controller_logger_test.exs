a5065846552613999955193b7cfbf2359551ee2a
Gabriel Jaldon <gjaldon85@gmail.com>
2014-11-27 23:53:12 +0800

    Log pipeline in controller logger

--- test/phoenix/plugs/controller_logger_test.exs
+++ test/phoenix/plugs/controller_logger_test.exs
@@ -9,11 +9,14 @@ defmodule Phoenix.ControllerLoggerTest do
   end
 
   test "logs controller, action, format and parameters" do
-    {_conn, [header, parameters]} = capture_log fn ->
-      conn = conn(:get, "/", foo: "bar", format: "html") |> fetch_params
+    {_conn, [header, parameters, pipeline]} = capture_log fn ->
+      conn = conn(:get, "/", foo: "bar", format: "html")
+             |> fetch_params
+             |> put_private(:phoenix_pipelines, [:browser])
       LoggerController.call(conn, LoggerController.init(:index))
     end
     assert header =~ "[debug] Processing by Phoenix.ControllerLoggerTest.LoggerController.index/2"
     assert parameters =~ "Parameters: %{\"foo\" => \"bar\", \"format\" => \"html\"}"
+    assert pipeline =~  "Pipeline: [:browser]"
   end
 end
