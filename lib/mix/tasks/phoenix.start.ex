c12f8ff7eaedcceb1a670d78e15a4b9b1c00780c
Trent Clarke <trent.clarke@gmail.com>
2014-12-05 23:32:13 +1100

    Disables recursion in phoenix.start to fix #462.
    As per discussion of issue #462, phoenix.start now
    * is nonrecursive
    * takes a list of routers to start as an argument
    * will only attempt to guess a router in non-umbrella project
    * will defer to a default list of routers provided in the mixfile

--- lib/mix/tasks/phoenix.start.ex
+++ lib/mix/tasks/phoenix.start.ex
@@ -2,26 +2,58 @@ defmodule Mix.Tasks.Phoenix.Start do
   use Mix.Task
 
   @shortdoc "Starts application workers"
-  @recursive true
 
   @moduledoc """
-  Starts the router or a given worker. Defaults to `MyApp.Router`
+  Starts one or more routers in the project. Defaults to `MyApp.Router` 
+  for standalone projects.
 
+      # start the default router(s) in the project
       $ mix phoenix.start
-      $ mix phoenix.start MyApp.AnotherRouter
 
+      # explicitly start several routers
+      $ mix phoenix.start Some.Router Another.Router
+
+  You can select the routers automatically started by phoenix.start when 
+  its invoked with no arguments by defining a `routers:` item in the 
+  project configuration:
+
+      defmodule MyApp.Mixfile do
+          use Mix.Project
+
+          def project do
+            [apps_path: "apps",
+             deps: deps,
+             routers: [Some.Router, Another.Router]]
+          end
+
+          defp deps do
+            []
+          end 
+      end
+
+  ## Umbrella projects
+  By default, `phoenix.start` will not start any routers in an umbrella 
+  project. Umbrella projects must either define their routers in the top
+  level mix file, or pass them specifically on the command line.
   """
-  def run([]) do
-    Mix.Task.run "app.start", []
-    Mix.Phoenix.router.start
-    no_halt
-  end
+  def run(routers \\ []) do
+    if routers == [] do 
+      routers = Keyword.get :Mix.Project.config, :routers, []
+    end
 
-  def run([worker]) do
-    Mix.Task.run "app.start", []
-    remote_worker = Module.concat("Elixir", worker)
-    remote_worker.start
-    no_halt
+    if Mix.Project.umbrella? do
+      start_all_apps
+    else
+      Mix.Task.run "app.start", []
+      if routers == [] do routers = [Mix.Phoenix.router.start] end
+    end
+
+    if routers != [] do
+      for r <- routers do 
+        Module.concat("Elixir", r).start
+      end
+      no_halt
+    end
   end
 
   defp no_halt do
@@ -30,5 +62,13 @@ defmodule Mix.Tasks.Phoenix.Start do
 
   defp iex_running? do
     Code.ensure_loaded?(IEx) && IEx.started?
+  end
+
+  defp start_all_apps do
+    config = Mix.Project.deps_config |> Keyword.delete(:deps_path)
+    for %Mix.Dep{app: app, opts: opts} <- Mix.Dep.Umbrella.loaded do
+      Mix.Project.in_project(app, opts[:path], config, 
+        fn _ -> Mix.Task.run "app.start", [] end)
+    end  
   end
 end
