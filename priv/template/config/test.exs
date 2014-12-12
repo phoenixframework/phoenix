aae12c5205dc6fb56fba4a94028270f7ee8b320d
Chris McCord <chris@chrismccord.com>
2014-12-12 16:08:09 -0500

    Enable Code Reloader for test env
    This is useful when testing views and templates outside of a web request.
    Otherwise, changes to views are not picked up and recompiled.

--- priv/template/config/test.exs
+++ priv/template/config/test.exs
@@ -2,3 +2,6 @@ use Mix.Config
 
 config :<%= application_name %>, <%= application_module %>.Endpoint,
   http: [port: System.get_env("PORT") || 4001]
+
+# Enables code reloading for test
+config :phoenix, :code_reloader, true
