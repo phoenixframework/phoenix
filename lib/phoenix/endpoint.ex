601a430c78f8092591beb493c0eb5baacd4b79ce
Gabriel Jaldon <gjaldon85@gmail.com>
2014-12-13 15:17:44 +0800

    Cache a static_path with Phoenix.Config.cache/3

--- lib/phoenix/endpoint.ex
+++ lib/phoenix/endpoint.ex
@@ -209,6 +209,16 @@ defmodule Phoenix.Endpoint do
           :__phoenix_url_helper__,
           &Phoenix.Endpoint.Adapter.url/1) <> path
       end
+
+      @doc """
+      Generates a route to a static file based on the `:static` configuration
+      for the endpoint.
+      """
+      def static_path(path) do
+        Phoenix.Config.cache(__MODULE__,
+          {:__phoenix_static__, path},
+          &Phoenix.Endpoint.Adapter.static_path(&1, path))
+      end
     end
   end
 
--- test/phoenix/endpoint_test.exs
+++ test/phoenix/endpoint_test.exs
@@ -2,11 +2,12 @@ defmodule Phoenix.EndpointTest do
   use ExUnit.Case, async: true
   use RouterHelper
 
-  @config [url: [host: "example.com"]]
-  Application.put_env(:endpoint_app, __MODULE__.Endpoint, @config)
+  @config [url: [host: "example.com"],
+           static: [root: "/priv/static", route: "/"]]
+  Application.put_env(:phoenix, __MODULE__.Endpoint, @config)
 
   defmodule Endpoint do
-    use Phoenix.Endpoint, otp_app: :endpoint_app
+    use Phoenix.Endpoint, otp_app: :phoenix
 
     plug Phoenix.CodeReloader, reloader: &__MODULE__.reload!/0
 
@@ -23,11 +24,14 @@ defmodule Phoenix.EndpointTest do
   test "has reloadable configuration" do
     assert Endpoint.config(:url) == [host: "example.com"]
     assert Endpoint.url("/") == "http://example.com/"
+    assert Endpoint.static_path("images/foo.png") == "/images/foo.png"
 
-    config = put_in @config[:url][:port], 1234
+    config = put_in(@config[:url][:port], 1234)
+    config = put_in(config[:static][:route], "/static")
     assert Endpoint.config_change([{Endpoint, config}], []) == :ok
     assert Endpoint.config(:url) == [host: "example.com", port: 1234]
     assert Endpoint.url("/") == "http://example.com:1234/"
+    assert Endpoint.static_path("/images/foo.png") == "/static/images/foo.png"
   end
 
   test "does not include code reloading if disabled" do
