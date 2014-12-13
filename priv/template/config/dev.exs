cd16b1692462e4fd82c2f457fe09a7c46dac0350
José Valim <jose.valim@plataformatec.com.br>
2014-12-13 11:31:00 +0100

    cache_static_lookup should be false for default in dev

--- priv/template/config/dev.exs
+++ priv/template/config/dev.exs
@@ -2,7 +2,8 @@ use Mix.Config
 
 config :<%= application_name %>, <%= application_module %>.Endpoint,
   http: [port: System.get_env("PORT") || 4000],
-  debug_errors: true
+  debug_errors: true,
+  cache_static_lookup: false
 
 # Enables code reloading for development
 config :phoenix, :code_reloader, true
