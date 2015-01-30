f543a344138bceee6e12819aaa3ce685346e2c1c
Chris McCord <chris@chrismccord.com>
2015-01-29 22:32:56 -0500

    Git rid of redis broadcaster pool ahead of client offload changes

--- lib/phoenix/pubsub/redis.ex
+++ lib/phoenix/pubsub/redis.ex
@@ -3,8 +3,6 @@ defmodule Phoenix.PubSub.Redis do
 
   @moduledoc false
 
-  @pool_size 5
-
   def start_link(opts) do
     supervisor_name = Module.concat(__MODULE__, Keyword.fetch!(opts, :name))
     Supervisor.start_link(__MODULE__, opts, name: supervisor_name)
@@ -13,21 +11,11 @@ defmodule Phoenix.PubSub.Redis do
   def init(opts) do
     server_name = Keyword.fetch!(opts, :name)
     local_name  = Module.concat(server_name, Local)
-    pool_name   = Module.concat(server_name, Pool)
-    server_opts = Keyword.merge(opts, local_name: local_name,
-                                      pool_name: pool_name)
-
-    pool_opts = [
-      name: {:local, pool_name},
-      worker_module: Phoenix.PubSub.RedisBroadcaster,
-      size: opts[:pool_size] || @pool_size,
-      max_overflow: 0
-    ]
+    server_opts = Keyword.merge(opts, local_name: local_name)
 
     children = [
       worker(Phoenix.PubSub.Local, [local_name], restart: :transient),
       worker(Phoenix.PubSub.RedisServer, [server_opts], restart: :transient),
-      :poolboy.child_spec(pool_name, pool_opts, [server_opts]),
     ]
     supervise children, strategy: :one_for_all
   end
