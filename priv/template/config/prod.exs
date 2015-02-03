d639f6edd65a70aff6847675027ae251ac16beea
edmz <edmz@users.noreply.github.com>
2015-02-03 12:23:17 -0600

    Making more obvious the https setting

--- priv/template/config/prod.exs
+++ priv/template/config/prod.exs
@@ -1,20 +1,24 @@
 use Mix.Config
 
-# ## SSL Support
-#
-# To get SSL working, you will need to set:
-#
-#     https: [port: 443,
-#             keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#             certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
-#
-# Where those two env variables point to a file on
-# disk for the key and cert.
-
 config :<%= application_name %>, <%= application_module %>.Endpoint,
   url: [host: "example.com"],
   http: [port: System.get_env("PORT")],
   secret_key_base: "<%= secret_key_base %>"
+  
+# ## SSL Support
+#
+# To get SSL working, you will need to add the `https` key
+# to the previous section:
+#
+#  config:<%= application_name %>, <%= application_module %>.Endpoint,
+#    ...
+#    https: [port: 443,
+#            keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
+#            certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
+#
+# Where those two env variables point to a file on
+# disk for the key and cert.
+  
 
 # Do not pring debug messages in production
 config :logger, level: :info
