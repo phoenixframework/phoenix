072552410468b9ebd1fa415ee58c25b2eca09923
Chris McCord <chris@chrismccord.com>
2015-02-08 15:08:22 -0500

    Add pubsub adapter config to boostrap template

--- priv/template/config/config.exs
+++ priv/template/config/config.exs
@@ -13,7 +13,9 @@ config :<%= application_name %>,
 config :<%= application_name %>, <%= application_module %>.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "<%= secret_key_base %>",
-  debug_errors: false
+  debug_errors: false,
+  pubsub: [adapter: Phoenix.PubSub.PG2]
+
 
 # Configures Elixir's Logger
 config :logger, :console,
