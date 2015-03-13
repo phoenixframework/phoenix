a3be123ec9b3bf9f1cd68e07e903652da590c0d0
Shankar Dhanasekaran <shankar@opendrops.com>
2015-03-13 20:26:24 +0530

    Add example of brunch file concatenation order
    Fixes https://github.com/phoenixframework/phoenix/issues/690

--- priv/static/brunch/brunch-config.js
+++ priv/static/brunch/brunch-config.js
@@ -3,6 +3,14 @@ exports.config = {
   files: {
     javascripts: {
       joinTo: 'js/app.js'
+      // To change the order of concatenation of files, explictly mention here
+      // https://github.com/brunch/brunch/tree/stable/docs#concatenation
+      // order: {
+      //   before: [
+      //     'web/static/vendor/js/jquery-2.1.1.js',
+      //     'web/static/vendor/js/bootstrap.min.js'
+      //   ]
+      // }      
     },
     stylesheets: {
       joinTo: 'css/app.css'
