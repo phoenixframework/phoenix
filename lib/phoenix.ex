f3015b829c96ff719be6aaf4adcaf648cc19b722
José Valim <jose.valim@plataformatec.com.br>
2015-04-11 10:05:05 +0200

    Remove consolidation work around

--- lib/phoenix.ex
+++ lib/phoenix.ex
@@ -3,16 +3,6 @@ defmodule Phoenix do
 
   @doc false
   def start(_type, _args) do
-    # Work around the fact consolidation for some
-    # protocols is not working currently
-    if :code.get_mode == :interactive do
-      :code.delete(Access)
-      :code.delete(Collectable)
-      :code.delete(List.Chars)
-      :code.delete(Phoenix.HTML.Safe)
-      :code.delete(Phoenix.Param)
-    end
-
     # Warm up caches
     _ = Phoenix.Template.engines
     _ = Phoenix.Template.format_encoder("index.html")
