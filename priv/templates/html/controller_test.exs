1a473f42c38ca4815bdf0d6e9df6949840717c6c
Lew Parker <parker@earthvectors.com>
2015-04-16 06:12:04 -0600

    Add tests for new and update action failure to generated tests  - also add a generated test for the edit endpoint

--- priv/templates/html/controller_test.exs
+++ priv/templates/html/controller_test.exs
@@ -3,6 +3,7 @@ defmodule <%= module %>ControllerTest do
   alias <%= module %>
 
   @valid_params <%= singular %>: <%= inspect params %>
+  @invalid_params <%= singular %>: %{}
 
   test "GET /<%= plural %>" do
     conn = get conn(), <%= singular %>_path(conn, :index)
@@ -19,10 +20,27 @@ defmodule <%= module %>ControllerTest do
     assert redirected_to(conn) == <%= singular %>_path(conn, :index)
   end
 
+  test "POST /<%= plural %> failure returns to the New page" do
+    conn = post conn(), <%= singular %>_path(conn, :create), @invalid_params
+    assert html_response(conn, 200) =~ "New <%= singular %>"
+  end
+
   test "GET /<%= plural %>/:id" do
     <%= singular %> = Repo.insert %<%= alias %>{}
     conn = get conn(), <%= singular %>_path(conn, :show, <%= singular %>.id)
     assert html_response(conn, 200) =~ "Show <%= singular %>"
+  end
+
+  test "GET /<%= plural %>/:id/edit" do
+    <%= singular %> = Repo.insert %<%= alias %>{}
+    conn = get conn(), <%= singular %>_path(conn, :edit, <%= singular %>.id)
+    assert html_response(conn, 200) =~ "Edit <%= singular %>"
+  end
+
+  test "PUT /<%= plural %>/:id failure returns to the Edit page" do
+    <%= singular %> = Repo.insert %<%= alias %>{}
+    conn = put conn(), <%= singular %>_path(conn, :update, <%= singular %>.id), @invalid_params
+    assert html_response(conn, 200) =~ "Edit <%= singular %>"
   end
 
   test "PUT /<%= plural %>/:id" do
