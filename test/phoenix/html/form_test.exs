825b6898e43248236c39bea121d57d889c68d041
Richard Nyström <ricn@rny.io>
2015-04-16 23:30:57 +0200

    Added telephone_input/3

--- test/phoenix/html/form_test.exs
+++ test/phoenix/html/form_test.exs
@@ -206,6 +206,24 @@ defmodule Phoenix.HTML.FormTest do
            {:safe, ~s(<input id="key" name="search[key][]" type="url" value="foo">)}
   end
 
+  ## telephone_input/3
+
+  test "telephone_input/3" do
+    assert telephone_input(:search, :key) ==
+           {:safe, ~s(<input id="search_key" name="search[key]" type="tel">)}
+
+    assert telephone_input(:search, :key, value: "foo", id: "key", name: "search[key][]") ==
+           {:safe, ~s(<input id="key" name="search[key][]" type="tel" value="foo">)}
+  end
+
+  test "telephone_input/3 with form" do
+    assert with_form(&telephone_input(&1, :key)) ==
+           {:safe, ~s(<input id="search_key" name="search[key]" type="tel" value="value">)}
+
+    assert with_form(&telephone_input(&1, :key, value: "foo", id: "key", name: "search[key][]")) ==
+           {:safe, ~s(<input id="key" name="search[key][]" type="tel" value="foo">)}
+  end
+
   ## submit/2
 
   test "submit/2" do
