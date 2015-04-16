a20a30fed0dfcf7c742422c2e9bdaa6e677b6a51
José Valim <jose.valim@plataformatec.com.br>
2015-04-16 15:55:11 +0200

    Use maps for date/times

--- lib/mix/phoenix.ex
+++ lib/mix/phoenix.ex
@@ -83,9 +83,9 @@ defmodule Mix.Phoenix do
       {k, :decimal}    -> {k, "120.5"}
       {k, :boolean}    -> {k, true}
       {k, :text}       -> {k, "some content"}
-      {k, :date}       -> {k, "2010-04-17"}
-      {k, :time}       -> {k, "14:00:00"}
-      {k, :datetime}   -> {k, "2010-04-17 14:00:00"}
+      {k, :date}       -> {k, %{year: 2010, month: 4, day: 17}}
+      {k, :time}       -> {k, %{hour: 14, min: 0}}
+      {k, :datetime}   -> {k, %{year: 2010, month: 4, day: 17, hour: 14, min: 0}}
       {k, :uuid}       -> {k, "7488a646-e31f-11e4-aace-600308960662"}
       {k, _}           -> {k, "some content"}
     end
