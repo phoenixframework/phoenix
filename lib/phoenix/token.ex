22e4f8ea52c6f427adf0c5dcfa741a9e1e45b48b
Jason S <jason@rokkincat.com>
2015-04-26 21:21:01 -0500

    Adding expiration and fixing nitpicky stuff

--- lib/phoenix/token.ex
+++ lib/phoenix/token.ex
@@ -14,26 +14,36 @@ defmodule Phoenix.Token do
   alias Plug.Crypto.MessageEncryptor
 
   @doc """
-    Encrypts your data into a token you can send down to clients
+  Encrypts your data into a token you can send down to clients
   """
   def gen_token(context, data) do
-    {secret, sign_secret} = get_endpoint_for(context) |> encryptor()
-    MessageEncryptor.encrypt_and_sign(data, secret, sign_secret)
+    {secret, sign_secret, max_age, encoder} = get_endpoint(context) |> encryptor()
+    message = %{ 
+      data: data,
+      exp: now_ms() + max_age
+    } |> encoder.encode!()
+    MessageEncryptor.encrypt_and_sign(message, secret, sign_secret)
   end
 
   @doc """
-    Decrypts the token into the originaly present data.
+  Decrypts the token into the originaly present data.
   """
   def verify_token(context, token) do
-    {secret, sign_secret} = get_endpoint_for(context) |> encryptor()
+    {secret, sign_secret, max_age, encoder} = get_endpoint(context) |> encryptor()
     case MessageEncryptor.verify_and_decrypt(token, secret, sign_secret) do
       :error -> :error
-      {:ok, data} -> 
+      {:ok, message} -> 
+        {:ok, %{ "data" => data, "exp" => exp}} = Poison.decode!(message)
+        if exp < now_ms() do
+          :token_expired
+        else
+          data
+        end
     end
   end
 
-  defp get_endpoint_for(conn = %Plug.Conn{}), do: Phoenix.Controller.endpoint_module(conn)
-  defp get_endpoint_for(socket = %Phoenix.Socket{}), do: socket.endpoint
+  defp get_endpoint(%Plug.Conn{} = conn), do: Phoenix.Controller.endpoint_module(conn)
+  defp get_endpoint(%Phoenix.Socket{} = socket), do: socket.endpoint
 
   # Gathers configuration and generates the key secrets and signing secrets.
   defp encryptor(endpoint) do
@@ -41,9 +51,17 @@ defmodule Phoenix.Token do
     secret_key_base = Dict.get(config, :secret_key_base)
     encryption_salt = Dict.get(config, :encryption_salt)
     signing_salt = Dict.get(config, :signing_salt)
+    max_age_in_ms = Dict.get(config, :max_age)
+    encoder =
+      Application.get_env(:phoenix, :format_encoders)
+      |> Keyword.get(:json, Poison)
 
     secret = KeyGenerator.generate(secret_key_base, encryption_salt)
     sign_secret = KeyGenerator.generate(secret_key_base, signing_salt)
-    {secret, sign_secret}
+    {secret, sign_secret, max_age_in_ms, encoder}
   end
+
+  defp time_to_ms({mega, sec, micro}),
+    do: ((((mega * 1000000) + sec) * 1000000) + micro) / 1000 |> trunc()
+  defp now_ms, do: :os.timestamp() |> time_to_ms()
 end
--- test/phoenix/token_text.exs
+++ test/phoenix/token_text.exs
@@ -1,3 +1,3 @@
-defmodule Phoenix.Test.Token do
+defmodule Phoenix.Test.TokenTest do
   use ExUnit.Case, async: true
 end
