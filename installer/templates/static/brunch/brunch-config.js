9a7a9e593a4ec63f01c8b959e2fc0fc79eb6079f
Steve <steve@stevedomin.com>
2015-05-10 02:12:46 +0200

    Commented example of having a separate vendor.js bundle

--- installer/templates/static/brunch/brunch-config.js
+++ installer/templates/static/brunch/brunch-config.js
@@ -3,6 +3,13 @@ exports.config = {
   files: {
     javascripts: {
       joinTo: 'js/app.js'
+      // To use a separate vendor.js bundle, specify two files path
+      // https://github.com/brunch/brunch/blob/stable/docs/config.md#files
+      // joinTo: {
+      //  'js/app.js': /^(web\/static\/js)/,
+      //  'js/vendor.js': /^(web\/static\/vendor)/
+      // }
+      //
       // To change the order of concatenation of files, explictly mention here
       // https://github.com/brunch/brunch/tree/stable/docs#concatenation
       // order: {
