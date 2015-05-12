48439924aa27f33eaa8291af2eaacf559c82f247
Rajesh Rajappan <rajappan.rajesh@gmail.com>
2015-05-12 06:15:18 +0100

    updated example channel info in the app.js

--- installer/templates/static/brunch/app.js
+++ installer/templates/static/brunch/app.js
@@ -2,7 +2,9 @@ import {Socket} from "phoenix"
 
 // let socket = new Socket("/ws")
 // socket.connect()
-// socket.join("topic:subtopic", {}).receive("ok", chan => {
+// let chan = socket.chan("topic:subtopic", {})
+// chan.join().receive("ok", chan => {
+//   console.log("Success!")
 // })
 
 let App = {
