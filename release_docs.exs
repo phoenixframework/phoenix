462ea8728974c27718e7e357ed66a3cd400b754a
José Valim <jose.valim@plataformatec.com.br>
2015-05-16 11:47:26 +0200

    Remove old release_docs.exs file

--- release_docs.exs
+++
@@ -1,6 +0,0 @@
-# Run with mix run release_docs.exs
-Mix.Task.run "docs"
-File.cd! "../docs", fn -> System.cmd "git", ["checkout", "gh-pages"] end
-File.rm_rf "../docs/*.html"
-File.cp_r "docs/.", "../docs/"
-File.rm_rf "docs"
