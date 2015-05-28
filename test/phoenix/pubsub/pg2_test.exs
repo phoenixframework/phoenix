1c9b9534db0bd5b897f37fabb177b96d75219c5d
José Valim <jose.valim@plataformatec.com.br>
2015-05-28 10:33:33 +0200

    Require proper pubsub test file

--- test/phoenix/pubsub/pg2_test.exs
+++ test/phoenix/pubsub/pg2_test.exs
@@ -1,2 +1,2 @@
 Application.put_env(:phoenix, :pubsub_test_adapter, Phoenix.PubSub.PG2)
-Code.require_file "../../support/pubsub_setup.exs", __DIR__
+Code.require_file "../../shared/pubsub_test.exs", __DIR__
