3612722863934b2a0e3c6f9a69c406a6a91212ec
Jānis Miezītis <janjiss@gmail.com>
2015-06-25 01:57:39 +0300

    Fixed the docstring to comply with the latest version of ecto
    The docstring says that you have to pass nil as params to return an empty changeset, yet this functionality is deprecated.

--- priv/templates/model/model.ex
+++ priv/templates/model/model.ex
@@ -14,7 +14,7 @@ defmodule <%= module %> do
   @doc """
   Creates a changeset based on the `model` and `params`.
 
-  If `params` are nil, an invalid changeset is returned
+  If the value of `params` is the symbol :empty, an invalid changeset is returned
   with no validation performed.
   """
   def changeset(model, params \\ :empty) do
