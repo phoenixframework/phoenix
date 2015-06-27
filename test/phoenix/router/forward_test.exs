89468a9d64420fdce21737213a2fe155e3e3c360
Chris McCord <chris@chrismccord.com>
2015-06-26 21:59:55 -0400

    Clean up forward tests

--- test/phoenix/router/forward_test.exs
+++ test/phoenix/router/forward_test.exs
@@ -4,22 +4,14 @@ defmodule Phoenix.Router.ForwardTest do
 
   defmodule Controller do
     use Phoenix.Controller
-    alias Phoenix.Router.ForwardTest.AdminDashboard
 
-    plug :put_assigns
+    plug :assign_fwd_conn
 
     def index(conn, _params), do: text(conn, "admin index")
     def stats(conn, _params), do: text(conn, "stats")
     def api_users(conn, _params), do: text(conn, "api users")
     def api_root(conn, _params), do: text(conn, "api root")
-
-    defp put_assigns(conn, _) do
-      page_path = AdminDashboard.Helpers.page_path(conn, :stats)
-
-      conn
-      |> assign(:internal_admin_page_path, page_path)
-      |> assign(:fwd_script_name, conn.script_name)
-    end
+    defp assign_fwd_conn(conn, _), do: assign(conn, :fwd_conn, conn)
   end
 
   defmodule ApiRouter do
@@ -41,22 +33,12 @@ defmodule Phoenix.Router.ForwardTest do
   defmodule Router do
     use Phoenix.Router
 
-    pipeline :browser do
-      plug :assigns
-    end
-
     scope "/" do
-      pipe_through :browser
       get "/stats", Controller, :stats
       forward "/admin", AdminDashboard
       scope "/internal" do
         forward "/api/v1", ApiRouter
       end
-    end
-
-    defp assigns(conn, _) do
-      Plug.Conn.assign(conn, :external_admin_page_path,
-                       AdminDashboard.Helpers.page_path(conn, :stats))
     end
   end
 
@@ -67,16 +49,18 @@ defmodule Phoenix.Router.ForwardTest do
 
   test "forwards path to plug" do
     conn = call(Router, :get, "admin")
+    fwd_conn = conn.assigns[:fwd_conn]
     assert conn.script_name == []
-    assert conn.assigns[:fwd_script_name] == ["admin"]
+    assert fwd_conn.script_name == ["admin"]
     assert conn.status == 200
     assert conn.resp_body == "admin index"
   end
 
   test "forwards any request starting with forward path" do
     conn = call(Router, :get, "admin/stats")
+    fwd_conn = conn.assigns[:fwd_conn]
     assert conn.script_name == []
-    assert conn.assigns[:fwd_script_name] == ["admin"]
+    assert fwd_conn.script_name == ["admin"]
     assert conn.status == 200
     assert conn.resp_body == "stats"
   end
@@ -120,20 +104,27 @@ defmodule Phoenix.Router.ForwardTest do
   end
 
   test "helpers cascade script name across forwards based on main router" do
-    assert AdminDashboard.Helpers.page_path(%Plug.Conn{}, :stats) == "/stats"
+    import AdminDashboard.Helpers
+    assert page_path(%Plug.Conn{}, :stats) == "/stats"
 
     conn = call(Router, :get, "stats")
-    assert conn.assigns[:internal_admin_page_path] == "/admin/stats"
-    assert conn.assigns[:external_admin_page_path] == "/admin/stats"
+    fwd_conn = conn.assigns[:fwd_conn]
+    assert page_path(fwd_conn, :stats) == "/admin/stats"
+    assert page_path(conn, :stats) == "/admin/stats"
+
     conn = call(Router, :get, "stats", _params = nil, _headers = [], ["phx"])
-    assert conn.assigns[:internal_admin_page_path] == "/phx/admin/stats"
-    assert conn.assigns[:external_admin_page_path] == "/phx/admin/stats"
+    fwd_conn = conn.assigns[:fwd_conn]
+    assert page_path(fwd_conn, :stats) == "/phx/admin/stats"
+    assert page_path(conn, :stats) == "/phx/admin/stats"
 
     conn = call(Router, :get, "admin/stats")
-    assert conn.assigns[:internal_admin_page_path] == "/admin/stats"
-    assert conn.assigns[:external_admin_page_path] == "/admin/stats"
+    fwd_conn = conn.assigns[:fwd_conn]
+    assert page_path(fwd_conn, :stats) == "/admin/stats"
+    assert page_path(conn, :stats) == "/admin/stats"
+
     conn = call(Router, :get, "admin/stats", _params = nil, _headers = [], ["phx"])
-    assert conn.assigns[:internal_admin_page_path] == "/phx/admin/stats"
-    assert conn.assigns[:external_admin_page_path] == "/phx/admin/stats"
+    fwd_conn = conn.assigns[:fwd_conn]
+    assert page_path(fwd_conn, :stats) == "/phx/admin/stats"
+    assert page_path(conn, :stats) == "/phx/admin/stats"
   end
 end
