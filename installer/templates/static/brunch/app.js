df90476ba02b50032f1bfe8defbc8bc5059fecb7
Chris McCord <chris@chrismccord.com>
2015-07-04 17:28:09 -0400

    Bump app.js template to use new conventions

--- installer/templates/static/brunch/app.js
+++ installer/templates/static/brunch/app.js
@@ -3,8 +3,8 @@ import {Socket} from "phoenix"
 // let socket = new Socket("/ws")
 // socket.connect()
 // let chan = socket.chan("topic:subtopic", {})
-// chan.join().receive("ok", chan => {
-//   console.log("Success!")
+// chan.join().receive("ok", resp => {
+//   console.log("Joined succesffuly!", resp)
 // })
 
 let App = {
