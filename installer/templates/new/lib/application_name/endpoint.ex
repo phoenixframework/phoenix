51461a83858ed33ac639bf57d6a4f413687998a7
José Valim <jose.valim@gmail.com>
2015-07-12 16:22:16 +0200

    Server fonts directory in Plug.Static by default

--- installer/templates/new/lib/application_name/endpoint.ex
+++ installer/templates/new/lib/application_name/endpoint.ex
@@ -7,7 +7,7 @@ defmodule <%= application_module %>.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :<%= application_name %>, gzip: false,
-    only: ~w(css images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
