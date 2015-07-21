47dee5f3ae41a04f8afbfc57879547e918b84924
José Valim <jose.valim@plataformatec.com.br>
2015-07-21 15:09:54 +0200

    Require pubsub adapters to define a dispatch table
    Prior to this commit, every PubSub operation had to
    be serialized through a server, often only to delegate
    to a local server.
    
    This commit solves this issue by requiring PubSub
    adapters to define a dispatch table with the same name
    as the server and reading directly from this table
    when a dispatch is necessary.

--- lib/phoenix/pubsub/pg2_server.ex
+++ lib/phoenix/pubsub/pg2_server.ex
@@ -4,49 +4,38 @@ defmodule Phoenix.PubSub.PG2Server do
   use GenServer
   alias Phoenix.PubSub.Local
 
-  def start_link(opts) do
-    GenServer.start_link __MODULE__, opts, name: Dict.fetch!(opts, :name)
+  def start_link(name, local_name) do
+    GenServer.start_link __MODULE__, {name, local_name}, name: name
   end
 
-  def init(opts) do
-    server_name   = Keyword.fetch!(opts, :name)
-    local_name    = Keyword.fetch!(opts, :local_name)
-    pg2_namespace = pg2_namespace(server_name)
-
-    :ok = :pg2.create(pg2_namespace)
-    :ok = :pg2.join(pg2_namespace, self)
-
-    {:ok, %{local_name: local_name, namespace: pg2_namespace}}
-  end
-
-  def handle_call({:subscribe, pid, topic, opts}, _from, state) do
-    response = {:perform, {Local, :subscribe, [state.local_name, pid, topic, opts]}}
-    {:reply, response, state}
-  end
-
-  def handle_call({:unsubscribe, pid, topic}, _from, state) do
-    response = {:perform, {Local, :unsubscribe, [state.local_name, pid, topic]}}
-    {:reply, response, state}
-  end
-
-  def handle_call({:broadcast, from_pid, topic, msg}, _from, state) do
-    case :pg2.get_members(state.namespace) do
+  def broadcast(name, local_name, from_pid, topic, msg) do
+    case :pg2.get_members(pg2_namespace(name)) do
       {:error, {:no_such_group, _}} ->
-        {:stop, :no_such_group, {:error, :no_such_group}, state}
+        {:error, :no_such_group}
 
       pids when is_list(pids) ->
-        Enum.each(pids, &send(&1, {:forward_to_local, from_pid, topic, msg}))
-        {:reply, :ok, state}
+        Enum.each(pids, fn
+          pid when node(pid) == node() ->
+            Local.broadcast(local_name, from_pid, topic, msg)
+          pid ->
+            send(pid, {:forward_to_local, from_pid, topic, msg})
+        end)
+        :ok
     end
   end
 
-  def handle_call(:stop, _from, state) do
-    {:stop, :normal, :ok, state}
+  def init({name, local_name}) do
+    pg2_namespace = pg2_namespace(name)
+    :ok = :pg2.create(pg2_namespace)
+    :ok = :pg2.join(pg2_namespace, self)
+    {:ok, local_name}
   end
 
-  def handle_info({:forward_to_local, from_pid, topic, msg}, state) do
-    Local.broadcast(state.local_name, from_pid, topic, msg)
-    {:noreply, state}
+  def handle_info({:forward_to_local, from_pid, topic, msg}, local_name) do
+    # The whole broadcast will happen inside the current process
+    # but only for messages coming from the distributed system.
+    Local.broadcast(local_name, from_pid, topic, msg)
+    {:noreply, local_name}
   end
 
   defp pg2_namespace(server_name), do: {:phx, server_name}
