9dd345d461e276c4d530b3e6ca56ae48f009d568
José Valim <jose.valim@plataformatec.com.br>
2015-07-29 14:45:51 +0200

    Rename sign_token/verify_token to sign/verify

--- lib/phoenix/token.ex
+++ lib/phoenix/token.ex
@@ -1,57 +1,58 @@
 defmodule Phoenix.Token do
   @moduledoc """
-  Tokens provide a way to  generate, verify, and decrypt bearer
+  Tokens provide a way to  generate, verify bearer
   tokens for use in Channels or API authentication.
 
   ## Basic Usage
+
   When generating a unique token for usage in an API or Channel
   it is advised to use a unique identifier for the user typically
-  the id from a Database. For example:
+  the id from a database. For example:
 
       iex> user_id = 1
-      iex> token = sign_token(endpoint, "user", user_id)
-      iex> user_id == verify_token(endpoint, token)
-      true
+      iex> token = Phoenix.Token.sign(endpoint, "user", user_id)
+      iex> Phoenix.Token.verify(endpoint, token)
+      {:ok, 1}
 
   In that example we have a user's id, we generate a token and send
-  it to the client. When the client uses the token in a futher
-  communication you verify the token, query the database and authorize
-  the user.
+  it to the client. We could send it to the client in multiple ways.
+  One is via the meta tag:
+
+      <%= tag :meta, name: "channel_token"
+                     content: Phoenix.Token.sign(@conn, "user", @current_user.id) %>
+
+  Or an endpoint that returns it:
+
+      def create(conn, params) do
+        user = User.create(params)
+        render conn, "user.json",
+               %{token: Phoenix.Token.sign(conn, "user", user.id), user: user}
+      end
 
   When using it with a socket a typical example might be:
 
-    defmodule MyChannel do
-      def join("my:" <> id = topic, %{token: token}, socket) do
-        case verify_token(socket, "user", token) do
-          user_id ->
+    defmodule MyApp.UserSocket do
+      use Phoenix.Socket
+
+      def connect(%{"token" => token}, socket) do
+        # Max age of 2 weeks (1209600 seconds)
+        case Phoenix.Token.verify(socket, "user", token, max_age: 1209600) do
+          {:ok, user_id} ->
             socket = assigns(socket, :user, Repo.get!(User, user_id))
-            reply socket, "join", %{}
-          {:error, :expired} -> :ignore
-          {:error, :invalid} -> :ignore
+            {:ok, socket}
+          {:error, _} ->
+            :error
         end
       end
     end
 
-  In this example the Phoenix.js client will be sending up the token
-  in the join command.
+  In this example the phoenix.js client will be sending up the token
+  in the connect command.
 
-  If you want to provide an API to the user send it down to the user
-  by generating it in a controller or view action.
-
-    def create(conn, params) do
-      user = User.create(params)
-      render conn, "user.json", %{token: sign_token(conn, "user", user.id), user: user}
-    end
-
-    Then the client can use it all the way through. If you'd like to
-    use the same token for API authorization then I suggest adding it
-    as a header to your authroized http requests
-
-      Authorization: Bearer #\{token}
-
-    Then create a plug to verify the token and authorize with the database.
-
+  `Phoenix.Token` can also be used for validating APIs, handling
+  password resets, e-mail confirmation and more.
   """
+
   alias Plug.Crypto.KeyGenerator
   alias Plug.Crypto.MessageVerifier
 
@@ -66,7 +67,7 @@ defmodule Phoenix.Token do
     * `:key_digest` - option passed to `Plug.Crypto.KeyGenerator`
       when generating the encryption and signing keys. Defaults to `:sha256';
   """
-  def sign_token(context, salt,  data, opts \\ []) when is_binary(data) or is_integer(data) or is_map(data) do
+  def sign(context, salt, data, opts \\ []) when is_binary(salt) do
     secret = get_endpoint(context) |> get_secret(salt, opts)
 
     message = %{
@@ -80,37 +81,40 @@ defmodule Phoenix.Token do
   Decrypts the token into the originaly present data.
 
   ## Options
+
+    * `:max_age` - verifies the token only if it has been generated
+      "max age" ago in seconds. A reasonable value is 2 weeks (`1209600`
+      seconds);
     * `:key_iterations` - option passed to `Plug.Crypto.KeyGenerator`
       when generating the encryption and signing keys. Defaults to 1000;
     * `:key_length` - option passed to `Plug.Crypto.KeyGenerator`
       when generating the encryption and signing keys. Defaults to 32;
     * `:key_digest` - option passed to `Plug.Crypto.KeyGenerator`
       when generating the encryption and signing keys. Defaults to `:sha256';
+
   """
-  def verify_token(context, salt, token, opts \\ []) do
+  def verify(context, salt, token, opts \\ []) when is_binary(salt) and is_binary(token) do
     secret = get_endpoint(context) |> get_secret(salt, opts)
     case MessageVerifier.verify(token, secret) do
-      :error -> {:error, :invalid}
       {:ok, message} ->
         %{data: data, signed: signed} = :erlang.binary_to_term(message)
 
-        max_age = if Dict.has_key?(opts, :max_age) do
-          opts[:max_age]
-        end
-
-        if max_age && (signed + max_age) < now_ms() do
+        if (max_age = opts[:max_age]) && (signed + max_age) < now_ms() do
           {:error, :expired}
         else
-          data
+          {:ok, data}
         end
+      :error ->
+        {:error, :invalid}
     end
   end
 
   defp get_endpoint(%Plug.Conn{} = conn), do: Phoenix.Controller.endpoint_module(conn)
   defp get_endpoint(%Phoenix.Socket{} = socket), do: socket.endpoint
+  defp get_endpoint(endpoint) when is_atom(endpoint), do: endpoint
 
   # Gathers configuration and generates the key secrets and signing secrets.
-  defp get_secret(endpoint, salt, opts \\ []) do
+  defp get_secret(endpoint, salt, opts) do
     secret_key_base = endpoint.config(:secret_key_base)
     iterations = Keyword.get(opts, :key_iterations, 1000)
     length = Keyword.get(opts, :key_length, 32)
@@ -122,7 +126,6 @@ defmodule Phoenix.Token do
     KeyGenerator.generate(secret_key_base, salt, key_opts)
   end
 
-  defp time_to_ms({mega, sec, micro}),
-    do: ((((mega * 1000000) + sec) * 1000000) + micro) / 1000 |> trunc()
+  defp time_to_ms({mega, sec, _micro}), do: (mega * 1000000 + sec) * 1000
   defp now_ms, do: :os.timestamp() |> time_to_ms()
 end
--- test/phoenix/token_test.exs
+++ test/phoenix/token_test.exs
@@ -3,51 +3,38 @@ defmodule Phoenix.TokenTest do
   alias Phoenix.Token
 
   defmodule TokenEndpoint do
-    use Phoenix.Endpoint, otp_app: :endpoint_token
+    def config(:secret_key_base), do: "abc123"
   end
 
-  Application.put_env(:endpoint_token, TokenEndpoint, secret_key_base: "abc123")
-
-  setup do 
-    {:ok, pid} = TokenEndpoint.start_link
-    on_exit(fn ->
-      Process.unlink(pid)
-      Process.exit(pid, :shutdown)
-    end)
-  end
-
-  test "happy path" do
+  test "signes and verifies token with connection" do
     id = 1
-    token = Token.sign_token(conn(), "id", id)
-    assert id == Token.verify_token(conn(), "id", token)
+    token = Token.sign(conn(), "id", id)
+    assert Token.verify(conn(), "id", token) == {:ok, id}
   end
 
-  test "given a junk token it fails" do
-    assert {:error, :invalid} == Token.verify_token(conn(), "garbage", "truck")
-  end
-
-  test "verify it works with a socket as well" do
+  test "signes and verifies token with socket" do
     id = 1
-    token = Token.sign_token(socket(), "id", id)
-    assert id == Token.verify_token(socket(), "id", token)
+    token = Token.sign(socket(), "id", id)
+    assert Token.verify(socket(), "id", token) == {:ok, id}
   end
 
-  test "overriding expiration" do
-    token = Token.sign_token(conn(), "id", 1)
-    Stream.timer(40) |> Enum.map(fn (_) ->
-      assert {:error, :expired} == Token.verify_token(conn(), "id", token, max_age: 30)
-    end)
+  test "fails on invalid token" do
+    token = Token.sign(TokenEndpoint, "id", 1)
+
+    assert Token.verify(TokenEndpoint, "id", "garbage") ==
+           {:error, :invalid}
+    assert Token.verify(TokenEndpoint, "not_id", token) ==
+           {:error, :invalid}
   end
 
-  test "nil expiration" do
-    token = Token.sign_token(conn(), "id", 1)
-    Stream.timer(40) |> Enum.map(fn (_) ->
-      assert {:error, :expired} != Token.verify_token(conn(), "id", token, max_age: nil)
-    end)
+  test "supports max age" do
+    token = Token.sign(conn(), "id", 1)
+    assert Token.verify(conn(), "id", token, max_age: 1000) == {:ok, 1}
+    assert Token.verify(conn(), "id", token, max_age: -1000) == {:error, :expired}
   end
 
   defp socket() do
-    %Phoenix.Socket{ endpoint: TokenEndpoint }
+    %Phoenix.Socket{endpoint: TokenEndpoint}
   end
 
   defp conn() do
