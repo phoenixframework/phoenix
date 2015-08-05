6819ec2bb12dfb8511fe02aea18d1ee9b8a3dd79
José Valim <jose.valim@gmail.com>
2015-08-05 09:32:49 +0200

    Pass view on generated render_one/render_many

--- priv/templates/phoenix.gen.json/view.ex
+++ priv/templates/phoenix.gen.json/view.ex
@@ -2,11 +2,11 @@ defmodule <%= module %>View do
   use <%= base %>.Web, :view
 
   def render("index.json", %{<%= plural %>: <%= plural %>}) do
-    %{data: render_many(<%= plural %>, "<%= singular %>.json")}
+    %{data: render_many(<%= plural %>, <%= module %>View, "<%= singular %>.json")}
   end
 
   def render("show.json", %{<%= singular %>: <%= singular %>}) do
-    %{data: render_one(<%= singular %>, "<%= singular %>.json")}
+    %{data: render_one(<%= singular %>, <%= module %>View, "<%= singular %>.json")}
   end
 
   def render("<%= singular %>.json", %{<%= singular %>: <%= singular %>}) do
