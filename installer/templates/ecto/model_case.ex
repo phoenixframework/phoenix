df9d26432019bc7c4fff832c90dfe0659ae17040
José Valim <jose.valim@gmail.com>
2015-08-26 15:44:46 +0200

    Update model_case.ex

--- installer/templates/ecto/model_case.ex
+++ installer/templates/ecto/model_case.ex
@@ -36,15 +36,22 @@ defmodule <%= application_module %>.ModelCase do
 
   ## Examples
 
-  Given a User model that has validation for the presence of a value for the
-  `:name` field and validation that `:password` is "safe":
+  Given a User model that lists `:name` as a required field and validates
+  `:password` to be safe, it would return:
 
       iex> errors_on(%User{}, password: "password")
-      [{:password, "is unsafe"}, {:name, "is blank"}]
+      [password: "is unsafe", name: "is blank"]
 
-  You would then write your assertion like:
+  You could then write your assertion like:
 
       assert {:password, "is unsafe"} in errors_on(%User{}, password: "password")
+  
+  You can also create the changeset manually and retrieve the errors
+  field directly:
+  
+      iex> changeset = User.changeset(%User{}, password: "password")
+      iex> {:password, "is unsafe"} in changeset.errors
+      true
   """
   def errors_on(model, data) do
     model.__struct__.changeset(model, data).errors
