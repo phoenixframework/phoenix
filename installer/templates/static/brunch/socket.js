dbb0a5dd148c0aa83c61e09a37fa0568f1224855
Josh Adams <josh.rubyist@gmail.com>
2015-08-30 14:49:26 -0500

    Fix a couple of typos in socket.js

--- installer/templates/static/brunch/socket.js
+++ installer/templates/static/brunch/socket.js
@@ -56,7 +56,7 @@ socket.connect({token: window.userToken})
 // Now that you are connected, you can join channels with a topic:
 let channel = socket.channel("topic:subtopic", {})
 channel.join()
-  .receive("ok", resp => { console.log("Joined succesffuly", resp) })
-  .receive("error", resp => { console.log("Unabled to join", resp) })
+  .receive("ok", resp => { console.log("Joined successfully", resp) })
+  .receive("error", resp => { console.log("Unable to join", resp) })
 
 export default socket
