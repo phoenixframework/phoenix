28fb11592a760392eccee14907dba53254a47b4d
Steve Domin <steve@stevedomin.com>
2015-09-10 02:23:25 +0100

    Add --no-html flag to Phoenix installer
    This allows to skip generating HTML views and including related
    dependencies (phoenix_html, phoenix_live_reload)

--- installer/templates/new/web/views/error_view.ex
+++ installer/templates/new/web/views/error_view.ex
@@ -1,7 +1,7 @@
 defmodule <%= application_module %>.ErrorView do
   use <%= application_module %>.Web, :view
 
-  def render("404.html", _assigns) do
+  <%= if html do %>def render("404.html", _assigns) do
     "Page not found"
   end
 
@@ -13,5 +13,17 @@ defmodule <%= application_module %>.ErrorView do
   # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
     render "500.html", assigns
+  end<% else %>def render("404.json", _assigns) do
+    %{"errors" => %{"detail" => "Page not found"}}
   end
+
+  def render("500.json", _assigns) do
+    %{"errors" => %{"detail" => "Server internal error"}}
+  end
+
+  # In case no render clause matches or no
+  # template is found, let's render it as 500
+  def template_not_found(_template, assigns) do
+    render "500.json", assigns
+  end<% end %>
 end
