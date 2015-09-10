e92ca7c7f950484cf08ebe443d073713d5a719be
José Valim <jose.valim@gmail.com>
2015-09-10 20:49:24 +0200

    Remove stacktrace depth from test
    ExUnit will support it with Elixir v1.1.

--- installer/templates/new/config/test.exs
+++ installer/templates/new/config/test.exs
@@ -8,6 +8,3 @@ config :<%= application_name %>, <%= application_module %>.Endpoint,
 
 # Print only warnings and errors during test
 config :logger, level: :warn
-
-# Set a higher stacktrace during test
-config :phoenix, :stacktrace_depth, 20
