d0e80673176d3f0e9f8656fa0ec3424d4ac56d59
José Valim <jose.valim@gmail.com>
2015-09-18 13:09:54 +0200

    Improve docs for aliases

--- installer/templates/new/mix.exs
+++ installer/templates/new/mix.exs
@@ -15,22 +15,22 @@ defmodule <%= application_module %>.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
+  # Configuration for the OTP application.
   #
-  # Type `mix help compile.app` for more information
+  # Type `mix help compile.app` for more information.
   def application do
     [mod: {<%= application_module %>, []},
      applications: [:phoenix, :phoenix_html, :cowboy, :logger<%= if ecto do %>,
                     :phoenix_ecto, <%= inspect adapter_app %><% end %>]]
   end
 
-  # Specifies which paths to compile per environment
+  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
   defp elixirc_paths(_),     do: ["lib", "web"]
 
-  # Specifies your project dependencies
+  # Specifies your project dependencies.
   #
-  # Type `mix help deps` for examples and options
+  # Type `mix help deps` for examples and options.
   defp deps do
     [<%= phoenix_dep %>,<%= if ecto do %>
      {:phoenix_ecto, "~> 1.1"},
@@ -41,6 +41,11 @@ defmodule <%= application_module %>.Mixfile do
   end<%= if ecto do %>
 
   # Aliases are shortcut or tasks specific to the current project.
+  # For example, to create, migrate and run the seeds file at once:
+  #
+  #     $ mix ecto.setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"]]
