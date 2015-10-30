24ac818b458e2785700679c52d562a2b2b7b4ac5
Chris McCord <chris@chrismccord.com>
2015-10-29 20:06:40 -0400

    Add windows friendly joinKey for workound. closes #1319

--- installer/templates/static/brunch/brunch-config.js
+++ installer/templates/static/brunch/brunch-config.js
@@ -59,7 +59,8 @@ exports.config = {
 
   modules: {
     autoRequire: {
-      "js/app.js": ["web/static/js/app"]
+      "js/app.js": ["web/static/js/app"],
+      "js\\app.js": ["web/static/js/app"]
     }
   },
 
