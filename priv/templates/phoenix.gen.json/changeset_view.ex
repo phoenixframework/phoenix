6759f7d7cd4e78a5331e9c37345b67286ad7f781
Chris McCord <chris@chrismccord.com>
2015-12-10 13:13:02 -0500

    Do not encode the changeset for errors
    Instead use new Ecto.Changeset.traverse_errors.

--- priv/templates/phoenix.gen.json/changeset_view.ex
+++ priv/templates/phoenix.gen.json/changeset_view.ex
@@ -1,9 +1,18 @@
 defmodule <%= base %>.ChangesetView do
   use <%= base %>.Web, :view
 
+  @doc """
+  Traverses and translates changeset errors.
+
+  See `Ecto.Changeset.traverse_errors/2` for more details.`
+  """
+  def translate_errors(changeset) do
+    Ecto.Changeset.traverse_errors(changeset, &translate_error(&1))
+  end
+
   def render("error.json", %{changeset: changeset}) do
     # When encoded, the changeset returns its errors
     # as a JSON object. So we just pass it forward.
-    %{errors: changeset}
+    %{errors: translate_errors(changeset)}
   end
 end
