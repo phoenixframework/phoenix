7bd0f25132c4b7a88a0eb47dcf5dd59899f652a3
José Valim <jose.valim@plataformatec.com.br>
2015-12-15 15:44:58 +0100

    Tidy up handler behaviour docs

--- lib/phoenix/endpoint/handler.ex
+++ lib/phoenix/endpoint/handler.ex
@@ -1,22 +1,21 @@
 defmodule Phoenix.Endpoint.Handler do
   @moduledoc """
-  API for exporting webserver plug adapter child spec
+  API for exporting a webserver.
 
-  A handler will need to implement a child_spec/3
-  function which takes
+  A handler will need to implement a `child_spec/3`
+  function which takes:
 
-    * the schme of the endpoint :http or :https
+    * the scheme of the endpoint :http or :https
     * phoenix top-most endpoint module
     * phoenix app configuration for the specified scheme
 
-  it has to return a child_spec
+  it has to return a child_spec.
   """
   use Behaviour
 
   @doc """
-  provides the children specification to be passed
-  to Phoenix.Endpoint.Server supervisor
+  Provides the children specification to be passed
+  to `Phoenix.Endpoint.Server` supervisor.
   """
-  @callback child_spec(scheme :: atom, endpoint :: module, config :: Keyword.t) :: Supervisor.Spec.spec
-
+  defcallback child_spec(scheme :: atom, endpoint :: module, config :: Keyword.t) :: Supervisor.Spec.spec
 end
