61ac735c32fbdc6896b6e4e9bee4b8b9460f6cba
José Valim <jose.valim@plataformatec.com.br>
2015-12-18 15:09:39 +0100

    Pass the repo explicitly to migration commands

--- installer/templates/new/test/test_helper.exs
+++ installer/templates/new/test/test_helper.exs
@@ -1,6 +1,6 @@
 ExUnit.start
 <%= if ecto do %>
-Mix.Task.run "ecto.create", ["--quiet"]
-Mix.Task.run "ecto.migrate", ["--quiet"]
+Mix.Task.run "ecto.create", ~w(-r <%= application_module %>.Repo --quiet)
+Mix.Task.run "ecto.migrate", ~w(-r <%= application_module %>.Repo --quiet)
 <%= adapter_config[:test_begin] %>
 <% end %>
