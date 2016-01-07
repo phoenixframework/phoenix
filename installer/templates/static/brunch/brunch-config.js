857fbcedbc631ad08f6485ae1aea6c2fb44b67e9
Chris McCord <chris@chrismccord.com>
2016-01-07 16:43:26 -0500

    Add whitelist note

--- installer/templates/static/brunch/brunch-config.js
+++ installer/templates/static/brunch/brunch-config.js
@@ -63,6 +63,8 @@ exports.config = {
 
   npm: {
     enabled: true,
+    // Whitelist the npm deps to be pulled in as front-end assets.
+    // All other deps in package.json will be excluded from the bundle.
     whitelist: ["phoenix", "phoenix_html"]
   }
 };
