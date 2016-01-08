f477067573530bfb3f705700d115638eb124778c
Chris McCord <chris@chrismccord.com>
2016-01-08 15:38:25 -0500

    Release 1.1.2

--- installer/mix.exs
+++ installer/mix.exs
@@ -3,7 +3,7 @@ defmodule Phoenix.New.Mixfile do
 
   def project do
     [app: :phoenix_new,
-     version: "1.1.1",
+     version: "1.1.2",
      elixir: "~> 1.0-dev"]
   end
 
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Phoenix.Mixfile do
   use Mix.Project
 
-  @version "1.1.2-dev"
+  @version "1.1.2"
 
   def project do
     [app: :phoenix,
