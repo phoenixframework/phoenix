59de38fb4943b2bc5b9d8ad13a9038565becfe57
Steve Domin <steve@stevedomin.com>
2016-01-13 15:33:17 +0000

    Skip phoenix_html npm dependency when using --no-html flag
    Fixes #1465

--- installer/templates/static/brunch/brunch-config.js
+++ installer/templates/static/brunch/brunch-config.js
@@ -65,6 +65,6 @@ exports.config = {
     enabled: true,
     // Whitelist the npm deps to be pulled in as front-end assets.
     // All other deps in package.json will be excluded from the bundle.
-    whitelist: ["phoenix", "phoenix_html"]
+    whitelist: ["phoenix"<%= if html do %>, "phoenix_html"<% end %>]
   }
 };
