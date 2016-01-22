300f3c540ae91ece5cf80543bd11ece00faf3baf
José Valim <jose.valim@plataformatec.com.br>
2016-01-22 23:44:51 +0100

    Add task for updating Phoenix

---
+++ installer/lib/local_phoenix.ex
@@ -0,0 +1,17 @@
+defmodule Mix.Tasks.Local.Phoenix do
+  use Mix.Task
+
+  @url "https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez"
+  @shortdoc "Updates Phoenix locally"
+
+  @moduledoc """
+  Updates Phoenix locally.
+
+      mix local.phoenix
+  
+  Accepts the same command line options as `archive.install`.
+  """
+  def run(args) do
+    Mix.Task.run "archive.install", [@url|args]
+  end
+end
