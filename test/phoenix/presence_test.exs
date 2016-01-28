5922dc758871672c644f8940567b9393abc91616
Chris McCord <chris@chrismccord.com>
2016-01-28 17:19:17 -0500

    Fix race conditions in presence tests

--- test/phoenix/presence_test.exs
+++ test/phoenix/presence_test.exs
@@ -24,63 +24,71 @@ defmodule Phoenix.PresenceTest do
     {:ok, pubsub: PresPub}
   end
 
-  test "default fetch/2 returns pass-through data" do
+  setup config do
+    {:ok, topic: config.test}
+  end
+
+  test "default fetch/2 returns pass-through data", config do
     presences = %{"u1" => %{metas: [%{name: "u1", phx_ref: "ref"}]}}
-    assert DefaultPresence.fetch("topic", presences) == presences
+    assert DefaultPresence.fetch(config.topic, presences) == presences
   end
 
-  test "list/1 lists presences from tracker" do
-    assert MyPresence.list("topic") == %{}
-    assert MyPresence.track_presence(self(), "topic", "u1", %{name: "u1"}) == :ok
+  test "list/1 lists presences from tracker", config do
+    assert MyPresence.list(config.topic) == %{}
+    assert MyPresence.track_presence(self(), config.topic, "u1", %{name: "u1"}) == :ok
     assert %{"u1" => %{extra: "extra", metas: [%{name: "u1", phx_ref: _}]}} =
-           MyPresence.list("topic")
+           MyPresence.list(config.topic)
   end
 
-  test "handle_join and handle_leave broadcasts events with default fetched data", config do
-    pid = spawn(fn -> :timer.sleep(:infinity) end)
-    Phoenix.PubSub.subscribe(config.pubsub, self(), "topic")
-    {:ok, _pid} = DefaultPresence.start_link(pubsub_server: config.pubsub)
-    DefaultPresence.track_presence(pid, "topic", "u1", %{name: "u1"})
+  test "handle_join and handle_leave broadcasts events with default fetched data",
+    %{topic: topic} = config do
 
-    assert_receive %Broadcast{topic: "topic", event: "presence_join", payload: %{
+    pid = spawn(fn -> :timer.sleep(:infinity) end)
+    Phoenix.PubSub.subscribe(config.pubsub, self(), topic)
+    {:ok, _pid} = DefaultPresence.start_link(pubsub_server: config.pubsub)
+    DefaultPresence.track_presence(pid, topic, "u1", %{name: "u1"})
+
+    assert_receive %Broadcast{topic: ^topic, event: "presence_join", payload: %{
       "u1" => %{metas: [%{name: "u1", phx_ref: u1_ref}]}
     }}
     assert %{"u1" => %{metas: [%{name: "u1", phx_ref: ^u1_ref}]}} =
-           DefaultPresence.list("topic")
+           DefaultPresence.list(topic)
 
     Process.exit(pid, :kill)
-    assert_receive %Broadcast{topic: "topic", event: "presence_leave", payload: %{
+    assert_receive %Broadcast{topic: ^topic, event: "presence_leave", payload: %{
       "u1" => %{metas: [%{name: "u1", phx_ref: ^u1_ref}]}
     }}
-    assert DefaultPresence.list("topic") == %{}
+    assert DefaultPresence.list(topic) == %{}
   end
 
-  test "handle_join and handle_leave broadcasts events with custom fetched data", config do
-    pid = spawn(fn -> :timer.sleep(:infinity) end)
-    Phoenix.PubSub.subscribe(config.pubsub, self(), "topic")
-    MyPresence.track_presence(pid, "topic", "u1", %{name: "u1"})
+  test "handle_join and handle_leave broadcasts events with custom fetched data",
+    %{topic: topic} = config do
 
-    assert_receive %Broadcast{topic: "topic", event: "presence_join", payload: %{
+    pid = spawn(fn -> :timer.sleep(:infinity) end)
+    Phoenix.PubSub.subscribe(config.pubsub, self(), topic)
+    MyPresence.track_presence(pid, topic, "u1", %{name: "u1"})
+
+    assert_receive %Broadcast{topic: ^topic, event: "presence_join", payload: %{
       "u1" => %{extra: "extra", metas: [%{name: "u1", phx_ref: u1_ref}]}
     }}
     assert %{"u1" => %{extra: "extra", metas: [%{name: "u1", phx_ref: ^u1_ref}]}} =
-           MyPresence.list("topic")
+           MyPresence.list(topic)
 
     Process.exit(pid, :kill)
-    assert_receive %Broadcast{topic: "topic", event: "presence_leave", payload: %{
+    assert_receive %Broadcast{topic: ^topic, event: "presence_leave", payload: %{
       "u1" => %{extra: "extra", metas: [%{name: "u1", phx_ref: ^u1_ref}]}
     }}
-    assert MyPresence.list("topic") == %{}
+    assert MyPresence.list(topic) == %{}
   end
 
   test "list maintains join order when grouping", config do
     pid = spawn(fn -> :timer.sleep(:infinity) end)
     pid2 = spawn(fn -> :timer.sleep(:infinity) end)
-    Phoenix.PubSub.subscribe(config.pubsub, self(), "topic")
-    MyPresence.track_presence(pid, "topic", "u1", %{name: "1st"})
-    MyPresence.track_presence(pid2, "topic", "u1", %{name: "2nd"})
+    Phoenix.PubSub.subscribe(config.pubsub, self(), config.topic)
+    MyPresence.track_presence(pid, config.topic, "u1", %{name: "1st"})
+    MyPresence.track_presence(pid2, config.topic, "u1", %{name: "2nd"})
 
     assert %{"u1" => %{metas: [%{name: "1st"}, %{name: "2nd"}]}} =
-           MyPresence.list("topic")
+           MyPresence.list(config.topic)
   end
 end
