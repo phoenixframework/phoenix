a766e666aa1d2b9e62a061b323b8cd9677927982
aphillipo <andrewphillipo@yahoo.co.uk>
2016-02-29 01:03:23 +0000

    Ecto.Adapters.SQL.restart_test_transaction

--- installer/test/phoenix_new_test.exs
+++ installer/test/phoenix_new_test.exs
@@ -274,11 +274,11 @@ defmodule Mix.Tasks.Phoenix.NewTest do
       assert_file "custom_path/config/prod.secret.exs", ~r/Tds.Ecto/
 
       assert_file "custom_path/test/support/conn_case.ex",
-        ~r/Ecto.Adapters.SQL.restart_test_transaction/
+        ~r/Ecto.Adapters.SQL.Sandbox.checkout/
       assert_file "custom_path/test/support/channel_case.ex",
-        ~r/Ecto.Adapters.SQL.restart_test_transaction/
+        ~r/Ecto.Adapters.SQL.Sandbox.checkout/
       assert_file "custom_path/test/support/model_case.ex",
-        ~r/Ecto.Adapters.SQL.restart_test_transaction/
+        ~r/Ecto.Adapters.SQL.Sandbox.checkout/
     end
   end
 
@@ -305,11 +305,11 @@ defmodule Mix.Tasks.Phoenix.NewTest do
       end
 
       assert_file "custom_path/test/support/conn_case.ex",
-        ~r/Ecto.Adapters.SQL.restart_test_transaction/
+        ~r/Ecto.Adapters.SQL.Sandbox.checkout/
       assert_file "custom_path/test/support/channel_case.ex",
-        ~r/Ecto.Adapters.SQL.restart_test_transaction/
+        ~r/Ecto.Adapters.SQL.Sandbox.checkout/
       assert_file "custom_path/test/support/model_case.ex",
-        ~r/Ecto.Adapters.SQL.restart_test_transaction/
+        ~r/Ecto.Adapters.SQL.Sandbox.checkout/
     end
   end
 
