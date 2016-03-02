d31dd52e3733ad81b86f402a66f02e19482e657e
Chris McCord <chris@chrismccord.com>
2016-03-02 13:11:12 -0500

    Match on exit signal

--- test/mix/tasks/phoenix.digest_test.exs
+++ test/mix/tasks/phoenix.digest_test.exs
@@ -2,7 +2,7 @@ defmodule Mix.Tasks.Phoenix.DigestTest do
   use ExUnit.Case, async: true
 
   test "fails when the given paths are invalid" do
-    catch_exit do
+    {:shutdown, 1} = catch_exit do
       Mix.Tasks.Phoenix.Digest.run(["invalid_path"])
       assert_received {:mix_shell, :error, ["The input path \"invalid_path\" does not exist"]}
     end
