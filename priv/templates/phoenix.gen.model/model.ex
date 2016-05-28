fb9c601459083ecad2a90c8dc04fe78094f0bff3
GriffinMB <byatt.griffin@gmail.com>
2016-05-27 20:25:10 -0400

    Potential fix for issue 1605

--- priv/templates/phoenix.gen.model/model.ex
+++ priv/templates/phoenix.gen.model/model.ex
@@ -3,7 +3,7 @@ defmodule <%= module %> do
 
   schema <%= inspect plural %> do
 <%= for {k, _} <- attrs do %>    field <%= inspect k %>, <%= inspect types[k] %><%= schema_defaults[k] %>
-<% end %><%= for {k, _, m, _} <- assocs do %>    belongs_to <%= inspect k %>, <%= m %>
+<% end %><%= for {k, k_id, m, _} <- assocs do %>    belongs_to <%= inspect k %>, <%= m %><%= if !String.ends_with?(Atom.to_string(k_id), "_id") do %>, define_field: false, foreign_key: <%= inspect k %><% end %>
 <% end %>
     timestamps
   end
