7e490ea2565d96048e82bdf9f27c0d32295e3d37
Chris McCord <chris@chrismccord.com>
2016-06-16 13:43:56 -0400

    Fix up docs

--- installer/templates/new/config/prod.secret.exs
+++ installer/templates/new/config/prod.secret.exs
@@ -4,7 +4,7 @@ use Mix.Config
 # you likely want to automate and keep it away from
 # your version control system.
 #
-# You should most probably document the content of this
+# You should document the content of this
 # file or create a script for recreating it, since it's
 # kept out of version control and might be hard to recover
 # or recreate for your teammates (or you later on).
