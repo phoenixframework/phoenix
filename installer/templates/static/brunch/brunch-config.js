20483cf32e3c35587352d8146c62849a68f9c4fd
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2016-06-22 21:19:44 +1000

    Update brunch-config documentaion links

--- installer/templates/static/brunch/brunch-config.js
+++ installer/templates/static/brunch/brunch-config.js
@@ -5,14 +5,13 @@ exports.config = {
       joinTo: "js/app.js"
 
       // To use a separate vendor.js bundle, specify two files path
-      // https://github.com/brunch/brunch/blob/master/docs/config.md#files
+      // http://brunch.io/docs/config#-files-
       // joinTo: {
       //  "js/app.js": /^(web\/static\/js)/,
       //  "js/vendor.js": /^(web\/static\/vendor)|(deps)/
       // }
       //
       // To change the order of concatenation of files, explicitly mention here
-      // https://github.com/brunch/brunch/tree/master/docs#concatenation
       // order: {
       //   before: [
       //     "web/static/vendor/js/jquery-2.1.1.js",
