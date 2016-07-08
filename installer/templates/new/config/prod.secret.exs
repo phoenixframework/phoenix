ce4ce4bdc55976db8833bcc67db4a6a9bdce2120
Henrik Nyh <henrik@nyh.se>
2016-07-08 18:33:58 +0100

    Set host and secret_key_base from ENVs if provided
    Makes e.g. Heroku and Dokku deploys even easier.

--- installer/templates/new/config/prod.secret.exs
+++ installer/templates/new/config/prod.secret.exs
@@ -8,5 +8,9 @@ use Mix.Config
 # file or create a script for recreating it, since it's
 # kept out of version control and might be hard to recover
 # or recreate for your teammates (or you later on).
+#
+# Alternatively, you can use the provided environment variables
+# for configuration. Make sure to remove the secret values from
+# this file, and remove the file from .gitignore so you can commit it.
 config :<%= application_name %>, <%= application_module %>.Endpoint,
-  secret_key_base: "<%= prod_secret_key_base %>"
+  secret_key_base: (System.get_env("SECRET_KEY_BASE") || "<%= prod_secret_key_base %>")
