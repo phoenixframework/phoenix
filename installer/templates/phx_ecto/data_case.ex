6d723e96f7cabbb3c452e431889e82528738a300
Chris McCord <chris@chrismccord.com>
2016-09-29 00:18:42 -0400

    Remove errors_on

--- installer/templates/phx_ecto/data_case.ex
+++ installer/templates/phx_ecto/data_case.ex
@@ -34,32 +34,4 @@ defmodule <%= app_module %>.DataCase do
 
     :ok
   end
-
-  @doc """
-  Helper for returning list of errors in a struct when given certain data.
-
-  ## Examples
-
-  Given a User schema that lists `:name` as a required field and validates
-  `:password` to be safe, it would return:
-
-      iex> errors_on(%User{}, %{password: "password"})
-      [password: "is unsafe", name: "is blank"]
-
-  You could then write your assertion like:
-
-      assert {:password, "is unsafe"} in errors_on(%User{}, %{password: "password"})
-
-  You can also create the changeset manually and retrieve the errors
-  field directly:
-
-      iex> changeset = User.changeset(%User{}, password: "password")
-      iex> {:password, "is unsafe"} in changeset.errors
-      true
-  """
-  def errors_on(struct, data) do
-    struct.__struct__.changeset(struct, data)
-    |> Ecto.Changeset.traverse_errors(&<%= app_module %>.Web.ErrorHelpers.translate_error/1)
-    |> Enum.flat_map(fn {key, errors} -> for msg <- errors, do: {key, msg} end)
-  end
 end
