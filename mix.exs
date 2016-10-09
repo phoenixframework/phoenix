1a615aa3760e751b5cfba471ef0099b446168d70
Wendy Smoak <wsmoak@gmail.com>
2016-10-09 18:01:01 -0400

    Include Phoenix guides with framework API docs
    - Declare dependency on phoenix_guides (in docs environment only)
    - Add extras page for each guide with custom group
    - Titles are taken from new h1 elements on guide pages
    - Make Overview the main page of the framework docs
    - Set preferred environment for `docs` task so you don't have to type `MIX_ENV=docs`, just `mix docs` will work
    
    See discussion on https://github.com/phoenixframework/phoenix_guides/issues/391

--- mix.exs
+++ mix.exs
@@ -9,6 +9,7 @@ defmodule Phoenix.Mixfile do
      elixir: "~> 1.3",
      deps: deps(),
      package: package(),
+     preferred_cli_env: [docs: :docs],
 
      # Because we define protocols on the fly to test
      # Phoenix.Param, we need to disable consolidation
@@ -17,7 +18,12 @@ defmodule Phoenix.Mixfile do
      xref: [exclude: [Ecto.Type]],
 
      name: "Phoenix",
-     docs: [source_ref: "v#{@version}", main: "Phoenix", logo: "logo.png"],
+     docs: [source_ref: "v#{@version}",
+            main: "overview",
+            logo: "logo.png",
+            extra_section: "GUIDES",
+            assets: "deps/phoenix_guides/images",
+            extras: extras()],
      source_url: "https://github.com/phoenixframework/phoenix",
      homepage_url: "http://www.phoenixframework.org",
      description: """
@@ -46,8 +52,9 @@ defmodule Phoenix.Mixfile do
      {:gettext, "~> 0.8", only: :test},
 
      # Docs dependencies
-     {:ex_doc, "~> 0.12", only: :docs},
+     {:ex_doc, "~> 0.14", only: :docs},
      {:inch_ex, "~> 0.2", only: :docs},
+     {:phoenix_guides, git: "https://github.com/phoenixframework/phoenix_guides.git", compile: false, app: false, only: :docs},
 
      # Test dependencies
      {:phoenix_html, "~> 2.6", only: :test},
@@ -61,5 +68,49 @@ defmodule Phoenix.Mixfile do
      links: %{github: "https://github.com/phoenixframework/phoenix"},
      files: ~w(lib priv web) ++
             ~w(brunch-config.js CHANGELOG.md LICENSE.md mix.exs package.json README.md)]
+  end
+
+  defp extras do
+    [
+      "deps/phoenix_guides/docs/introduction/overview.md": [group: "Introduction"],
+      "deps/phoenix_guides/docs/introduction/installation.md": [group: "Introduction"],
+      "deps/phoenix_guides/docs/introduction/learning.md": [group: "Introduction"],
+      "deps/phoenix_guides/docs/introduction/community.md": [group: "Introduction"],
+
+      "deps/phoenix_guides/docs/up_and_running.md": [group: "Guides"],
+      "deps/phoenix_guides/docs/adding_pages.md": [group: "Guides"],
+      "deps/phoenix_guides/docs/routing.md": [group: "Guides"],
+      "deps/phoenix_guides/docs/plug.md": [group: "Guides"],
+      "deps/phoenix_guides/docs/controllers.md": [group: "Guides"],
+      "deps/phoenix_guides/docs/views.md": [group: "Guides"],
+      "deps/phoenix_guides/docs/templates.md": [group: "Guides"],
+      "deps/phoenix_guides/docs/channels.md": [group: "Guides"],
+      "deps/phoenix_guides/docs/ecto_models.md": [group: "Guides"],
+
+      "deps/phoenix_guides/docs/testing/testing.md": [group: "Testing"],
+      "deps/phoenix_guides/docs/testing/testing_models.md": [group: "Testing"],
+      "deps/phoenix_guides/docs/testing/testing_controllers.md": [group: "Testing"],
+      "deps/phoenix_guides/docs/testing/testing_views.md": [group: "Testing"],
+      "deps/phoenix_guides/docs/testing/testing_channels.md": [group: "Testing"],
+
+      "deps/phoenix_guides/docs/deployment/deployment.md": [group: "Deployment"],
+      "deps/phoenix_guides/docs/deployment/heroku.md": [group: "Deployment"],
+      "deps/phoenix_guides/docs/deployment/exrm_releases.md": [group: "Deployment"],
+
+      "deps/phoenix_guides/docs/bonus_guides/upgrading_phoenix.md": [group: "Bonus Guides"],
+      "deps/phoenix_guides/docs/bonus_guides/custom_primary_key.md": [group: "Bonus Guides"],
+      "deps/phoenix_guides/docs/bonus_guides/using_mysql.md": [group: "Bonus Guides"],
+      "deps/phoenix_guides/docs/bonus_guides/static_assets.md": [group: "Bonus Guides"],
+      "deps/phoenix_guides/docs/bonus_guides/file_uploads.md": [group: "Bonus Guides"],
+      "deps/phoenix_guides/docs/bonus_guides/sending_email_with_mailgun.md": [group: "Bonus Guides"],
+      "deps/phoenix_guides/docs/bonus_guides/sending_email_with_smtp.md": [group: "Bonus Guides"],
+      "deps/phoenix_guides/docs/bonus_guides/sessions.md": [group: "Bonus Guides"],
+      "deps/phoenix_guides/docs/bonus_guides/custom_errors.md": [group: "Bonus Guides"],
+      "deps/phoenix_guides/docs/bonus_guides/using_ssl.md": [group: "Bonus Guides"],
+      "deps/phoenix_guides/docs/bonus_guides/phoenix_behind_proxy.md": [group: "Bonus Guides"],
+      "deps/phoenix_guides/docs/bonus_guides/config.md": [group: "Bonus Guides"],
+      "deps/phoenix_guides/docs/bonus_guides/learning_elixir.md": [group: "Bonus Guides"],
+      "deps/phoenix_guides/docs/bonus_guides/seeding_data.md": [group: "Bonus Guides"],
+    ]
   end
 end
