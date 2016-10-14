9f11abce820102aa9dbcce8948fc4e54d80f14c0
Szetty <szederjesiarnold@gmail.com>
2016-10-14 22:07:59 +0300

    #1936 Take into consideration javascript source files' contents Change source mapping url to the digested file Add tests for this Modify test file, and add map file

--- test/phoenix/digester_test.exs
+++ test/phoenix/digester_test.exs
@@ -3,6 +3,7 @@ defmodule Phoenix.DigesterTest do
 
   @output_path Path.join("tmp", "phoenix_digest")
   @fake_now 32132173
+  @hash_regex ~S"[a-fA-F\d]{32}"
 
   setup do
     File.rm_rf!(@output_path)
@@ -26,15 +27,15 @@ defmodule Phoenix.DigesterTest do
       assert "css/app.css" in output_files
       assert "css/app.css.gz" in output_files
       assert "manifest.json" in output_files
-      assert Enum.any?(output_files, &(String.match?(&1, ~r/(phoenix-[a-fA-F\d]{32}\.png)/)))
-      refute Enum.any?(output_files, &(String.match?(&1, ~r/(phoenix-[a-fA-F\d]{32}\.png\.gz)/)))
+      assert Enum.any?(output_files, &(String.match?(&1, ~r/(phoenix-#{@hash_regex}\.png)/)))
+      refute Enum.any?(output_files, &(String.match?(&1, ~r/(phoenix-#{@hash_regex}\.png\.gz)/)))
 
       json =
         Path.join(@output_path, "manifest.json")
         |> File.read!()
         |> Poison.decode!()
 
-      assert json["latest"]["phoenix.png"] =~ ~r"phoenix-[a-fA-F\d]{32}.png"
+      assert json["latest"]["phoenix.png"] =~ ~r"phoenix-#{@hash_regex}.png"
       assert json["version"] == 1
     end
 
@@ -57,11 +58,11 @@ defmodule Phoenix.DigesterTest do
       assert json["digests"]["foo-d978852bea6530fcd197b5445ed008fd.css"]["logical_path"] == "foo.css"
 
       # Add new entries
-      key = Enum.find(Map.keys(json["digests"]), &(&1 =~ ~r"phoenix-[a-fA-F\d]{32}.png"))
+      key = Enum.find(Map.keys(json["digests"]), &(&1 =~ ~r"phoenix-#{@hash_regex}.png"))
       assert json["digests"][key]["logical_path"] == "phoenix.png"
       assert is_integer(json["digests"][key]["mtime"])
       assert json["digests"][key]["size"] == 13900
-      assert json["digests"][key]["digest"] =~ ~r"[a-fA-F\d]{32}"
+      assert json["digests"][key]["digest"] =~ ~r"#{@hash_regex}"
       assert json["version"] == 1
     end
 
@@ -74,14 +75,14 @@ defmodule Phoenix.DigesterTest do
       assert "static/phoenix.png" in output_files
       refute "static/phoenix.png.gz" in output_files
       assert "manifest.json" in output_files
-      assert Enum.any?(output_files, &(String.match?(&1, ~r/(phoenix-[a-fA-F\d]{32}\.png)/)))
-      refute Enum.any?(output_files, &(String.match?(&1, ~r/(phoenix-[a-fA-F\d]{32}\.png\.gz)/)))
+      assert Enum.any?(output_files, &(String.match?(&1, ~r/(phoenix-#{@hash_regex}\.png)/)))
+      refute Enum.any?(output_files, &(String.match?(&1, ~r/(phoenix-#{@hash_regex}\.png\.gz)/)))
 
       json =
         Path.join(@output_path, "manifest.json")
         |> File.read!()
         |> Poison.decode!()
-      assert json["latest"]["static/phoenix.png"] =~ ~r"static/phoenix-[a-fA-F\d]{32}\.png"
+      assert json["latest"]["static/phoenix.png"] =~ ~r"static/phoenix-#{@hash_regex}\.png"
     end
 
     test "doesn't duplicate files when digesting and compressing twice" do
@@ -99,7 +100,7 @@ defmodule Phoenix.DigesterTest do
 
       refute "file.js.gz.gz" in output_files
       refute "manifest.json.gz" in output_files
-      refute Enum.any?(output_files, & &1 =~ ~r/file-[a-fA-F\d]{32}.[\w|\d]*.[-[a-fA-F\d]{32}/)
+      refute Enum.any?(output_files, & &1 =~ ~r/file-#{@hash_regex}.[\w|\d]*.[-#{@hash_regex}/)
     end
 
     test "digests only absolute and relative asset paths found within stylesheets" do
@@ -108,7 +109,7 @@ defmodule Phoenix.DigesterTest do
 
       digested_css_filename =
         assets_files(@output_path)
-        |> Enum.find(&(&1 =~ ~r"app-[a-fA-F\d]{32}.css"))
+        |> Enum.find(&(&1 =~ ~r"app-#{@hash_regex}.css"))
 
       digested_css =
         Path.join(@output_path, digested_css_filename)
@@ -116,11 +117,31 @@ defmodule Phoenix.DigesterTest do
 
       refute digested_css =~ ~r"/phoenix\.png"
       refute digested_css =~ ~r"\.\./images/relative\.png"
-      assert digested_css =~ ~r"/phoenix-[a-fA-F\d]{32}\.png\?vsn=d"
-      assert digested_css =~ ~r"\.\./images/relative-[a-fA-F\d]{32}\.png\?vsn=d"
+      assert digested_css =~ ~r"/phoenix-#{@hash_regex}\.png\?vsn=d"
+      assert digested_css =~ ~r"\.\./images/relative-#{@hash_regex}\.png\?vsn=d"
 
-      refute digested_css =~ ~r"http://www.phoenixframework.org/absolute-[a-fA-F\d]{32}.png"
+      refute digested_css =~ ~r"http://www.phoenixframework.org/absolute-#{@hash_regex}.png"
       assert digested_css =~ ~r"http://www.phoenixframework.org/absolute.png"
+    end
+
+    test "digests sourceMappingURL asset paths found within javascript source files" do
+      input_path = "test/fixtures/digest/priv/static/"
+      assert :ok = Phoenix.Digester.compile(input_path, @output_path)
+
+      digested_js_map_filename =
+        assets_files(@output_path)
+        |> Enum.find(&(&1 =~ ~r"app.js-#{@hash_regex}.map"))
+
+      digested_js_filename =
+        assets_files(@output_path)
+        |> Enum.find(&(&1 =~ ~r"app-#{@hash_regex}.js"))
+
+      digested_js =
+        Path.join(@output_path, digested_js_filename)
+        |> File.read!()
+
+      refute digested_js =~ ~r"app.js.map"
+      assert digested_js =~ ~r"#{digested_js_map_filename}$"
     end
 
     test "does not digest assets within undigested files" do
@@ -133,8 +154,8 @@ defmodule Phoenix.DigesterTest do
 
       assert undigested_css =~ ~r"/phoenix\.png"
       assert undigested_css =~ ~r"\.\./images/relative\.png"
-      refute undigested_css =~ ~r"/phoenix-[a-fA-F\d]{32}\.png"
-      refute undigested_css =~ ~r"\.\./images/relative-[a-fA-F\d]{32}\.png"
+      refute undigested_css =~ ~r"/phoenix-#{@hash_regex}\.png"
+      refute undigested_css =~ ~r"\.\./images/relative-#{@hash_regex}\.png"
     end
   end
 
