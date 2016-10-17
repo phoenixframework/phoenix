0f0dcead6c0ca686d9bc64c3ef1d6ae76d672615
Philip Giuliani <philip.giuliani@keepinmind.info>
2016-10-17 16:35:34 +0200

    Also remove phoenix_html from assets

--- installer/templates/assets/brunch/app.js
+++ installer/templates/assets/brunch/app.js
@@ -11,7 +11,7 @@
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
-import "phoenix_html"
+<%= if html do %>import "phoenix_html"<% end %>
 
 // Import local files
 //
