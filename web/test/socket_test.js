e45f464827edd9ae8c47f12b6f55ac4d99feec1a
Ross Kaffenberger <rosskaff@gmail.com>
2016-11-11 16:27:28 -0500

    Add mocha tests for Socket connect()

--- web/test/socket_test.js
+++ web/test/socket_test.js
@@ -1,5 +1,7 @@
 import assert from "assert"
+
 import jsdom from "jsdom"
+import { WebSocket, Server as WebSocketServer } from "mock-socket"
 
 import {Socket} from "../static/js/phoenix"
 
@@ -53,3 +55,52 @@ describe("endpointURL", () => {
   })
 })
 
+describe("connect with WebSocket", () => {
+  let mockServer
+
+  before(() => {
+    jsdom.changeURL(window, "http://example.com/");
+    mockServer = new WebSocketServer('wss://example.com/')
+  })
+
+  after(() => {
+    mockServer.stop()
+  })
+
+  describe("establishes websocket connection with endpoint", () => {
+    const mockServer = new WebSocketServer('wss://example.com/')
+
+    socket = new Socket("/socket")
+    socket.connect()
+
+    let conn = socket.conn
+    assert.ok(conn instanceof WebSocket)
+    assert.equal(conn.url, socket.endPointURL())
+  })
+
+  describe("sets callbacks for connection", () => {
+    socket = new Socket("/socket")
+    let opens = 0
+    socket.onOpen(() => ++opens)
+    let closes = 0
+    socket.onClose(() => ++closes)
+    let lastError
+    socket.onError((error) => lastError = error)
+    let lastMessage
+    socket.onMessage((message) => lastMessage = message.payload)
+
+    socket.connect()
+
+    socket.conn.onopen[0]()
+    assert.equal(opens, 1)
+
+    socket.conn.onclose[0]()
+    assert.equal(closes, 1)
+
+    socket.conn.onerror[0]("error")
+    assert.equal(lastError, "error")
+
+    socket.conn.onmessage[0]({data: '{"topic":"topic","event":"event","payload":"message","status":"ok"}'})
+    assert.equal(lastMessage, "message")
+  })
+})
