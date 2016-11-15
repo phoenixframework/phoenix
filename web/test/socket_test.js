1798137672af12c1178c0ccdce31543a178e6cda
Ross Kaffenberger <rosskaff@gmail.com>
2016-11-14 20:37:04 -0500

    Add tests for Socket connect() with LongPoll

--- web/test/socket_test.js
+++ web/test/socket_test.js
@@ -2,8 +2,9 @@ import assert from "assert"
 
 import jsdom from "jsdom"
 import { WebSocket, Server as WebSocketServer } from "mock-socket"
+import { XMLHttpRequest } from "xmlhttprequest"
 
-import {Socket} from "../static/js/phoenix"
+import { Socket, LongPoll } from "../static/js/phoenix"
 
 let socket
 
@@ -65,6 +66,7 @@ describe("connect with WebSocket", () => {
 
   after(() => {
     mockServer.stop()
+    window.WebSocket = null
   })
 
   it("establishes websocket connection with endpoint", () => {
@@ -99,6 +101,63 @@ describe("connect with WebSocket", () => {
     assert.equal(lastError, "error")
 
     socket.conn.onmessage[0]({data: '{"topic":"topic","event":"event","payload":"message","status":"ok"}'})
+    assert.equal(lastMessage, "message")
+  })
+
+  it("is idempotent", () => {
+    socket = new Socket("/socket")
+    socket.connect()
+
+    let conn = socket.conn
+
+    socket.connect()
+
+    assert.deepStrictEqual(conn, socket.conn)
+  })
+})
+
+describe("connect with long poll", () => {
+  before(() => {
+    window.XMLHttpRequest = XMLHttpRequest
+  })
+
+  after(() => {
+    window.XMLHttpRequest = null
+  })
+
+  it("establishes long poll connection with endpoint", () => {
+    socket = new Socket("/socket")
+    socket.connect()
+
+    let conn = socket.conn
+    assert.ok(conn instanceof LongPoll)
+    assert.equal(conn.pollEndpoint, "http://example.com/socket/longpoll?vsn=1.0.0")
+    assert.equal(conn.timeout, 20000)
+  })
+
+  it("sets callbacks for connection", () => {
+    socket = new Socket("/socket")
+    let opens = 0
+    socket.onOpen(() => ++opens)
+    let closes = 0
+    socket.onClose(() => ++closes)
+    let lastError
+    socket.onError((error) => lastError = error)
+    let lastMessage
+    socket.onMessage((message) => lastMessage = message.payload)
+
+    socket.connect()
+
+    socket.conn.onopen()
+    assert.equal(opens, 1)
+
+    socket.conn.onclose()
+    assert.equal(closes, 1)
+
+    socket.conn.onerror("error")
+    assert.equal(lastError, "error")
+
+    socket.conn.onmessage({data: '{"topic":"topic","event":"event","payload":"message","status":"ok"}'})
     assert.equal(lastMessage, "message")
   })
 
