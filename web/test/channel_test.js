5df7c93e037dfedd13d90178a3f5119566a7f852
Ross Kaffenberger <rosskaff@gmail.com>
2016-12-23 18:05:21 -0500

    Add javascript test for Channel joinPush success and timeout

--- web/test/channel_test.js
+++ web/test/channel_test.js
@@ -38,9 +38,12 @@ describe("constructor", () => {
 })
 
 describe("join", () => {
+  const defaultTimeout = 1000
+  const defaultRef = 1
+
   beforeEach(() => {
-    socket = new Socket("/socket", { timeout: 1000 })
-    sinon.stub(socket, "makeRef", () => { return 1 })
+    socket = new Socket("/socket", { timeout: defaultTimeout })
+    sinon.stub(socket, "makeRef", () => { return defaultRef })
 
     channel = new Channel("topic", { one: "two" }, socket)
   })
@@ -65,7 +68,7 @@ describe("join", () => {
     assert.throws(() => channel.join(), /tried to join multiple times/)
   })
 
-  it("triggers socket push", () => {
+  it("triggers socket push with channel params", () => {
     const spy = sinon.spy(socket, "push")
 
     channel.join()
@@ -75,7 +78,332 @@ describe("join", () => {
       topic: "topic",
       event: "phx_join",
       payload: { one: "two" },
-      ref: 1,
+      ref: defaultRef,
     }))
+  })
+
+  it("can set timeout on joinPush", () => {
+    const newTimeout = 2000
+    const joinPush = channel.joinPush
+
+    assert.equal(joinPush.timeout, defaultTimeout)
+
+    channel.join(newTimeout)
+
+    assert.equal(joinPush.timeout, newTimeout)
+  })
+
+  it("sets joinPush refEventName", () => {
+    const joinPush = channel.joinPush
+
+    assert.equal(joinPush.refEvent, null)
+
+    channel.join()
+
+    assert.equal(joinPush.refEvent, "chan_reply_1")
+  })
+})
+
+describe("joinPush", () => {
+  let joinPush, clock, response
+
+  const helpers = {
+    receiveOk: () => {
+      clock.tick(channel.timeout / 2) // before timeout
+      return channel.trigger("chan_reply_1", { status: "ok", response }, 1)
+    },
+
+    receiveTimeout: () => {
+      clock.tick(channel.timeout * 2) // after timeout
+    },
+
+    receiveError: () => {
+      clock.tick(channel.timeout / 2) // before timeout
+      return channel.trigger("chan_reply_1", { status: "error", response }, 1)
+    },
+
+    getBindings: (event) => {
+      return channel.bindings.filter(bind => bind.event === event )
+    }
+  }
+
+  beforeEach(() => {
+    clock = sinon.useFakeTimers()
+
+    socket = new Socket("/socket", { timeout: 1000 })
+    sinon.stub(socket, "makeRef", () => { return 1 })
+
+    channel = new Channel("topic", { one: "two" }, socket)
+    joinPush = channel.joinPush
+
+    channel.join()
+  })
+
+  afterEach(() => {
+    clock.restore()
+  })
+
+  describe("receives 'ok'", () => {
+    beforeEach(() => {
+      response = { chan: "reply" }
+    })
+
+    it("triggers receive('ok') callback after ok response", () => {
+      const spyOk = sinon.spy()
+
+      joinPush.receive("ok", spyOk)
+
+      helpers.receiveOk()
+
+      assert.ok(spyOk.calledOnce)
+    })
+
+    it("triggers receive('ok') callback if ok response already received", () => {
+      const spyOk = sinon.spy()
+
+      helpers.receiveOk()
+
+      joinPush.receive("ok", spyOk)
+
+      assert.ok(spyOk.calledOnce)
+    })
+
+    it("does not trigger other receive callbacks after ok response", () => {
+      const spyError = sinon.spy()
+      const spyTimeout = sinon.spy()
+
+      joinPush
+        .receive("error", spyError)
+        .receive("timeout", spyTimeout)
+
+      helpers.receiveOk()
+      clock.tick(channel.timeout * 2) // attempt timeout
+
+      assert.ok(!spyError.called)
+      assert.ok(!spyTimeout.called)
+    })
+
+    it("clears timeoutTimer", () => {
+      assert.ok(joinPush.timeoutTimer)
+
+      helpers.receiveOk()
+
+      assert.equal(joinPush.timeoutTimer, null)
+    })
+
+    it("sets receivedResp", () => {
+      assert.equal(joinPush.receivedResp, null)
+
+      helpers.receiveOk()
+
+      assert.deepEqual(joinPush.receivedResp, { status: "ok", response })
+    })
+
+    it("removes channel bindings", () => {
+      let bindings = helpers.getBindings("chan_reply_1")
+      assert.equal(bindings.length, 1)
+
+      helpers.receiveOk()
+
+      bindings = helpers.getBindings("chan_reply_1")
+      assert.equal(bindings.length, 0)
+    })
+
+    it("sets channel state to joined", () => {
+      helpers.receiveOk()
+
+      assert.equal(channel.state, "joined")
+    })
+
+    it("resets channel rejoinTimer", () => {
+      assert.ok(channel.rejoinTimer)
+
+      const spy = sinon.spy(channel.rejoinTimer, "reset")
+
+      helpers.receiveOk()
+
+      assert.ok(spy.calledOnce)
+    })
+
+    it("sends and empties channel's buffered pushEvents", () => {
+      const pushEvent = { send: () => {} }
+      const spy = sinon.spy(pushEvent, "send")
+
+      channel.pushBuffer.push(pushEvent)
+
+      helpers.receiveOk()
+
+      assert.ok(spy.calledOnce)
+      assert.equal(channel.pushBuffer.length, 0)
+    })
+  })
+
+  describe("receives 'timeout'", () => {
+    it("sets channed state to errored", () => {
+      helpers.receiveTimeout()
+
+      assert.equal(channel.state, "errored")
+    })
+
+    it("triggers receive('timeout') callback after ok response", () => {
+      const spyTimeout = sinon.spy()
+
+      joinPush.receive("timeout", spyTimeout)
+
+      helpers.receiveTimeout()
+
+      assert.ok(spyTimeout.calledOnce)
+    })
+
+    it("triggers receive('timeout') callback if already timed out", () => {
+      const spyTimeout = sinon.spy()
+
+      helpers.receiveTimeout()
+
+      joinPush.receive("timeout", spyTimeout)
+
+      assert.ok(spyTimeout.calledOnce)
+    })
+
+    it.skip("does not trigger other receive callbacks after ok response", () => {
+      const spyOk = sinon.spy()
+      const spyError = sinon.spy()
+
+      joinPush
+        .receive("ok", spyOk)
+        .receive("error", spyError)
+
+      helpers.receiveTimeout()
+      helpers.receiveOk()
+
+      // TODO should this fail?
+      assert.ok(!spyOk.called)
+      assert.ok(!spyError.called)
+    })
+
+    it("schedules rejoinTimer timeout", () => {
+      assert.ok(channel.rejoinTimer)
+
+      const spy = sinon.spy(channel.rejoinTimer, "scheduleTimeout")
+
+      helpers.receiveTimeout()
+
+      assert.ok(spy.called) // TODO why called multiple times?
+    })
+
+    it("cannot send after timeout", () => {
+      const spy = sinon.spy(socket, "push")
+
+      helpers.receiveTimeout()
+
+      joinPush.send()
+
+      assert.ok(!spy.called)
+    })
+
+    it("sets receivedResp", () => {
+      assert.equal(joinPush.receivedResp, null)
+
+      helpers.receiveTimeout()
+
+      assert.deepEqual(joinPush.receivedResp, { status: "timeout", response: {} })
+    })
+  })
+
+  describe("receives 'error'", () => {
+    beforeEach(() => {
+      response = { chan: "fail" }
+    })
+
+    it("triggers receive('error') callback after error response", () => {
+      const spyError = sinon.spy()
+
+      joinPush.receive("error", spyError)
+
+      helpers.receiveError()
+
+      assert.ok(spyError.calledOnce)
+    })
+
+    it("triggers receive('error') callback if error response already received", () => {
+      const spyError = sinon.spy()
+
+      helpers.receiveError()
+
+      joinPush.receive("error", spyError)
+
+      assert.ok(spyError.calledOnce)
+    })
+
+    it.skip("does not trigger other receive callbacks after ok response", () => {
+      const spyOk = sinon.spy()
+      const spyTimeout = sinon.spy()
+
+      joinPush
+        .receive("ok", spyOk)
+        .receive("timeout", spyTimeout)
+
+      helpers.receiveOk()
+      clock.tick(channel.timeout * 2) // attempt timeout
+
+      // TODO should this fail?
+      assert.ok(!spyOk.called)
+      assert.ok(!spyTimeout.called)
+    })
+
+    it("clears timeoutTimer", () => {
+      assert.ok(joinPush.timeoutTimer)
+
+      helpers.receiveError()
+
+      assert.equal(joinPush.timeoutTimer, null)
+    })
+
+    it("sets receivedResp", () => {
+      assert.equal(joinPush.receivedResp, null)
+
+      helpers.receiveError()
+
+      assert.deepEqual(joinPush.receivedResp, { status: "error", response })
+    })
+
+    it("removes channel bindings", () => {
+      let bindings = helpers.getBindings("chan_reply_1")
+      assert.equal(bindings.length, 1)
+
+      helpers.receiveError()
+
+      bindings = helpers.getBindings("chan_reply_1")
+      assert.equal(bindings.length, 0)
+    })
+
+    it("does not set channel state to joined", () => {
+      helpers.receiveError()
+
+      assert.equal(channel.state, "joining")
+    })
+
+    it.skip("does not reset channel rejoinTimer", () => {
+      assert.ok(channel.rejoinTimer)
+
+      const spy = sinon.spy(channel.rejoinTimer, "reset")
+
+      helpers.receiveError()
+
+      assert.ok(!spy.called)
+      assert.ok(channel.rejoinTimer)
+    })
+
+    it("does not trigger channel's buffered pushEvents", () => {
+      const pushEvent = { send: () => {} }
+      const spy = sinon.spy(pushEvent, "send")
+
+      channel.pushBuffer.push(pushEvent)
+
+      helpers.receiveError()
+
+      assert.ok(!spy.called)
+      assert.equal(channel.pushBuffer.length, 1)
+    })
   })
 })
