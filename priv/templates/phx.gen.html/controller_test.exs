8a3165dd20262fd459814cb22de1087561886022
Chris McCord <chris@chrismccord.com>
2017-02-01 01:02:59 -0500

    Fix modules

--- priv/templates/phx.gen.html/controller_test.exs
+++ priv/templates/phx.gen.html/controller_test.exs
@@ -1,7 +1,7 @@
-defmodule <%= schema.module %>ControllerTest do
-  use <%= context.base_module %>.ConnCase
+defmodule <%= inspect schema.module %>ControllerTest do
+  use <%= inspect context.base_module %>.ConnCase
 
-  alias <%= schema.module %>
+  alias <%= inspect schema.module %>
 
   test "lists all entries on index", %{conn: conn} do
     conn = get conn, <%= schema.singular %>_path(conn, :index)
