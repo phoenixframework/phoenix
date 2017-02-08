75af1c214f6ed173da10331fdcc0a1e49cfad98c
Chris McCord <chris@chrismccord.com>
2017-02-08 12:48:06 -0500

    Document gen.html and gen.schem and add table option

--- lib/mix/tasks/phx.gen.html.ex
+++ lib/mix/tasks/phx.gen.html.ex
@@ -1,21 +1,49 @@
 defmodule Mix.Tasks.Phx.Gen.Html do
+  @shortdoc "Generates controller, views, and bounded context for an HTML resource"
+
+  @moduledoc """
+  Generates controller, views, and bounded context for an HTML resource.
+
+      mix phx.gen.html Accounts User users name:string age:integer
+
+  The first argument is the context name followed by
+  the schema module and its plural name (used for resources and schema).
+
+  The above generated resource will contain:
+
+    * a context module in lib/accounts.ex, serving as the API boundary
+      to the resource
+    * a schema in lib/accounts/user.ex, with an `accounts_users` table
+    * a view in lib/web/views/user_view.ex
+    * a controller in lib/web/controllers/user_controller.ex
+    * a migration file for the repository
+    * default CRUD templates in lib/web/templates/user
+    * test files for generated context and controller features
+
+
+  ## Schema table name
+
+  By deault, the schema table name will be the plural name, namespaced by the
+  context module name. You can customize this value by providing the `--table`
+  option to the generator.
+
+  Read the documentation for `phx.gen.schema` for more information on attributes
+  and supported options.
+  """
   use Mix.Task
 
   alias Mix.Phoenix.Context
   alias Mix.Tasks.Phx.Gen
 
-  @shortdoc "TODO"
-
-  @moduledoc """
-  TODO
-  """
-
   def run(args) do
-    switches = [binary_id: :boolean, model: :boolean]
+    switches = [binary_id: :boolean, model: :boolean, table: :string]
     {opts, parsed, _} = OptionParser.parse(args, switches: switches)
-    [context_name, schema_name | schema_args] = validate_args!(parsed)
+    [context_name, schema_name, plural | schema_args] = validate_args!(parsed)
 
-    schema = Gen.Schema.build([inspect(Module.concat(context_name, schema_name)) | schema_args])
+    table = Keyword.get(opts, :table, Phoenix.Naming.underscore(context_name) <> "_#{plural}")
+    schema_module = inspect(Module.concat(context_name, schema_name))
+
+    schema = Gen.Schema.build([schema_module, plural | schema_args] ++ ["--table", table])
     context = Context.new(context_name, schema, opts)
     Mix.Phoenix.check_module_name_availability!(context.module)
     binding = [context: context, schema: schema]
--- priv/templates/phx.gen.html/schema.ex
+++ priv/templates/phx.gen.html/schema.ex
@@ -1,7 +1,7 @@
 defmodule <%= inspect schema.module %> do
   use Ecto.Schema
 
-  schema <%= inspect schema.plural %> do
+  schema <%= inspect schema.table %> do
 <%= for {k, _} <- schema.attrs do %>    field <%= inspect k %>, <%= inspect schema.types[k] %><%= schema.defaults[k] %>
 <% end %><%= for {k, _, m, _} <- schema.assocs do %>    belongs_to <%= inspect k %>, <%= m %><%= if(String.ends_with?(inspect(k), "_id"), do: "", else: ", foreign_key: " <> inspect(k) <> "_id") %>
 <% end %>
