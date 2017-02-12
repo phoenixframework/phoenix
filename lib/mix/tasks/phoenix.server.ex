17c0bdb53ce5c87048d27f5b19e37e964272ecea
José Valim <jose.valim@gmail.com>
2017-02-12 10:17:53 +0100

    Always run Mix.Tasks.Run in phoenix.server

--- lib/mix/tasks/phoenix.server.ex
+++ lib/mix/tasks/phoenix.server.ex
@@ -20,7 +20,7 @@ defmodule Mix.Tasks.Phoenix.Server do
   """
   def run(args) do
     Application.put_env(:phoenix, :serve_endpoints, true, persistent: true)
-    Mix.Task.run "run", run_args() ++ args
+    Mix.Tasks.Run.run run_args() ++ args
   end
 
   defp run_args do
