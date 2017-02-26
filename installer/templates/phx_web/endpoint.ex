55bc2d0fcb531e6a0d8ab907a6d98ac5c7da32ee
José Valim <jose.valim@plataformatec.com.br>
2017-02-26 23:32:19 +0100

    Update endpoint docs

--- installer/templates/phx_web/endpoint.ex
+++ installer/templates/phx_web/endpoint.ex
@@ -43,9 +43,12 @@ defmodule <%= endpoint_module %> do
   @doc """
   Dynamically loads configuration from the system environment
   on startup.
+
+  It receives the endpoint configuration from the config files
+  and must return the updated configuration.
   """
-  def load_from_system_env(opts) do
+  def load_from_system_env(config) do
     port = System.get_env("PORT") || raise "expected the PORT environment variable to be set"
-    {:ok, Keyword.put(opts, :http, [:inet6, port: port])}
+    {:ok, Keyword.put(config, :http, [:inet6, port: port])}
   end
 end
