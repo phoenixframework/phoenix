d414f631908c95908f49ea368c26d9226f1d1295
Wojtek Mach <wojtek@wojtekmach.pl>
2017-03-02 20:51:44 +0100

    Minor docs update

--- installer/templates/phx_ecto/data_case.ex
+++ installer/templates/phx_ecto/data_case.ex
@@ -38,7 +38,7 @@ defmodule <%= app_module %>.DataCase do
   @doc """
   A helper that transform changeset errors to a map of messages.
 
-      changeset = Blog.create_user(%{password: "short"})
+      changeset = Accounts.create_user(%{password: "short"})
       assert "password is too short" in errors_on(changeset).password
       assert %{password: ["password is too short"]} = errors_on(changeset)
 
