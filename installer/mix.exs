9a01619150671f822a624c8552857606d22e8d8d
Chris McCord <chris@chrismccord.com>
2017-03-14 15:26:50 -0400

    Release v1.2.3 with latest plug

--- installer/mix.exs
+++ installer/mix.exs
@@ -3,7 +3,7 @@ defmodule Phoenix.New.Mixfile do
 
   def project do
     [app: :phoenix_new,
-     version: "1.2.2",
+     version: "1.2.3",
      elixir: "~> 1.2"]
   end
 
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Phoenix.Mixfile do
   use Mix.Project
 
-  @version "1.2.2"
+  @version "1.2.3"
 
   def project do
     [app: :phoenix,
