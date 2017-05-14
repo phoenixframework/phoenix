2319178aeb7f86a0936004edea69743dfc9e2d2f
David Kumru <davidkumru@users.noreply.github.com>
2017-05-14 13:11:52 +0200

    Fix grammar

--- installer/templates/phx_umbrella/apps/app_name_web/config/prod.secret.exs
+++ installer/templates/phx_umbrella/apps/app_name_web/config/prod.secret.exs
@@ -1,12 +1,12 @@
 use Mix.Config
 
 # In this file, we keep production configuration that
-# you likely want to automate and keep it away from
+# you'll likely want to automate and keep away from
 # your version control system.
 #
 # You should document the content of this
 # file or create a script for recreating it, since it's
 # kept out of version control and might be hard to recover
-# or recreate for your teammates (or you later on).
+# or recreate for your teammates (or yourself later on).
 config :<%= web_app_name %>, <%= endpoint_module %>,
   secret_key_base: "<%= prod_secret_key_base %>"
