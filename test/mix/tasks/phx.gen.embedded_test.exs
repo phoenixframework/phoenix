86bc1a9f388618b375ce449e6441559fdb3eecb4
Mike Buhot <michael.buhot@everydayhero.com.au>
2017-05-17 22:00:52 +1000

    Remove requirement for plural name for embedded schema
    The plural does not appear in the output, so it isn't needed as an argument.

--- test/mix/tasks/phx.gen.embedded_test.exs
+++ test/mix/tasks/phx.gen.embedded_test.exs
@@ -13,17 +13,17 @@ defmodule Mix.Tasks.Phx.Gen.EmbeddedTest do
 
   test "build" do
     in_tmp_project "embedded build", fn ->
-      schema = Gen.Embedded.build(~w(Blog.Post posts title:string), [])
+      schema = Gen.Embedded.build(~w(Blog.Post title:string))
 
       assert %Schema{
         alias: Post,
         module: Phoenix.Blog.Post,
         repo: Phoenix.Repo,
-        migration?: true,
+        migration?: false,
         migration_defaults: %{title: ""},
-        plural: "posts",
+        plural: nil,
         singular: "post",
-        human_plural: "Posts",
+        human_plural: "Nil",
         human_singular: "Post",
         attrs: [title: :string],
         types: %{title: :string},
@@ -36,7 +36,7 @@ defmodule Mix.Tasks.Phx.Gen.EmbeddedTest do
 
   test "generates embedded schema", config do
     in_tmp_project config.test, fn ->
-      Gen.Embedded.run(~w(Blog.Post blog_posts title:string))
+      Gen.Embedded.run(~w(Blog.Post title:string))
       assert_file "lib/phoenix/blog/post.ex", fn file ->
         assert file =~ "embedded_schema do"
       end
@@ -45,7 +45,7 @@ defmodule Mix.Tasks.Phx.Gen.EmbeddedTest do
 
   test "generates nested embedded schema", config do
     in_tmp_project config.test, fn ->
-      Gen.Embedded.run(~w(Blog.Admin.User users name:string))
+      Gen.Embedded.run(~w(Blog.Admin.User name:string))
 
       assert_file "lib/phoenix/blog/admin/user.ex", fn file ->
         assert file =~ "defmodule Phoenix.Blog.Admin.User do"
@@ -56,7 +56,7 @@ defmodule Mix.Tasks.Phx.Gen.EmbeddedTest do
 
   test "generates embedded schema with proper datetime types", config do
     in_tmp_project config.test, fn ->
-      Gen.Embedded.run(~w(Blog.Comment comments title:string drafted_at:datetime published_at:naive_datetime edited_at:utc_datetime))
+      Gen.Embedded.run(~w(Blog.Comment title:string drafted_at:datetime published_at:naive_datetime edited_at:utc_datetime))
 
       assert_file "lib/phoenix/blog/comment.ex", fn file ->
         assert file =~ "field :drafted_at, :naive_datetime"
