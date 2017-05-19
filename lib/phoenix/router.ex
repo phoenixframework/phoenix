1fad496c700128fe3251b3476d22aeb125561826
Stan Bright <stan@stanbright.com>
2017-05-19 15:46:05 +1000

    Fix outdated links: from elixir-lang.org to hexdocs.pm
    There are a number of links pointing to elixir-lang.org that
    are redirected to hexdocs.pm currently. Simply replace the old ones
    to the new targets.
    
    e.g.
    http://elixir-lang.org/docs/stable/elixir/Application.html
    =>
    https://hexdocs.pm/elixir/Application.html

--- lib/phoenix/router.ex
+++ lib/phoenix/router.ex
@@ -72,7 +72,7 @@ defmodule Phoenix.Router do
 
   The URL generated in the named URL helpers is based on the configuration for
   `:url`, `:http` and `:https`. However, if for some reason you need to manually
-  control the URL generation, the url helpers also allow you to pass in a [`URI`](http://elixir-lang.org/docs/stable/elixir/URI.html)
+  control the URL generation, the url helpers also allow you to pass in a [`URI`](https://hexdocs.pm/elixir/URI.html)
   struct:
 
       uri = %URI{scheme: "https", host: "other.example.com"}
