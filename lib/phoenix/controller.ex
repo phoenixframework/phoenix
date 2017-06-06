ab05d420bc37460f7e4e650d4f04b188736066bc
Odiumediae <odiumediae@users.noreply.github.com>
2017-06-07 00:11:03 +0200

    Update docs for action_fallback (#2342)
    * Update docs for action_fallback
    
    Correct the doc example for `action_fallback`, by fetching the post with the correct param, id.
    
    * Correct typo
    
    Correct typo in action_fallback documentation.

--- lib/phoenix/controller.ex
+++ lib/phoenix/controller.ex
@@ -189,7 +189,7 @@ defmodule Phoenix.Controller do
         action_fallback MyFallbackController
 
         def show(conn, %{"id" => id}, current_user) do
-          with {:ok, post} <- Blog.fetch_post(post),
+          with {:ok, post} <- Blog.fetch_post(id),
                :ok <- Authorizer.authorize(current_user, :view, post) do
 
             render(conn, "show.json", post: post)
@@ -200,7 +200,7 @@ defmodule Phoenix.Controller do
   In the above example, `with` is used to match only a successful
   post fetch, followed by valid authorization for the current user.
   In the event either of those fail to match, `with` will not invoke
-  the render block and instead returned the unmatched value. In this case,
+  the render block and instead return the unmatched value. In this case,
   imagine `Blog.fetch_post/2` returned `{:error, :not_found}` or
   `Authorizer.authorize/3` returned `{:error, :unauthorized}`. For cases
   where these datastructures serve as return values across multiple
