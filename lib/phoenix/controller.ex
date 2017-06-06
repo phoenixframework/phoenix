4d62f76fe91b18f372307612d18d2b3db0215099
Odiumediae <odiumediae@users.noreply.github.com>
2017-06-06 23:35:03 +0200

    Update docs for action_fallback
    Correct the doc example for `action_fallback`, by fetching the post with the correct param, id.

--- lib/phoenix/controller.ex
+++ lib/phoenix/controller.ex
@@ -189,7 +189,7 @@ defmodule Phoenix.Controller do
         action_fallback MyFallbackController
 
         def show(conn, %{"id" => id}, current_user) do
-          with {:ok, post} <- Blog.fetch_post(post),
+          with {:ok, post} <- Blog.fetch_post(id),
                :ok <- Authorizer.authorize(current_user, :view, post) do
 
             render(conn, "show.json", post: post)
