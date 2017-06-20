3aa856b12f99167e116ad5d86d6b8cc9a94cf401
Aaron Tinio <aptinio@gmail.com>
2017-06-21 03:12:33 +0800

    Use new mix.exs style (#2351)

--- installer/templates/phx_umbrella/apps/app_name/mix.exs
+++ installer/templates/phx_umbrella/apps/app_name/mix.exs
@@ -2,25 +2,29 @@ defmodule <%= app_module %>.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :<%= app_name %>,
-     version: "0.0.1",
-     build_path: "../../_build",
-     config_path: "../../config/config.exs",
-     deps_path: "../../deps",
-     lockfile: "../../mix.lock",
-     elixir: "~> 1.4",
-     elixirc_paths: elixirc_paths(Mix.env),
-     start_permanent: Mix.env == :prod,
-     aliases: aliases(),
-     deps: deps()]
+    [
+      app: :<%= app_name %>,
+      version: "0.0.1",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.4",
+      elixirc_paths: elixirc_paths(Mix.env),
+      start_permanent: Mix.env == :prod,
+      aliases: aliases(),
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application.
   #
   # Type `mix help compile.app` for more information.
   def application do
-    [mod: {<%= app_module %>.Application, []},
-     extra_applications: [:logger, :runtime_tools]]
+    [
+      mod: {<%= app_module %>.Application, []},
+      extra_applications: [:logger, :runtime_tools]
+    ]
   end
 
   # Specifies which paths to compile per environment.
@@ -31,8 +35,10 @@ defmodule <%= app_module %>.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [<%= if ecto do %>{:<%= adapter_app %>, ">= 0.0.0"},
-     {:ecto, "~> 2.1"}<% end %>]
+    [<%= if ecto do %>
+      {:<%= adapter_app %>, ">= 0.0.0"},
+      {:ecto, "~> 2.1"}
+    <% end %>]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.<%= if ecto do %>
@@ -42,8 +48,10 @@ defmodule <%= app_module %>.Mixfile do
   #
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
-    [<%= if ecto do %>"ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-     "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]<% end %>]
+    [<%= if ecto do %>
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+    <% end %>]
   end
 end
--- mix.exs
+++ mix.exs
@@ -6,112 +6,127 @@ defmodule Phoenix.Mixfile do
   # TODO v1.4: Update to elixir 1.4 + new application inference
 
   def project do
-    [app: :phoenix,
-     version: @version,
-     elixir: "~> 1.3",
-     deps: deps(),
-     package: package(),
-     preferred_cli_env: [docs: :docs],
+    [
+      app: :phoenix,
+      version: @version,
+      elixir: "~> 1.3",
+      deps: deps(),
+      package: package(),
+      preferred_cli_env: [docs: :docs],
 
-     # Because we define protocols on the fly to test
-     # Phoenix.Param, we need to disable consolidation
-     # for the test environment for Elixir v1.2 onward.
-     consolidate_protocols: Mix.env != :test,
-     xref: [exclude: [Ecto.Type]],
+      # Because we define protocols on the fly to test
+      # Phoenix.Param, we need to disable consolidation
+      # for the test environment for Elixir v1.2 onward.
+      consolidate_protocols: Mix.env != :test,
+      xref: [exclude: [Ecto.Type]],
 
-     name: "Phoenix",
-     docs: [source_ref: "v#{@version}",
-            main: "overview",
-            logo: "logo.png",
-            extra_section: "GUIDES",
-            assets: "deps/phoenix_guides/images",
-            extras: extras()],
-     aliases: aliases(),
-     source_url: "https://github.com/phoenixframework/phoenix",
-     homepage_url: "http://www.phoenixframework.org",
-     description: """
-     Productive. Reliable. Fast. A productive web framework that
-     does not compromise speed and maintainability.
-     """]
+      name: "Phoenix",
+      docs: [
+        source_ref: "v#{@version}",
+        main: "overview",
+        logo: "logo.png",
+        extra_section: "GUIDES",
+        assets: "deps/phoenix_guides/images",
+        extras: extras()
+      ],
+      aliases: aliases(),
+      source_url: "https://github.com/phoenixframework/phoenix",
+      homepage_url: "http://www.phoenixframework.org",
+      description: """
+      Productive. Reliable. Fast. A productive web framework that
+      does not compromise speed and maintainability.
+      """
+    ]
   end
 
   def application do
-    [mod: {Phoenix, []},
-     applications: [:plug, :poison, :logger, :eex, :phoenix_pubsub, :crypto],
-     env: [stacktrace_depth: nil,
-           template_engines: [],
-           format_encoders: [],
-           filter_parameters: ["password"],
-           serve_endpoints: false,
-           gzippable_exts: ~w(.js .css .txt .text .html .json .svg)]]
+    [
+      mod: {Phoenix, []},
+      applications: [:plug, :poison, :logger, :eex, :phoenix_pubsub, :crypto],
+      env: [
+        stacktrace_depth: nil,
+        template_engines: [],
+        format_encoders: [],
+        filter_parameters: ["password"],
+        serve_endpoints: false,
+        gzippable_exts: ~w(.js .css .txt .text .html .json .svg)
+      ]
+    ]
   end
 
   defp deps do
-    [{:cowboy, "~> 1.0", optional: true},
-     {:plug, "~> 1.3.2 or ~> 1.4"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:poison, "~> 2.2 or ~> 3.0"},
-     {:gettext, "~> 0.8", only: :test},
+    [
+      {:cowboy, "~> 1.0", optional: true},
+      {:plug, "~> 1.3.2 or ~> 1.4"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:poison, "~> 2.2 or ~> 3.0"},
+      {:gettext, "~> 0.8", only: :test},
 
-     # Docs dependencies
-     {:ex_doc, "~> 0.16", only: :docs},
-     {:inch_ex, "~> 0.2", only: :docs},
-     {:phoenix_guides, git: "https://github.com/phoenixframework/phoenix_guides.git", compile: false, app: false, only: :docs},
+      # Docs dependencies
+      {:ex_doc, "~> 0.16", only: :docs},
+      {:inch_ex, "~> 0.2", only: :docs},
+      {:phoenix_guides, git: "https://github.com/phoenixframework/phoenix_guides.git", compile: false, app: false, only: :docs},
 
-     # Test dependencies
-     {:phoenix_html, "~> 2.6", only: :test},
-     {:websocket_client, git: "https://github.com/jeremyong/websocket_client.git", only: :test}]
+      # Test dependencies
+      {:phoenix_html, "~> 2.6", only: :test},
+      {:websocket_client, git: "https://github.com/jeremyong/websocket_client.git", only: :test}
+    ]
   end
 
   defp package do
-    [maintainers: [
-      "Chris McCord", "José Valim", "Lance Halvorsen", "Gary Rennie",
-      "Jason Stiebs", "Eric Meadows-Jönsson", "Sonny Scroggin"],
-     licenses: ["MIT"],
-     links: %{github: "https://github.com/phoenixframework/phoenix"},
-     files: ~w(assets lib priv) ++
-            ~w(brunch-config.js CHANGELOG.md LICENSE.md mix.exs package.json README.md)]
+    [
+      maintainers: [
+        "Chris McCord", "José Valim", "Lance Halvorsen", "Gary Rennie",
+        "Jason Stiebs", "Eric Meadows-Jönsson", "Sonny Scroggin"
+      ],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/phoenixframework/phoenix"},
+      files: ~w(assets lib priv) ++
+        ~w(brunch-config.js CHANGELOG.md LICENSE.md mix.exs package.json README.md)
+    ]
   end
 
   defp extras do
-    ["introduction/overview.md": [group: "Introduction"],
-     "introduction/installation.md": [group: "Introduction"],
-     "introduction/learning.md": [group: "Introduction"],
-     "introduction/community.md": [group: "Introduction"],
+    [
+      "introduction/overview.md": [group: "Introduction"],
+      "introduction/installation.md": [group: "Introduction"],
+      "introduction/learning.md": [group: "Introduction"],
+      "introduction/community.md": [group: "Introduction"],
 
-     "up_and_running.md": [group: "Guides"],
-     "adding_pages.md": [group: "Guides"],
-     "routing.md": [group: "Guides"],
-     "plug.md": [group: "Guides"],
-     "controllers.md": [group: "Guides"],
-     "views.md": [group: "Guides"],
-     "templates.md": [group: "Guides"],
-     "channels.md": [group: "Guides"],
-     "ecto_models.md": [group: "Guides"],
+      "up_and_running.md": [group: "Guides"],
+      "adding_pages.md": [group: "Guides"],
+      "routing.md": [group: "Guides"],
+      "plug.md": [group: "Guides"],
+      "controllers.md": [group: "Guides"],
+      "views.md": [group: "Guides"],
+      "templates.md": [group: "Guides"],
+      "channels.md": [group: "Guides"],
+      "ecto_models.md": [group: "Guides"],
 
-     "testing/testing.md": [group: "Testing"],
-     "testing/testing_models.md": [group: "Testing"],
-     "testing/testing_controllers.md": [group: "Testing"],
-     "testing/testing_views.md": [group: "Testing"],
-     "testing/testing_channels.md": [group: "Testing"],
+      "testing/testing.md": [group: "Testing"],
+      "testing/testing_models.md": [group: "Testing"],
+      "testing/testing_controllers.md": [group: "Testing"],
+      "testing/testing_views.md": [group: "Testing"],
+      "testing/testing_channels.md": [group: "Testing"],
 
-     "deployment/deployment.md": [group: "Deployment"],
-     "deployment/heroku.md": [group: "Deployment"],
+      "deployment/deployment.md": [group: "Deployment"],
+      "deployment/heroku.md": [group: "Deployment"],
 
-     "bonus_guides/upgrading_phoenix.md": [group: "Bonus Guides"],
-     "bonus_guides/custom_primary_key.md": [group: "Bonus Guides"],
-     "bonus_guides/using_mysql.md": [group: "Bonus Guides"],
-     "bonus_guides/static_assets.md": [group: "Bonus Guides"],
-     "bonus_guides/file_uploads.md": [group: "Bonus Guides"],
-     "bonus_guides/sending_email_with_mailgun.md": [group: "Bonus Guides"],
-     "bonus_guides/sending_email_with_smtp.md": [group: "Bonus Guides"],
-     "bonus_guides/sessions.md": [group: "Bonus Guides"],
-     "bonus_guides/custom_errors.md": [group: "Bonus Guides"],
-     "bonus_guides/using_ssl.md": [group: "Bonus Guides"],
-     "bonus_guides/phoenix_behind_proxy.md": [group: "Bonus Guides"],
-     "bonus_guides/config.md": [group: "Bonus Guides"],
-     "bonus_guides/learning_elixir.md": [group: "Bonus Guides"],
-     "bonus_guides/seeding_data.md": [group: "Bonus Guides"]]
+      "bonus_guides/upgrading_phoenix.md": [group: "Bonus Guides"],
+      "bonus_guides/custom_primary_key.md": [group: "Bonus Guides"],
+      "bonus_guides/using_mysql.md": [group: "Bonus Guides"],
+      "bonus_guides/static_assets.md": [group: "Bonus Guides"],
+      "bonus_guides/file_uploads.md": [group: "Bonus Guides"],
+      "bonus_guides/sending_email_with_mailgun.md": [group: "Bonus Guides"],
+      "bonus_guides/sending_email_with_smtp.md": [group: "Bonus Guides"],
+      "bonus_guides/sessions.md": [group: "Bonus Guides"],
+      "bonus_guides/custom_errors.md": [group: "Bonus Guides"],
+      "bonus_guides/using_ssl.md": [group: "Bonus Guides"],
+      "bonus_guides/phoenix_behind_proxy.md": [group: "Bonus Guides"],
+      "bonus_guides/config.md": [group: "Bonus Guides"],
+      "bonus_guides/learning_elixir.md": [group: "Bonus Guides"],
+      "bonus_guides/seeding_data.md": [group: "Bonus Guides"]
+    ]
     |> Enum.map(fn {file, opts} -> {:"deps/phoenix_guides/docs/#{file}", opts} end)
   end
 
