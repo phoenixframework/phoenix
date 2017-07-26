89b988e80d5f8845127d09bd904f7715691c2451
José Valim <jose.valim@plataformatec.com.br>
2017-07-26 10:58:44 +0200

    Increase assert timeout to help with CI timing

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -26,4 +26,8 @@ end
 # For mix tests
 Mix.shell(Mix.Shell.Process)
 
-ExUnit.start(assert_receive_timeout: 200)
+assert_timeout = String.to_integer(
+  System.get_env("ELIXIR_ASSERT_TIMEOUT") || "200"
+)
+
+ExUnit.start(assert_receive_timeout: assert_timeout)
