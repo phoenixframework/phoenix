e037b844aa352fce3c6900a11bc255dc34f024e0
Jesse Shieh <jesseshieh@users.noreply.github.com>
2017-09-14 17:23:48 -0700

    Default server: true in prod.exs

--- installer/templates/phx_single/config/prod.exs
+++ installer/templates/phx_single/config/prod.exs
@@ -15,6 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :<%= web_app_name %>, <%= endpoint_module %>,
   load_from_system_env: true,
+  server: true,
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
