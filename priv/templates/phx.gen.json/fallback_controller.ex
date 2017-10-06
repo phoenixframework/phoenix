d8f0ab2fef786925c1606c98ca334b34126f6f5f
evuez <helloevuez@gmail.com>
2017-10-06 05:37:01 +0200

    Deprecate render/3 and render/4 in favor of put_view/2 (#2544)
    * Add deprecation warnings
    
    * Update controller guide
    
    * Use put_view/2 in fallback_controller template
    
    * Use put_view/2 in the doc for action_fallback

--- priv/templates/phx.gen.json/fallback_controller.ex
+++ priv/templates/phx.gen.json/fallback_controller.ex
@@ -9,12 +9,14 @@ defmodule <%= inspect context.web_module %>.FallbackController do
   def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
     conn
     |> put_status(:unprocessable_entity)
-    |> render(<%= inspect context.web_module %>.ChangesetView, "error.json", changeset: changeset)
+    |> put_view(<%= inspect context.web_module %>.ChangesetView)
+    |> render("error.json", changeset: changeset)
   end
 
   def call(conn, {:error, :not_found}) do
     conn
     |> put_status(:not_found)
-    |> render(<%= inspect context.web_module %>.ErrorView, :"404")
+    |> put_view(<%= inspect context.web_module %>.ErrorView)
+    |> render(:"404")
   end
 end
