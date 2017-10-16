1ffdf12b2c8e5e693d54dbdc05c660050378d012
Andrey Korobkov <korobkov@fryxell.info>
2017-10-16 22:09:44 +0400

    Write large numbers with underscores
    WHY IT MATTERS
    ┃
    ┃       Numbers can contain underscores for readability purposes.
    ┃       These do not affect the value of the number, but can help read large numbers
    ┃       more easily.
    ┃
    ┃       Like all `Readability` issues, this one is not a technical concern.
    ┃       But you can improve the odds of others reading and liking your code by making
    ┃       it easier to follow.

--- lib/phoenix/transports/long_poll.ex
+++ lib/phoenix/transports/long_poll.ex
@@ -10,7 +10,7 @@ defmodule Phoenix.Transports.LongPoll do
         window_ms: 10_000,
         pubsub_timeout_ms: 2_000,
         transport_log: false,
-        crypto: [max_age: 1209600]
+        crypto: [max_age: 1_209_600]
 
     * `:window_ms` - how long the client can wait for new messages
       in its poll request
@@ -44,7 +44,7 @@ defmodule Phoenix.Transports.LongPoll do
      serializer: [{Phoenix.Transports.LongPollSerializer, "~> 1.0.0"},
                   {Phoenix.Transports.V2.LongPollSerializer, "~> 2.0.0"}],
      transport_log: false,
-     crypto: [max_age: 1209600]]
+     crypto: [max_age: 1_209_600]]
   end
 
   ## Plug callbacks
