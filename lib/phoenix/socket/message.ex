phoenixframework/phoenix

commit 0ebbc8ace469f8265ae0bf7aae3cccad8c02baa2

Support linting code with Credo (#2567)
* Use `reraise` inside a rescue block to preserve the original stacktrace

WHY IT MATTERS
┃
┃       Using `Kernel.raise` inside of a `rescue` block creates a new stacktrace,
┃       which obscures the cause of the original error.

* Fix space missing after commas

WHY IT MATTERS
┃
┃       You can use white-space after commas to make items of lists,
┃       tuples and other enumerations easier to separate from one another.

* Write large numbers with underscores

WHY IT MATTERS
┃
┃       Numbers can contain underscores for readability purposes.
┃       These do not affect the value of the number, but can help read large numbers
┃       more easily.
┃
┃       Like all `Readability` issues, this one is not a technical concern.
┃       But you can improve the odds of others reading and liking your code by making
┃       it easier to follow.

* Fix whitespace around parentheses/brackets

WHY IT MATTERS
┃
┃       Don't use spaces after `(`, `[`, and `{` or before `}`, `]`, and `)`. This is
┃       the **preferred** way, although other styles are possible, as long as it is
┃       applied consistently.

lib/phoenix/socket/message.ex
@@ -30,7 +30,7 @@ defmodule Phoenix.Socket.Message do
       }
     rescue
       err in [KeyError] ->
-        raise Phoenix.Socket.InvalidMessageError, message: "missing key #{inspect err.key}"
+        reraise Phoenix.Socket.InvalidMessageError, message: "missing key #{inspect err.key}"
     end
   end
 end

lib/phoenix/transports/long_poll.ex
@@ -10,7 +10,7 @@ defmodule Phoenix.Transports.LongPoll do
         window_ms: 10_000,
         pubsub_timeout_ms: 2_000,
         transport_log: false,
-        crypto: [max_age: 1209600]
+        crypto: [max_age: 1_209_600]
 
     * `:window_ms` - how long the client can wait for new messages
       in its poll request
@@ -44,7 +44,7 @@ defmodule Phoenix.Transports.LongPoll do
      serializer: [{Phoenix.Transports.LongPollSerializer, "~> 1.0.0"},
                   {Phoenix.Transports.V2.LongPollSerializer, "~> 2.0.0"}],
      transport_log: false,
-     crypto: [max_age: 1209600]]
+     crypto: [max_age: 1_209_600]]
   end
 
   ## Plug callbacks
@@ -124,7 +124,7 @@ defmodule Phoenix.Transports.LongPoll do
   defp decode({:ok, body, conn}, serializer) do
     assign(conn, :message, serializer.decode!(body, []))
   rescue
-    Phoenix.Socket.InvalidMessageError -> raise Plug.Parsers.ParseError
+    Phoenix.Socket.InvalidMessageError -> reraise Plug.Parsers.ParseError
   end
   defp decode(_bad_request, _serializr), do: raise Plug.BadRequestError
 

lib/mix/tasks/phx.gen.embedded.ex
@@ -101,7 +101,7 @@ defmodule Mix.Tasks.Phx.Gen.Embedded do
   @doc false
   def copy_new_files(%Schema{} = schema, paths, binding) do
     files = files_to_be_generated(schema)
-    Mix.Phoenix.copy_from(paths,"priv/templates/phx.gen.embedded", binding, files)
+    Mix.Phoenix.copy_from(paths, "priv/templates/phx.gen.embedded", binding, files)
 
     schema
   end

lib/mix/tasks/phx.gen.html.ex
@@ -144,7 +144,7 @@ defmodule Mix.Tasks.Phx.Gen.Html do
       {:eex, "new.html.eex",        Path.join([web_prefix, "templates", web_path, schema.singular, "new.html.eex"])},
       {:eex, "show.html.eex",       Path.join([web_prefix, "templates", web_path, schema.singular, "show.html.eex"])},
       {:eex, "view.ex",             Path.join([web_prefix, "views", web_path, "#{schema.singular}_view.ex"])},
-      {:eex, "controller_test.exs", Path.join([test_prefix,"controllers", web_path, "#{schema.singular}_controller_test.exs"])},
+      {:eex, "controller_test.exs", Path.join([test_prefix, "controllers", web_path, "#{schema.singular}_controller_test.exs"])},
     ]
   end
 

lib/mix/tasks/phx.gen.schema.ex
@@ -124,7 +124,7 @@ defmodule Mix.Tasks.Phx.Gen.Schema do
   @doc false
   def copy_new_files(%Schema{context_app: ctx_app} = schema, paths, binding) do
     files = files_to_be_generated(schema)
-    Mix.Phoenix.copy_from(paths,"priv/templates/phx.gen.schema", binding, files)
+    Mix.Phoenix.copy_from(paths, "priv/templates/phx.gen.schema", binding, files)
 
     if schema.migration? do
       migration_path = Mix.Phoenix.context_app_path(ctx_app, "priv/repo/migrations/#{timestamp()}_create_#{schema.table}.exs")

lib/phoenix/digester.ex
@@ -149,7 +149,7 @@ defmodule Phoenix.Digester do
   defp map_digested_file(file_path, output_path) do
     {:ok, stats} = File.stat(file_path)
     digested_filename = Path.basename(file_path)
-    [digest,_] = Regex.run(@digested_file_regex, digested_filename)
+    [digest, _] = Regex.run(@digested_file_regex, digested_filename)
     digest = String.trim_leading(digest, "-")
 
     %{absolute_path: file_path,

lib/phoenix/endpoint/cowboy_handler.ex
@@ -110,7 +110,7 @@ defmodule Phoenix.Endpoint.CowboyHandler do
   end
 
   defp info(scheme, endpoint, ref) do
-    {addr,port} = :ranch.get_addr(ref)
+    {addr, port} = :ranch.get_addr(ref)
     addr_str = :inet.ntoa(addr)
     "Running #{inspect endpoint} with Cowboy using #{scheme}://#{addr_str}:#{port}"
   end

lib/phoenix/presence.ex
@@ -103,7 +103,7 @@ defmodule Phoenix.Presence do
   """
   alias Phoenix.Socket.Broadcast
 
-  @type presences :: %{ String.t => %{metas: [map()]}}
+  @type presences :: %{String.t => %{metas: [map()]}}
   @type presence :: %{key: String.t, meta: map()}
   @type topic :: String.t
 

commit b11dfd1e4f63716d810d277c76f8cb6247a8b363

Fix reraising errors
Was broken in 0ebbc8a

lib/phoenix/socket/message.ex
@@ -30,7 +30,8 @@ defmodule Phoenix.Socket.Message do
       }
     rescue
       err in [KeyError] ->
-        reraise Phoenix.Socket.InvalidMessageError, message: "missing key #{inspect err.key}"
+        stacktrace = System.stacktrace
+        reraise Phoenix.Socket.InvalidMessageError, [message: "missing key #{inspect err.key}"], stacktrace
     end
   end
 end

lib/phoenix/transports/long_poll.ex
@@ -124,7 +124,9 @@ defmodule Phoenix.Transports.LongPoll do
   defp decode({:ok, body, conn}, serializer) do
     assign(conn, :message, serializer.decode!(body, []))
   rescue
-    Phoenix.Socket.InvalidMessageError -> reraise Plug.Parsers.ParseError
+    Phoenix.Socket.InvalidMessageError ->
+      stacktrace = System.stacktrace
+      reraise Plug.Parsers.ParseError, stacktrace
   end
   defp decode(_bad_request, _serializr), do: raise Plug.BadRequestError
 