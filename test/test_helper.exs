14074dc11918ebaa93c03b4d265f025362340d57
José Valim <jose.valim@plataformatec.com.br>
2018-02-06 10:38:02 +0100

    Fix the build matrix in regards to Elixir versions

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -25,4 +25,5 @@ assert_timeout = String.to_integer(
   System.get_env("ELIXIR_ASSERT_TIMEOUT") || "200"
 )
 
-ExUnit.start(assert_receive_timeout: assert_timeout)
+exclude = if Version.match?(System.version, "~> 1.5"), do: [], else: [phx_new: true]
+ExUnit.start(assert_receive_timeout: assert_timeout, exclude: exclude)
