ca24694f8585ae862756c754824563fc76c3d782
Chris McCord <chris@chrismccord.com>
2018-02-13 11:34:38 -0500

    Temporarily override plug dep

--- mix.exs
+++ mix.exs
@@ -55,8 +55,8 @@ defmodule Phoenix.Mixfile do
   defp deps do
     [
       {:cowboy, "~> 1.0 or ~> 2.2.2 or ~> 2.3", optional: true},
-      # TODO: bump to 1.5.0 stable when it goes out with `init_mode` support
-      {:plug, github: "elixir-plug/plug", branch: "master", override: true},
+      # TODO: before 1.4 release bump to 1.5.0 stable when it goes out with `init_mode` support
+      {:plug, "~> 1.5.0-rc2", override: true},
       {:phoenix_pubsub, "~> 1.0"},
       {:jason, "~> 1.0", optional: true},
       {:gettext, "~> 0.8", only: :test},
@@ -66,6 +66,7 @@ defmodule Phoenix.Mixfile do
       {:inch_ex, "~> 0.2", only: :docs},
 
       # Test dependencies
+      # TODO: before phoenix 1.4 release bump to next stable release with relaxed plug dep
       {:phoenix_html, "~> 2.10", only: :test},
       {:websocket_client, git: "https://github.com/jeremyong/websocket_client.git", only: :test}
     ]
