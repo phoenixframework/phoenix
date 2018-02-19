e12637d23d32fcba578516faae1c885c9e0c395a
Nick Janetakis <nick.janetakis@gmail.com>
2018-02-19 17:15:07 -0500

    Add get_flash/1 to new project generator (#2768)

--- installer/templates/phx_umbrella/apps/app_name_web/lib/app_name.ex
+++ installer/templates/phx_umbrella/apps/app_name_web/lib/app_name.ex
@@ -32,7 +32,7 @@ defmodule <%= web_namespace %> do
                         namespace: <%= web_namespace %>
 
       # Import convenience functions from controllers
-      import Phoenix.Controller, only: [get_flash: 2, view_module: 1]<%= if html do %>
+      import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]<%= if html do %>
 
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML<% end %>
