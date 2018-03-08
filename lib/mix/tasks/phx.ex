4796d78a5220c5ec6982a18895e751b14a6d4cd2
Paweł Dawczak <pawel.dawczak@gmail.com>
2018-03-08 22:31:08 +0000

    Add default phx task

---
+++ lib/mix/tasks/phx.ex
@@ -0,0 +1,32 @@
+defmodule Mix.Tasks.Phx do
+  use Mix.Task
+
+  @shortdoc "Prints Phoenix help information"
+
+  @moduledoc """
+  Prints Phoenix tasks and their information.
+
+      mix phx
+
+  """
+
+  @doc false
+  def run(args) do
+    {_opts, args, _} = OptionParser.parse(args, switches: [])
+
+    case args do
+      [] -> general()
+      _ ->
+        Mix.raise "Invalid arguments, expected: mix phx"
+    end
+  end
+
+  defp general() do
+    Application.ensure_all_started(:phoenix)
+    Mix.shell.info "Phoenix v#{Application.spec(:phoenix, :vsn)}"
+    Mix.shell.info "Productive. Reliable. Fast."
+    Mix.shell.info "A productive web framework that does not compromise speed and maintainability."
+    Mix.shell.info "\nAvailable tasks:\n"
+    Mix.Tasks.Help.run(["--search", "phx."])
+  end
+end
