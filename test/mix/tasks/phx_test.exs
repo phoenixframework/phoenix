d0da72e99ead46ffba099ad054c3a939ef910565
Paweł Dawczak <pawel.dawczak@gmail.com>
2018-03-09 07:59:06 +0000

    Add phx task (#2795)

---
+++ test/mix/tasks/phx_test.exs
@@ -0,0 +1,20 @@
+defmodule Mix.Tasks.Phx.Test do
+  use ExUnit.Case
+
+  test "provide a list of available ecto mix tasks" do
+    Mix.Tasks.Phx.run []
+    assert_received {:mix_shell, :info, ["mix phx.digest" <> _]}
+    assert_received {:mix_shell, :info, ["mix phx.digest.clean" <> _]}
+    assert_received {:mix_shell, :info, ["mix phx.gen.channel" <> _]}
+    assert_received {:mix_shell, :info, ["mix phx.gen.context" <> _]}
+    assert_received {:mix_shell, :info, ["mix phx.gen.embedded" <> _]}
+    assert_received {:mix_shell, :info, ["mix phx.gen.html" <> _]}
+    assert_received {:mix_shell, :info, ["mix phx.gen.json" <> _]}
+  end
+
+  test "expects no arguments" do
+    assert_raise Mix.Error, fn ->
+      Mix.Tasks.Phx.run ["invalid"]
+    end
+  end
+end
