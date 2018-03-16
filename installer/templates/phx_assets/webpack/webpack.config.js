60c5f7b85f8aae6c4a6aa39170920fdeb318246d
Sam Chen <chenxsan@gmail.com>
2018-03-16 22:07:29 +0800

    Optimize extracted css

--- installer/templates/phx_assets/webpack/webpack.config.js
+++ installer/templates/phx_assets/webpack/webpack.config.js
@@ -21,7 +21,14 @@ module.exports = {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: 'css-loader'
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true
+              }
+            }
+          ]
         })
       }
     ]
