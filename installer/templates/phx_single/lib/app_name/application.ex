95c2f45c849eee4591540eb4724cb60e7b39570e
Fernando Tapia Rico <fertapric@gmail.com>
2018-03-23 18:00:59 +0100

    Use @impl true instead of @impl <module>

--- installer/templates/phx_single/lib/app_name/application.ex
+++ installer/templates/phx_single/lib/app_name/application.ex
@@ -5,7 +5,7 @@ defmodule <%= app_module %>.Application do
 
   use Application
 
-  @impl Application
+  @impl true
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [<%= if ecto do %>
