a3a5f9c5da3e8eeb4c5779dfd26a874d13998003
Fernando Tapia Rico <fertapric@gmail.com>
2018-03-29 17:35:37 +0200

    Update application.ex templates with default docs
    [ci skip]

--- installer/templates/phx_umbrella/apps/app_name_web/lib/app_name/application.ex
+++ installer/templates/phx_umbrella/apps/app_name_web/lib/app_name/application.ex
@@ -1,5 +1,8 @@
 defmodule <%= web_namespace %>.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
   @moduledoc false
+
   use Application
 
   def start(_type, _args) do
