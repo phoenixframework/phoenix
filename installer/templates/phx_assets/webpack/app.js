1675642caf87bc867922a979d2a5dd37b850b30f
Michał Łępicki <michallepicki@users.noreply.github.com>
2018-04-09 18:16:54 +0200

    change ExtractTextPlugin to MiniCssExtractPlugin in js comment (#2837)

--- installer/templates/phx_assets/webpack/app.js
+++ installer/templates/phx_assets/webpack/app.js
@@ -1,5 +1,5 @@
 // We need to import the CSS so that webpack will load it.
-// The ExtractTextPlugin is used to separate it out into
+// The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
 import css from "../css/app.css";
 
