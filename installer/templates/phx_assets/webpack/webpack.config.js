5bd62226afe4b0e26c4335fece5b8149c67fe3f0
Gary Rennie <gazler@gmail.com>
2018-04-09 03:15:50 +0100

    Replace extract-text-webpack-plugin with mini-css-extract-plugin (#2831)
    According to the TL;DR of the comment below, mini-css-extract-plugin is
    replacing extract-text-webpack-plugin which will be deprecated. Since
    this does not have a built in optimizer for CSS, a separate plugin is
    used. However overriding the optimizer means that UglifyJS must be
    included manually (previously this was a transitive dependency of
    webpack which was used automatically in production mode)
    
    https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/749#issuecomment-374549467
    
    This closes #2818

--- installer/templates/phx_assets/webpack/webpack.config.js
+++ installer/templates/phx_assets/webpack/webpack.config.js
@@ -1,8 +1,16 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => ({
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: false }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
   entry: './js/app.js',
   output: {
     filename: 'app.js',
@@ -19,17 +27,12 @@ module.exports = (env, options) => ({
       },
       {
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: {
-            loader: 'css-loader', options: { minimize: options.mode === 'production' }
-          }
-        })
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin('../css/app.css'),
+    new MiniCssExtractPlugin({ filename: '../css/app.css' }),
     new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
   ]
 });
