88c6404603a4796b86c9d45ccff6a8c33f794fc8
José Valim <jose.valim@plataformatec.com.br>
2018-06-16 13:18:48 +0200

    Use latest Plug

--- installer/templates/phx_single/config/prod.exs
+++ installer/templates/phx_single/config/prod.exs
@@ -27,15 +27,22 @@ config :logger, level: :info
 #       url: [host: "example.com", port: 443],
 #       https: [:inet6,
 #               port: 443,
+#               cipher_suite: :strong,
 #               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
 #               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
 #
-# Where those two env variables return an absolute path to
-# the key and cert in disk or a relative path inside priv,
-# for example "priv/ssl/server.key".
+# The `cipher_suite` is set to `:strong` to support only the
+# latest and more secure SSL ciphers. This means old browsers
+# and clients may not be supported. You can set it to
+# `:compatible` for wider support.
 #
-# We also recommend setting `force_ssl`, ensuring no data is
-# ever sent via http, always redirecting to https:
+# `:keyfile` and `:certfile` expect an absolute path to the key
+# and cert in disk or a relative path inside priv, for example
+# "priv/ssl/server.key". For all supported SSL configuration
+# options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
+#
+# We also recommend setting `force_ssl` in your endpoint, ensuring
+# no data is ever sent via http, always redirecting to https:
 #
 #     config :<%= app_name %>, <%= endpoint_module %>,
 #       force_ssl: [hsts: true]
--- installer/templates/phx_umbrella/apps/app_name_web/config/prod.exs
+++ installer/templates/phx_umbrella/apps/app_name_web/config/prod.exs
@@ -23,22 +23,29 @@ config :<%= web_app_name %>, <%= endpoint_module %>,
 # To get SSL working, you will need to add the `https` key
 # to the previous section and set your `:url` port to 443:
 #
-#     config :<%= web_app_name %>, <%= endpoint_module %>,
+#     config :<%= app_name %>, <%= endpoint_module %>,
 #       ...
 #       url: [host: "example.com", port: 443],
 #       https: [:inet6,
 #               port: 443,
+#               cipher_suite: :strong,
 #               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
 #               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
 #
-# Where those two env variables return an absolute path to
-# the key and cert in disk or a relative path inside priv,
-# for example "priv/ssl/server.key".
+# The `cipher_suite` is set to `:strong` to support only the
+# latest and more secure SSL ciphers. This means old browsers
+# and clients may not be supported. You can set it to
+# `:compatible` for wider support.
 #
-# We also recommend setting `force_ssl`, ensuring no data is
-# ever sent via http, always redirecting to https:
+# `:keyfile` and `:certfile` expect an absolute path to the key
+# and cert in disk or a relative path inside priv, for example
+# "priv/ssl/server.key". For all supported SSL configuration
+# options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
 #
-#     config :<%= web_app_name %>, <%= endpoint_module %>,
+# We also recommend setting `force_ssl` in your endpoint, ensuring
+# no data is ever sent via http, always redirecting to https:
+#
+#     config :<%= app_name %>, <%= endpoint_module %>,
 #       force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
