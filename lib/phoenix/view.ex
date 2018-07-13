f2993dce074afd00953ce62183e4486cb169fa5f
Micael Nussbaumer <micaelnussbaumer@gmail.com>
2018-07-13 20:54:12 +0100

    forgot eex expression in view.ex

--- lib/phoenix/view.ex
+++ lib/phoenix/view.ex
@@ -20,7 +20,8 @@ defmodule Phoenix.View do
             use Phoenix.View, root: "lib/your_app_web/templates", namespace: "web"
             
             # Import convenience functions from controllers
-            import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]<%= if html do %>
+            import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
+
             # Use all HTML functionality (forms, tags, etc)
             use Phoenix.HTML
           
