ac88958550fbd861e2f1e1af6e3c6b787b1a202e
Mike Over <mikeover@users.noreply.github.com>
2018-08-28 11:26:00 -0400

    Fix app config in namespaced umbrella apps (#3031)
    * Fix app config in namespaced umbrella apps
    
    * Remove extra newlines

--- installer/templates/phx_umbrella/apps/app_name/config/config.exs
+++ installer/templates/phx_umbrella/apps/app_name/config/config.exs
@@ -2,8 +2,10 @@
 # should only configure the :<%= app_name %> application itself
 # and only for organization purposes. All other config goes to
 # the umbrella root.
-use Mix.Config<%= if ecto do %>
+use Mix.Config<%= if namespaced? || ecto do %>
 
-config :<%= app_name %>, ecto_repos: [<%= app_module %>.Repo]<% end %>
+config :<%= app_name %><%= if namespaced? do %>,
+  namespace: <%= app_module %><% end %><%= if ecto do %>,
+  ecto_repos: [<%= app_module %>.Repo]<% end %><% end %>
 
 import_config "#{Mix.env()}.exs"
