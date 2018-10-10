f675e04ab42766448d5f9854eb0f72c9302bc13a
Bram Verburg <bram.verburg@voltone.net>
2018-10-10 14:43:37 +0300

    Block phx.gen.cert task at umbrella level

--- lib/mix/tasks/phx.gen.cert.ex
+++ lib/mix/tasks/phx.gen.cert.ex
@@ -48,6 +48,10 @@ defmodule Mix.Tasks.Phx.Gen.Cert do
 
   @doc false
   def run(all_args) do
+    if Mix.Project.umbrella?() do
+      Mix.raise("mix phx.gen.channel can only be run inside an application directory")
+    end
+
     {opts, args} =
       OptionParser.parse!(
         all_args,
