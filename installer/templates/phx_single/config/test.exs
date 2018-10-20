cb0948a75bae1e1ebe8305ea31b290b165bf9a6d
Benjamin Milde <benni@kobrakai.de>
2018-10-20 20:05:41 +0200

    Move test ports to 4002 to not overlap with the proposed ssl port

--- installer/templates/phx_single/config/test.exs
+++ installer/templates/phx_single/config/test.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :<%= app_name %>, <%= endpoint_module %>,
-  http: [port: 4001],
+  http: [port: 4002],
   server: false
 
 # Print only warnings and errors during test
