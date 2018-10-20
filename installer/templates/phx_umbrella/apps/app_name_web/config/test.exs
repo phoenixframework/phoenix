0cb95e6018eeff70c6e1d7a1675f250cae45f3d2
Benjamin Milde <benni@kobrakai.de>
2018-10-20 22:05:17 +0200

    Move test ports to 4002 to not overlap with the proposed ssl port (#3110)

--- installer/templates/phx_umbrella/apps/app_name_web/config/test.exs
+++ installer/templates/phx_umbrella/apps/app_name_web/config/test.exs
@@ -7,5 +7,5 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :<%= web_app_name %>, <%= endpoint_module %>,
-  http: [port: 4001],
+  http: [port: 4002],
   server: false
