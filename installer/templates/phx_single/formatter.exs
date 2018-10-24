d615b77b9efbc6a812f0dfe2dfc968a1d4303028
Fernando Tapia Rico <fertapric@gmail.com>
2018-10-24 19:38:50 +0200

    Import Ecto's .formatter.exs in templates (#3117)

--- installer/templates/phx_single/formatter.exs
+++ installer/templates/phx_single/formatter.exs
@@ -1,5 +1,5 @@
 [
-  import_deps: [:phoenix],
+  import_deps: [<%= if ecto do %>:ecto, <% end %>:phoenix],
   inputs: ["*.{ex,exs}", "{config,lib,priv,test}/**/*.{ex,exs}"]<%= if ecto do %>,
   subdirectories: ["priv/*/migrations"]<% end %>
 ]
