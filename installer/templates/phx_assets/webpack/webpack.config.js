091e7bb82ddab262e5690998f82db82afaf556ee
Sfusato <raie2003@scs.ubbcluj.ro>
2018-12-11 14:38:41 +0200

    Switch from uglifyjs-webpack-plugin to terser-webpack-plugin (#3189)
    * Switch from uglifyjs-webpack-plugin to terser-webpack-plugin
    
    * spaces instead of tabs

--- installer/templates/phx_assets/webpack/webpack.config.js
+++ installer/templates/phx_assets/webpack/webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const glob = require('glob');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => ({
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: false }),
+      new TerserPlugin({ cache: true, parallel: true, sourceMap: false }),
       new OptimizeCSSAssetsPlugin({})
     ]
   },
