02118169920768b4cca82e259e6b7f49af3f9a1c
Gary Rennie <gazler@gmail.com>
2019-01-03 14:59:27 +0000

    Explicitly include the layout option when generating render_errors
    This will hint to the users that a layout can be provided for error
    pages.

--- installer/templates/phx_single/config/config.exs
+++ installer/templates/phx_single/config/config.exs
@@ -16,7 +16,7 @@ config :<%= app_name %><%= if namespaced? do %>,
 config :<%= app_name %>, <%= endpoint_module %>,
   url: [host: "localhost"],
   secret_key_base: "<%= secret_key_base %>",
-  render_errors: [view: <%= web_namespace %>.ErrorView, accepts: ~w(<%= if html do %>html <% end %>json)],
+  render_errors: [view: <%= web_namespace %>.ErrorView, accepts: ~w(<%= if html do %>html <% end %>json), layout: false],
   pubsub: [name: <%= app_module %>.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
