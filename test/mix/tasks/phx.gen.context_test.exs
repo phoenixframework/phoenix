af02d98b4ae3e12dd92e48151e112940c06e80a6
Boris Kuznetsov <achempion@gmail.com>
2019-02-22 01:38:31 +0300

    prohibit context creation with the same name as the application

--- test/mix/tasks/phx.gen.context_test.exs
+++ test/mix/tasks/phx.gen.context_test.exs
@@ -104,6 +104,10 @@ defmodule Mix.Tasks.Phx.Gen.ContextTest do
         Gen.Context.run(~w(Blog Blog blogs))
       end
 
+      assert_raise Mix.Error, ~r/Cannot generate context Phoenix because it has the same name as the application/, fn ->
+        Gen.Context.run(~w(Phoenix Post blogs))
+      end
+
       assert_raise Mix.Error, ~r/Invalid arguments/, fn ->
         Gen.Context.run(~w(Blog.Post posts))
       end
