0bd30927a6d1ef1d086e3239855b4e24eb6a256c
Ryan Nielson <ryan.nielson@gmail.com>
2019-03-01 20:22:09 -0400

    A small indentation fix in webpack.config.js

--- installer/templates/phx_assets/webpack/webpack.config.js
+++ installer/templates/phx_assets/webpack/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = (env, options) => ({
     ]
   },
   entry: {
-      './js/app.js': ['./js/app.js'].concat(glob.sync('./vendor/**/*.js'))
+    './js/app.js': ['./js/app.js'].concat(glob.sync('./vendor/**/*.js'))
   },
   output: {
     filename: 'app.js',
