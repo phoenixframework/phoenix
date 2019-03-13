42644507967d6c297a128800a73a3c92e08f3e35
Chris McCord <chris@chrismccord.com>
2019-03-12 23:40:35 -0400

    Only log if server is running

--- lib/phoenix/endpoint/supervisor.ex
+++ lib/phoenix/endpoint/supervisor.ex
@@ -14,8 +14,9 @@ defmodule Phoenix.Endpoint.Supervisor do
     case Supervisor.start_link(__MODULE__, {otp_app, mod}, name: mod) do
       {:ok, _} = ok ->
         warmup(mod)
-        log_access_info(mod)
+        log_access_info(otp_app, mod)
         ok
+
       {:error, _} = error ->
         error
     end
@@ -414,9 +415,9 @@ defmodule Phoenix.Endpoint.Supervisor do
     end
   end
 
-  defp log_access_info(endpoint) do
-    if endpoint.config(:https) || endpoint.config(:http) do
-      Logger.info("Access #{inspect endpoint} at #{endpoint.url}")
+  defp log_access_info(otp_app, endpoint) do
+    if Phoenix.Endpoint.server?(otp_app, endpoint) do
+      Logger.info("Access #{inspect(endpoint)} at #{endpoint.url()}")
     end
   end
 end
