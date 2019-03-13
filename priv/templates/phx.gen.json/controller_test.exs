2a2d6f3d93249ccae9ca4411355e2be0344dea44
Chris McCord <chris@chrismccord.com>
2019-03-12 22:57:19 -0400

    Use inspect to future-proof conversion

--- priv/templates/phx.gen.json/controller_test.exs
+++ priv/templates/phx.gen.json/controller_test.exs
@@ -37,7 +37,7 @@ defmodule <%= inspect context.web_module %>.<%= inspect Module.concat(schema.web
 
       assert %{
                "id" => id<%= for {key, val} <- schema.params.create do %>,
-               "<%= key %>" => <%= if val == %{}, do: "%{}", else: Phoenix.json_library().encode!(val) %><% end %>
+               "<%= key %>" => <%= val |> Phoenix.json_library().encode!() |> Phoenix.json_library().decode!() |> inspect() %><% end %>
              } = json_response(conn, 200)["data"]
     end
 
