d151d1665c63f83dc2f432c559767a36bade8cc7
Allan Klaus <allan_klaus@globo.com>
2019-04-04 13:43:58 -0300

    Improve encode_filename to use option encode with default true

--- lib/phoenix/controller.ex
+++ lib/phoenix/controller.ex
@@ -928,15 +928,15 @@ defmodule Phoenix.Controller do
 
   defp prepare_send_download(conn, filename, opts) do
     content_type = opts[:content_type] || MIME.from_path(filename)
-    encoded_filename = encode_filename(filename, opts[:encode])
+    encoded_filename = encode_filename(filename, Keyword.get(opts, :encode, true))
     warn_if_ajax(conn)
     conn
     |> put_resp_content_type(content_type, opts[:charset])
     |> put_resp_header("content-disposition", ~s[attachment; filename="#{encoded_filename}"])
   end
 
-  defp encode_filename(filename, encode) when encode == false, do: filename
-  defp encode_filename(filename, _encode), do: URI.encode_www_form(filename)
+  defp encode_filename(filename, false), do: filename
+  defp encode_filename(filename, true), do: URI.encode_www_form(filename)
 
   defp ajax?(conn) do
     case get_req_header(conn, "x-requested-with") do
