0ea46e80786d83b00db8ca33dd7a2949a16aa0f7
José Valim <jose.valim@gmail.com>
2019-05-07 09:45:32 +0200

    Update lib/phoenix/socket/transport.ex
    Co-Authored-By: Gazler <gazler@gmail.com>

--- lib/phoenix/socket/transport.ex
+++ lib/phoenix/socket/transport.ex
@@ -201,7 +201,7 @@ defmodule Phoenix.Socket.Transport do
 
   If `reason` is `:closed`, it means the client closed the socket. This is
   considered a `:normal` exit signal, so linked process will not automatically
-  exit. See Process.exit/2 for more details on exit signals.
+  exit. See `Process.exit/2` for more details on exit signals.
   """
   @callback terminate(reason :: term, state) :: :ok
 
