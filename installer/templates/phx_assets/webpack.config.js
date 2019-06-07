284f8f8529babef102aa05a23a7faf4f9883975f
Cameron Dykes <yellow5c@gmail.com>
2019-06-07 10:52:17 -0500

    Patch webpack config to load app.js after vendor js

--- installer/templates/phx_assets/webpack.config.js
+++ installer/templates/phx_assets/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = (env, options) => ({
     ]
   },
   entry: {
-    './js/app.js': ['./js/app.js'].concat(glob.sync('./vendor/**/*.js'))
+    './js/app.js': glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
   },
   output: {
     filename: 'app.js',
