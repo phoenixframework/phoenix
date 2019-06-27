5691dc54e0e1991177da09ae94191face751b6cc
José Valim <jose.valim@plataformatec.com.br>
2019-06-27 21:02:14 +0200

    Update version on local.phx note

--- installer/lib/mix/tasks/local.phx.ex
+++ installer/lib/mix/tasks/local.phx.ex
@@ -10,9 +10,9 @@ defmodule Mix.Tasks.Local.Phx do
 
   Accepts the same command line options as `archive.install hex phx_new`.
 
-  *Note: Older versions of this task (up to and including 1.4.0) do not fetch
-  the latest version from hex.  If your phx_new archive is older than 1.4, it's
-  necessary to call `mix archive.install hex phx_new` manually at least once.*
+  *Note: Older versions of this task (up to and including 1.4.8) do not fetch
+  the latest version from hex. If your `phx_new` archive is older than 1.4.9,
+  it's necessary to call `mix archive.install hex phx_new` manually at least once.*
   """
   def run(args) do
     Mix.Task.run("archive.install", ["hex", "phx_new" | args])
