f77d06586b0f5fc76e77e5d547f13ebf7a23ec5d
Adam Millerchip <adam@millerchip.net>
2019-06-27 21:57:09 +0900

    Use hex in mix local.phx

--- installer/lib/mix/tasks/local.phx.ex
+++ installer/lib/mix/tasks/local.phx.ex
@@ -1,7 +1,6 @@
 defmodule Mix.Tasks.Local.Phx do
   use Mix.Task
 
-  @url "https://github.com/phoenixframework/archives/raw/master/phx_new.ez"
   @shortdoc "Updates the Phoenix project generator locally"
 
   @moduledoc """
@@ -9,9 +8,13 @@ defmodule Mix.Tasks.Local.Phx do
 
       mix local.phx
 
-  Accepts the same command line options as `archive.install`.
+  Accepts the same command line options as `archive.install hex phx_new`.
+
+  *Note: Older versions of this task (up to and including 1.4.0) do not fetch
+  the latest version from hex.  If your phx_new archive is older than 1.4, it's
+  necessary to call `mix archive.install hex phx_new` manually at least once.*
   """
   def run(args) do
-    Mix.Task.run "archive.install", [@url | args]
+    Mix.Task.run("archive.install", ["hex", "phx_new" | args])
   end
 end
