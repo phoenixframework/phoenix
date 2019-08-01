c2270181ae93532f12f1da9a7cbb339cce97f023
Gary Rennie <gazler@gmail.com>
2019-08-01 21:02:29 +0100

    Allow using a keyword list/map for socket assigns
    This reflects the capabilities of Live View, allow multiple vallues to
    be assigned at once.

--- test/phoenix/socket/socket_test.exs
+++ test/phoenix/socket/socket_test.exs
@@ -37,4 +37,30 @@ defmodule Phoenix.SocketTest do
       assert socket.assigns[:foo] == :bar
     end
   end
+
+  describe "assign/2" do
+    test "assigns a map socket" do
+      socket = %Phoenix.Socket{}
+      assert socket.assigns[:foo] == nil
+      socket = assign(socket, %{foo: :bar, abc: :def})
+      assert socket.assigns[:foo] == :bar
+      assert socket.assigns[:abc] == :def
+    end
+
+    test "merges if values exist" do
+      socket = %Phoenix.Socket{}
+      socket = assign(socket, %{foo: :bar, abc: :def})
+      socket = assign(socket, %{foo: :baz})
+      assert socket.assigns[:foo] == :baz
+      assert socket.assigns[:abc] == :def
+    end
+
+    test "merges keyword lists" do
+      socket = %Phoenix.Socket{}
+      socket = assign(socket, %{foo: :bar, abc: :def})
+      socket = assign(socket, [foo: :baz])
+      assert socket.assigns[:foo] == :baz
+      assert socket.assigns[:abc] == :def
+    end
+  end
 end
