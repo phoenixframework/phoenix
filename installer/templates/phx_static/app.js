c40b80ff33ecc7b67a8d424d2304d9f2a245a758
m31271n <m31271n@pm.me>
2019-12-12 22:50:54 +0800

    Use phoenix_html.js installed by mix (#3625)

--- installer/templates/phx_static/app.js
+++ installer/templates/phx_static/app.js
@@ -1,3 +1,3 @@
 // for phoenix_html support, including form and button helpers
 // copy the following scripts into your javascript bundle:
-// * https://raw.githubusercontent.com/phoenixframework/phoenix_html/master/priv/static/phoenix_html.js
+// * deps/phoenix_html/priv/static/phoenix_html.js
