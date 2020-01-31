af24e9bdac4f25cb808dbf3e1837ffc163a95300
José Valim <jose.valim@dashbit.co>
2020-01-31 22:59:49 +0100

    Provide a LiveView signing salt out of the box

--- installer/templates/phx_single/config/config.exs
+++ installer/templates/phx_single/config/config.exs
@@ -17,7 +17,8 @@ config :<%= app_name %>, <%= endpoint_module %>,
   url: [host: "localhost"],
   secret_key_base: "<%= secret_key_base %>",
   render_errors: [view: <%= web_namespace %>.ErrorView, accepts: ~w(<%= if html do %>html <% end %>json), layout: false],
-  pubsub_server: <%= app_module %>.PubSub
+  pubsub_server: <%= app_module %>.PubSub,
+  live_view: [signing_salt: "<%= lv_signing_salt %>"]
 
 # Configures Elixir's Logger
 config :logger, :console,
