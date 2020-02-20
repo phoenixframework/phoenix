068331c079d61c64b32f42a8bf6cd9797bbc04cb
Chris Keathley <c@keathley.io>
2020-02-20 15:40:13 -0500

    Emit telemetry when router fails (#3675)

--- lib/phoenix/router.ex
+++ lib/phoenix/router.ex
@@ -358,9 +358,15 @@ defmodule Phoenix.Router do
             conn
         rescue
           e in Plug.Conn.WrapperError ->
+            measurements = %{duration: System.monotonic_time() - start}
+            metadata = %{kind: :error, error: e, stacktrace: System.stacktrace()}
+            :telemetry.execute([:phoenix, :router_dispatch, :failure], measurements, metadata)
             Plug.Conn.WrapperError.reraise(e)
         catch
           :error, reason ->
+            measurements = %{duration: System.monotonic_time() - start}
+            metadata = %{kind: :error, error: reason, stacktrace: System.stacktrace()}
+            :telemetry.execute([:phoenix, :router_dispatch, :failure], measurements, metadata)
             Plug.Conn.WrapperError.reraise(piped_conn, :error, reason, System.stacktrace())
         end
     end
