14f9bcec1ed76517c1e41ab4a88e49dfe24f4b8a
type1fool <historicstork@gmail.com>
2020-02-20 22:20:18 -0500

    update entry and output names

--- installer/templates/phx_assets/webpack.config.js
+++ installer/templates/phx_assets/webpack.config.js
@@ -14,10 +14,10 @@ module.exports = (env, options) => ({
     ]
   },
   entry: {
-    './js/app.js': glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
+    'app': glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
   },
   output: {
-    filename: 'app.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, '../priv/static/js'),
     publicPath: "/js/"
   },
