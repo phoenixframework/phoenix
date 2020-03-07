9adfb65f1e1b40116571289638e15600ad8799c3
Michael Crumm <mike@crumm.net>
2020-03-07 10:09:29 -0800

    Only invoke view_helpers/0 when generated for html apps
    This fixes a compilation error in the generated {app_name}_web.ex file.
    
    The view_helpers/0 function is not generated in the web context if "--no-html" is used, so we need to check for html before generating the line to invoke view_helpers().

--- installer/templates/phx_umbrella/apps/app_name_web/lib/app_name.ex
+++ installer/templates/phx_umbrella/apps/app_name_web/lib/app_name.ex
@@ -34,10 +34,10 @@ defmodule <%= web_namespace %> do
         namespace: <%= web_namespace %>
 
       # Import convenience functions from controllers
-      import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
+      import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]<%= if html do %>
 
       # Include shared imports and aliases for views
-      unquote(view_helpers())
+      unquote(view_helpers())<% end %>
     end
   end<%= if live do %>
 
