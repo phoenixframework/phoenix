ef313483bf4453b96864c55f6334853c03aada75
Michael Crumm <mike@crumm.net>
2020-03-07 11:33:27 -0800

    Generate view_helpers/0 for --no-html apps

--- installer/templates/phx_single/lib/app_name_web.ex
+++ installer/templates/phx_single/lib/app_name_web.ex
@@ -34,10 +34,10 @@ defmodule <%= web_namespace %> do
         namespace: <%= web_namespace %>
 
       # Import convenience functions from controllers
-      import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]<%= if html do %>
+      import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
 
       # Include shared imports and aliases for views
-      unquote(view_helpers())<% end %>
+      unquote(view_helpers())
     end
   end<%= if live do %>
 
@@ -73,21 +73,21 @@ defmodule <%= web_namespace %> do
       use Phoenix.Channel<%= if gettext do %>
       import <%= web_namespace %>.Gettext<% end %>
     end
-  end<%= if html do %>
+  end
 
   defp view_helpers do
-    quote do
+    quote do<%= if html do %>
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML<%= if live do %>
 
       # Import convenience functions for LiveView rendering
       import Phoenix.LiveView.Helpers<% end %>
-
+<% end %>
       import <%= web_namespace %>.ErrorHelpers<%= if gettext do %>
       import <%= web_namespace %>.Gettext<% end %>
       alias <%= web_namespace %>.Router.Helpers, as: Routes
     end
-  end<% end %>
+  end
 
   @doc """
   When used, dispatch to the appropriate controller/view/etc.
