ec01c3dfebfdcce8ec5f504d010d2876234790f8
Michael Crumm <mike@crumm.net>
2020-04-09 10:42:36 -0700

    Fix dashboard installer, closes #3740, closes #3751 (#3755)
    - Adds live_dashboard_path to the dead app.html.eex layout
    
    - Conditions live_dashboard layout link on existence so the generated tests pass
    
    - Ensures the dashboard works on new apps with --no-html
    
    Co-authored-by: José Valim <jose.valim@dashbit.co>

--- installer/templates/phx_web/router.ex
+++ installer/templates/phx_web/router.ex
@@ -19,23 +19,7 @@ defmodule <%= web_namespace %>.Router do
     pipe_through :browser
 
     <%= if live do %>live "/", PageLive, :index<% else %>get "/", PageController, :index<% end %>
-  end<%= if dashboard do %>
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() == :dev do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: <%= web_namespace %>.Telemetry
-    end
-  end<% end %>
+  end
 
   # Other scopes may use custom stacks.
   # scope "/api", <%= web_namespace %> do
@@ -44,5 +28,22 @@ defmodule <%= web_namespace %>.Router do
 
   scope "/api", <%= web_namespace %> do
     pipe_through :api
+  end<% end %><%= if dashboard do %>
+
+  # Enables LiveDashboard only for development
+  #
+  # If you want to use the LiveDashboard in production, you should put
+  # it behind authentication and allow only admins to access it.
+  # If your application does not have an admins-only section yet,
+  # you can use Plug.BasicAuth to set up some basic authentication
+  # as long as you are also using SSL (which you should anyway).
+  if Mix.env() in [:dev, :test] do
+    import Phoenix.LiveDashboard.Router
+
+    scope "/" do<%= if html do %>
+      pipe_through :browser<% else %>
+      pipe_through [:fetch_session, :protect_from_forgery]<% end %>
+      live_dashboard "/dashboard", metrics: <%= web_namespace %>.Telemetry
+    end
   end<% end %>
 end
