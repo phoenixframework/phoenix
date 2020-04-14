e6e809b586f5e55afebbfac3ec1d1b2c972c68e8
Wojtek Mach <wojtekmach@users.noreply.github.com>
2020-04-14 12:54:51 +0200

    Add `impl true` to PageLive from `mix phx.new --live` (#3756)

--- installer/templates/phx_live/live/page_live.ex
+++ installer/templates/phx_live/live/page_live.ex
@@ -1,14 +1,17 @@
 defmodule <%= web_namespace %>.PageLive do
   use <%= web_namespace %>, :live_view
 
+  @impl true
   def mount(_params, _session, socket) do
     {:ok, assign(socket, query: "", results: %{})}
   end
 
+  @impl true
   def handle_event("suggest", %{"q" => query}, socket) do
     {:noreply, assign(socket, results: search(query), query: query)}
   end
 
+  @impl true
   def handle_event("search", %{"q" => query}, socket) do
     case search(query) do
       %{^query => vsn} ->
