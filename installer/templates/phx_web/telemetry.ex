11e63ad5ed5eab098504907a234cf7b80ff84322
José Valim <jose.valim@dashbit.co>
2020-05-05 00:20:44 +0200

    Add a quick description to telemetry_poller

--- installer/templates/phx_web/telemetry.ex
+++ installer/templates/phx_web/telemetry.ex
@@ -9,6 +9,8 @@ defmodule <%= web_namespace %>.Telemetry do
   @impl true
   def init(_arg) do
     children = [
+      # Telemetry poller will execute the given period measurements
+      # every 10_000ms. Learn more here: https://hexdocs.pm/telemetry_metrics
       {:telemetry_poller, measurements: periodic_measurements(), period: 10_000}
       # Add reporters as children of your supervision tree.
       # {Telemetry.Metrics.ConsoleReporter, metrics: metrics()}
