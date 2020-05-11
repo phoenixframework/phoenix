e76eab4f62b7e49cb8920eb5b0aa747b1cdcdc3f
José Valim <jose.valim@dashbit.co>
2020-05-11 16:17:57 +0200

    Add comments to fallback controller, closes #3835

--- priv/templates/phx.gen.json/fallback_controller.ex
+++ priv/templates/phx.gen.json/fallback_controller.ex
@@ -6,14 +6,16 @@ defmodule <%= inspect context.web_module %>.FallbackController do
   """
   use <%= inspect context.web_module %>, :controller
 
-  <%= if schema.generate? do %>def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+  <%= if schema.generate? do %># This clause handles errors returned by Ecto's insert/update/delete.
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
     conn
     |> put_status(:unprocessable_entity)
     |> put_view(<%= inspect context.web_module %>.ChangesetView)
     |> render("error.json", changeset: changeset)
   end
 
-  <% end %>def call(conn, {:error, :not_found}) do
+  <% end %># This clause is an example of how to handle resources that cannot be found.
+  def call(conn, {:error, :not_found}) do
     conn
     |> put_status(:not_found)
     |> put_view(<%= inspect context.web_module %>.ErrorView)
