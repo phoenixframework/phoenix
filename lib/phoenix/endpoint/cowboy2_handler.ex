170ce44d5bb082a1bcc3edc4cdb0ceee480ad74b
Vince Foley <vince@newrelic.com>
2020-05-17 10:32:11 -0700

    Update conn after handler call

--- lib/phoenix/endpoint/cowboy2_handler.ex
+++ lib/phoenix/endpoint/cowboy2_handler.ex
@@ -60,6 +60,7 @@ defmodule Phoenix.Endpoint.Cowboy2Handler do
 
         {:plug, conn, handler, opts} ->
           %{adapter: {@connection, req}} =
+            conn =
             conn
             |> handler.call(opts)
             |> maybe_send(handler)
