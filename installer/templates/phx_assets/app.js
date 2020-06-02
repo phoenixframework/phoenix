1b401de1d5bc4aaae9e96aebe6f426dc8764e87d
Chris McCord <chris@chrismccord.com>
2020-06-02 14:26:34 -0400

    Update installer/templates/phx_assets/app.js

--- installer/templates/phx_assets/app.js
+++ installer/templates/phx_assets/app.js
@@ -29,7 +29,8 @@ liveSocket.connect()
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
-// >> liveSocket.enableLatencySim(1000)
+// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
+// >> liveSocket.disableLatencySim(1000)
 // Call disableLatencySim() to disable:
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
