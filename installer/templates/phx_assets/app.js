d9edfb014238cbe4815dcb27aac967b7c8323444
Sadjow Leão <sadjow@gmail.com>
2020-06-02 15:29:04 -0300

    Clarify usage of enableLatencySim in app.js (#3875)
    * Clarify usage of enableLatencySim in app.js
    
    Co-authored-by: Chris McCord <chris@chrismccord.com>

--- installer/templates/phx_assets/app.js
+++ installer/templates/phx_assets/app.js
@@ -29,6 +29,7 @@ liveSocket.connect()
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
-// >> liveSocket.enableLatencySim(1000)
+// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
+// >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
 <% end %>
