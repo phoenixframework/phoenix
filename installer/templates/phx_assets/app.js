eb8a97ffd9607e2d7b29129b64c04cbc2230000a
Chris McCord <chris@chrismccord.com>
2020-06-02 14:27:43 -0400

    Update installer/templates/phx_assets/app.js

--- installer/templates/phx_assets/app.js
+++ installer/templates/phx_assets/app.js
@@ -31,7 +31,6 @@ liveSocket.connect()
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim(1000)
-// Call disableLatencySim() to disable:
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
 <% end %>
