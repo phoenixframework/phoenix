cd308a09fb9f70794519ad4905570dfd1691f432
Henrik Nyh <henrik@nyh.se>
2020-06-10 17:58:14 +0100

    user_socket.ex docs: Grammar (#3883)

--- installer/templates/phx_web/channels/user_socket.ex
+++ installer/templates/phx_web/channels/user_socket.ex
@@ -20,7 +20,7 @@ defmodule <%= web_namespace %>.UserSocket do
     {:ok, socket}
   end
 
-  # Socket id's are topics that allow you to identify all sockets for a given user:
+  # Socket IDs are topics that allow you to identify all sockets for a given user:
   #
   #     def id(socket), do: "user_socket:#{socket.assigns.user_id}"
   #
