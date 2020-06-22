f8c87e166a95a3945905f5345b2891995afe5ae7
Max Veytsman <maxim@ontoillogical.com>
2020-06-22 17:38:43 -0400

    Fix bug in presence documentation example
    The example presence code has a bug where it's building a map of users keyed by id, but then using a key that it gets from the presences map to index into it. The users table is going to have integer primary keys, but presences keys are cast to a string (see https://hexdocs.pm/phoenix/Phoenix.Presence.html#c:track/3) 
    
    This means that the code as written will always return `nil` for the user.
    
    I'm not sure if adding `String.to_integer` here is the cleanest approach, please let me know if there's a better a way to do this!

--- lib/phoenix/presence.ex
+++ lib/phoenix/presence.ex
@@ -100,7 +100,7 @@ defmodule Phoenix.Presence do
         users = presences |> Map.keys() |> Accounts.get_users_map()
 
         for {key, %{metas: metas}} <- presences, into: %{} do
-          {key, %{metas: metas, user: users[key]}}
+          {key, %{metas: metas, user: users[String.to_integer(key)]}}
         end
       end
 
