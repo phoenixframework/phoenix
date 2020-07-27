f516082d5d59dbbb5932ff10ea5f1a93d9ccf462
njwest <njwest9@gmail.com>
2020-07-27 12:22:34 +0800

    Docs: Add Web to Presence module name

--- lib/phoenix/presence.ex
+++ lib/phoenix/presence.ex
@@ -21,7 +21,7 @@ defmodule Phoenix.Presence do
   which uses `Phoenix.Presence` and provide the `:otp_app` which
   holds your configuration, as well as the `:pubsub_server`.
 
-      defmodule MyApp.Presence do
+      defmodule MyAppWeb.Presence do
         use Phoenix.Presence,
           otp_app: :my_app,
           pubsub_server: MyApp.PubSub
