cb2a67ac02297f0aed95ae0c5c6ab2b613121572
Damir Vandic <info@dvic.io>
2020-09-16 19:52:02 +0200

    Replace remaining Mix.Config usage (#3989)

--- installer/templates/phx_umbrella/config/prod.secret.exs
+++ installer/templates/phx_umbrella/config/prod.secret.exs
@@ -2,4 +2,4 @@
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
-use Mix.Config
+import Config
