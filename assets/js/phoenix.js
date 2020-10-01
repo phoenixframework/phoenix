232a939060f4c0a516a0c9fdc40dc21d0658a50d
Chris McCord <chris@chrismccord.com>
2020-10-01 16:13:24 -0400

    Add binary serialization support to default serializers

--- assets/js/phoenix.js
+++ assets/js/phoenix.js
@@ -51,6 +51,13 @@
  * Successful joins receive an "ok" status, while unsuccessful joins
  * receive "error".
  *
+ * With the default serializers and WebSocket transport, JSON text frames are
+ * used for pushing a JSON object literal. If an `ArrayBuffer` instance is provided,
+ * binary encoding will be used and the message will be sent with the binary
+ * opcode.
+ *
+ * *Note*: binary messages are only supported on the WebSocket transport.
+ *
  * ## Duplicate Join Subscriptions
  *
  * While the client may join any number of topics on any number of channels,
@@ -662,17 +669,110 @@ export class Channel {
 
 /* The default serializer for encoding and decoding messages */
 export let Serializer = {
+  HEADER_LENGTH: 1,
+  META_LENGTH: 4,
+  KINDS: {push: 0, reply: 1, broadcast: 2},
+
   encode(msg, callback){
-    let payload = [
-      msg.join_ref, msg.ref, msg.topic, msg.event, msg.payload
-    ]
-    return callback(JSON.stringify(payload))
+    if(msg.payload.constructor === ArrayBuffer){
+      return callback(this.binaryEncode(msg))
+    } else {
+      let payload = [msg.join_ref, msg.ref, msg.topic, msg.event, msg.payload]
+      return callback(JSON.stringify(payload))
+    }
   },
 
   decode(rawPayload, callback){
-    let [join_ref, ref, topic, event, payload] = JSON.parse(rawPayload)
+    if(rawPayload.constructor === ArrayBuffer){
+      return callback(this.binaryDecode(rawPayload))
+    } else {
+      let [join_ref, ref, topic, event, payload] = JSON.parse(rawPayload)
+      return callback({join_ref, ref, topic, event, payload})
+    }
+  },
 
-    return callback({join_ref, ref, topic, event, payload})
+  // private
+
+  binaryEncode(message) {
+    let {join_ref, ref, event, topic, payload} = message
+    let metaLength = this.META_LENGTH + join_ref.length + ref.length + topic.length + event.length
+    let header = new ArrayBuffer(this.HEADER_LENGTH + metaLength)
+    let view = new DataView(header)
+    let offset = 0
+
+    view.setUint8(offset++, this.KINDS.push) // kind
+    view.setUint8(offset++, join_ref.length)
+    view.setUint8(offset++, ref.length)
+    view.setUint8(offset++, topic.length)
+    view.setUint8(offset++, event.length)
+    Array.from(join_ref, char => view.setUint8(offset++, char.charCodeAt(0)))
+    Array.from(ref, char => view.setUint8(offset++, char.charCodeAt(0)))
+    Array.from(topic, char => view.setUint8(offset++, char.charCodeAt(0)))
+    Array.from(event, char => view.setUint8(offset++, char.charCodeAt(0)))
+
+    var combined = new Uint8Array(header.byteLength + payload.byteLength)
+    combined.set(new Uint8Array(header), 0)
+    combined.set(new Uint8Array(payload), header.byteLength)
+
+    return combined.buffer
+  },
+
+  binaryDecode(buffer){
+    let view = new DataView(buffer)
+    let kind = view.getUint8(0)
+    let decoder = new TextDecoder()
+    switch(kind){
+      case this.KINDS.push:      return this.decodePush(buffer, view, decoder)
+      case this.KINDS.reply:     return this.decodeReply(buffer, view, decoder)
+      case this.KINDS.broadcast: return this.decodeBroadcast(buffer, view, decoder)
+    }
+  },
+
+  decodePush(buffer, view, decoder){
+    let joinRefSize = view.getUint8(1)
+    let topicSize = view.getUint8(2)
+    let eventSize = view.getUint8(3)
+    let offset = this.HEADER_LENGTH + this.META_LENGTH - 1 // pushes have no ref
+    let joinRef = decoder.decode(buffer.slice(offset, offset + joinRefSize))
+    offset = offset + joinRefSize
+    let topic = decoder.decode(buffer.slice(offset, offset + topicSize))
+    offset = offset + topicSize
+    let event = decoder.decode(buffer.slice(offset, offset + eventSize))
+    offset = offset + eventSize
+    let data = buffer.slice(offset, buffer.byteLength)
+    return {join_ref: joinRef, ref: null, topic: topic, event: event, payload: data}
+  },
+
+  decodeReply(buffer, view, decoder){
+    let joinRefSize = view.getUint8(1)
+    let refSize = view.getUint8(2)
+    let topicSize = view.getUint8(3)
+    let eventSize = view.getUint8(4)
+    let offset = this.HEADER_LENGTH + this.META_LENGTH
+    let joinRef = decoder.decode(buffer.slice(offset, offset + joinRefSize))
+    offset = offset + joinRefSize
+    let ref = decoder.decode(buffer.slice(offset, offset + refSize))
+    offset = offset + refSize
+    let topic = decoder.decode(buffer.slice(offset, offset + topicSize))
+    offset = offset + topicSize
+    let event = decoder.decode(buffer.slice(offset, offset + eventSize))
+    offset = offset + eventSize
+    let data = buffer.slice(offset, buffer.byteLength)
+    let payload = {status: event, response: data}
+    return {join_ref: joinRef, ref: ref, topic: topic, event: CHANNEL_EVENTS.reply, payload: payload}
+  },
+
+  decodeBroadcast(buffer, view, decoder){
+    let topicSize = view.getUint8(1)
+    let eventSize = view.getUint8(2)
+    let offset = this.HEADER_LENGTH + 2
+    let topic = decoder.decode(buffer.slice(offset, offset + topicSize))
+    offset = offset + topicSize
+    let event = decoder.decode(buffer.slice(offset, offset + eventSize))
+    offset = offset + eventSize
+    let data = buffer.slice(offset, buffer.byteLength)
+
+    return {join_ref: null, ref: null, topic: topic, event: event, payload: data}
   }
 }
 
@@ -754,8 +854,8 @@ export class Socket {
     this.ref                  = 0
     this.timeout              = opts.timeout || DEFAULT_TIMEOUT
     this.transport            = opts.transport || global.WebSocket || LongPoll
-    this.defaultEncoder       = Serializer.encode
-    this.defaultDecoder       = Serializer.decode
+    this.defaultEncoder       = Serializer.encode.bind(Serializer)
+    this.defaultDecoder       = Serializer.decode.bind(Serializer)
     this.closeWasClean        = false
     this.unloaded             = false
     this.binaryType           = opts.binaryType || "arraybuffer"
@@ -1111,7 +1211,7 @@ export class Socket {
 
   abnormalClose(reason){
     this.closeWasClean = false
-    this.conn.close(WS_CLOSE_NORMAL, reason)
+    if(this.conn.readyState === SOCKET_STATES.open){ this.conn.close(WS_CLOSE_NORMAL, reason) }
   }
 
   flushSendBuffer(){
--- test/phoenix/socket/v2_json_serializer_test.exs
+++ test/phoenix/socket/v2_json_serializer_test.exs
@@ -3,14 +3,74 @@ defmodule Phoenix.Socket.V2.JSONSerializerTest do
   alias Phoenix.Socket.{Broadcast, Message, Reply, V2}
 
   @serializer V2.JSONSerializer
-  @v2_fastlane_json "[null,null,\"t\",\"e\",\"m\"]"
-  @v2_reply_json "[null,null,\"t\",\"phx_reply\",{\"response\":\"m\",\"status\":null}]"
-  @v2_msg_json "[null,null,\"t\",\"e\",\"m\"]"
+  @v2_fastlane_json "[null,null,\"t\",\"e\",{\"m\":1}]"
+  @v2_reply_json "[null,null,\"t\",\"phx_reply\",{\"response\":{\"m\":1},\"status\":null}]"
+  @v2_msg_json "[null,null,\"t\",\"e\",{\"m\":1}]"
+
+  @client_push <<
+    # push
+    0::size(8),
+    # join_ref_size
+    2,
+    # ref_size
+    3,
+    # topic_size
+    5,
+    # event_size
+    5,
+    "12",
+    "123",
+    "topic",
+    "event",
+    101,
+    102,
+    103
+  >>
+
+  @reply <<
+    # reply
+    1::size(8),
+    # join_ref_size
+    2,
+    # ref_size
+    3,
+    # topic_size
+    5,
+    # status_size
+    2,
+    "12",
+    "123",
+    "topic",
+    "ok",
+    101,
+    102,
+    103
+  >>
+
+  @broadcast <<
+    # broadcast
+    2::size(8),
+    # topic_size
+    5,
+    # event_size
+    5,
+    "topic",
+    "event",
+    101,
+    102,
+    103
+  >>
 
   def encode!(serializer, msg) do
-    {:socket_push, :text, encoded} = serializer.encode!(msg)
-    assert is_list(encoded)
-    IO.iodata_to_binary(encoded)
+    case serializer.encode!(msg) do
+      {:socket_push, :text, encoded} ->
+        assert is_list(encoded)
+        IO.iodata_to_binary(encoded)
+
+      {:socket_push, :binary, encoded} ->
+        assert is_binary(encoded)
+        encoded
+    end
   end
 
   def decode!(serializer, msg, opts \\ []) do
@@ -18,28 +78,166 @@ defmodule Phoenix.Socket.V2.JSONSerializerTest do
   end
 
   def fastlane!(serializer, msg) do
-    {:socket_push, :text, encoded} = serializer.fastlane!(msg)
-    assert is_list(encoded)
-    IO.iodata_to_binary(encoded)
+    case serializer.fastlane!(msg) do
+      {:socket_push, :text, encoded} ->
+        assert is_list(encoded)
+        IO.iodata_to_binary(encoded)
+
+      {:socket_push, :binary, encoded} ->
+        assert is_binary(encoded)
+        encoded
+    end
   end
 
   test "encode!/1 encodes `Phoenix.Socket.Message` as JSON" do
-    msg = %Message{topic: "t", event: "e", payload: "m"}
+    msg = %Message{topic: "t", event: "e", payload: %{m: 1}}
     assert encode!(@serializer, msg) == @v2_msg_json
   end
 
+  test "encode!/1 raises when payload is not a map" do
+    msg = %Message{topic: "t", event: "e", payload: "invalid"}
+    assert_raise ArgumentError, fn -> encode!(@serializer, msg) end
+  end
+
   test "encode!/1 encodes `Phoenix.Socket.Reply` as JSON" do
-    msg = %Reply{topic: "t", payload: "m"}
+    msg = %Reply{topic: "t", payload: %{m: 1}}
     assert encode!(@serializer, msg) == @v2_reply_json
   end
 
   test "decode!/2 decodes `Phoenix.Socket.Message` from JSON" do
-    assert %Message{topic: "t", event: "e", payload: "m"} ==
-      decode!(@serializer, @v2_msg_json)
+    assert %Message{topic: "t", event: "e", payload: %{"m" => 1}} ==
+             decode!(@serializer, @v2_msg_json, opcode: :text)
   end
 
   test "fastlane!/1 encodes a broadcast into a message as JSON" do
-    msg = %Broadcast{topic: "t", event: "e", payload: "m"}
+    msg = %Broadcast{topic: "t", event: "e", payload: %{m: 1}}
     assert fastlane!(@serializer, msg) == @v2_fastlane_json
+  end
+
+  test "fastlane!/1 raises when payload is not a map" do
+    msg = %Broadcast{topic: "t", event: "e", payload: "invalid"}
+    assert_raise ArgumentError, fn -> fastlane!(@serializer, msg) end
+  end
+
+  describe "binary encode" do
+    test "general pushed message" do
+      push = <<
+        # push
+        0::size(8),
+        # join_ref_size
+        2,
+        # topic_size
+        5,
+        # event_size
+        5,
+        "12",
+        "topic",
+        "event",
+        101,
+        102,
+        103
+      >>
+
+      assert encode!(@serializer, %Phoenix.Socket.Message{
+               join_ref: "12",
+               ref: nil,
+               topic: "topic",
+               event: "event",
+               payload: {:binary, <<101, 102, 103>>}
+             }) == push
+    end
+
+    test "encode with oversized headers" do
+      assert_raise ArgumentError, ~r/unable to convert topic to binary/, fn ->
+        encode!(@serializer, %Phoenix.Socket.Message{
+          join_ref: "12",
+          ref: nil,
+          topic: String.duplicate("t", 256),
+          event: "event",
+          payload: {:binary, <<101, 102, 103>>}
+        })
+      end
+
+      assert_raise ArgumentError, ~r/unable to convert event to binary/, fn ->
+        encode!(@serializer, %Phoenix.Socket.Message{
+          join_ref: "12",
+          ref: nil,
+          topic: "topic",
+          event: String.duplicate("e", 256),
+          payload: {:binary, <<101, 102, 103>>}
+        })
+      end
+
+      assert_raise ArgumentError, ~r/unable to convert join_ref to binary/, fn ->
+        encode!(@serializer, %Phoenix.Socket.Message{
+          join_ref: String.duplicate("j", 256),
+          ref: nil,
+          topic: "topic",
+          event: "event",
+          payload: {:binary, <<101, 102, 103>>}
+        })
+      end
+    end
+
+    test "reply" do
+      assert encode!(@serializer, %Phoenix.Socket.Reply{
+               join_ref: "12",
+               ref: "123",
+               topic: "topic",
+               status: :ok,
+               payload: {:binary, <<101, 102, 103>>}
+             }) == @reply
+    end
+
+    test "reply with oversized headers" do
+      assert_raise ArgumentError, ~r/unable to convert ref to binary/, fn ->
+        encode!(@serializer, %Phoenix.Socket.Reply{
+          join_ref: "12",
+          ref: String.duplicate("r", 256),
+          topic: "topic",
+          status: :ok,
+          payload: {:binary, <<101, 102, 103>>}
+        })
+      end
+    end
+
+    test "fastlane" do
+      assert fastlane!(@serializer, %Phoenix.Socket.Broadcast{
+               topic: "topic",
+               event: "event",
+               payload: {:binary, <<101, 102, 103>>}
+             }) == @broadcast
+    end
+
+    test "fastlane with oversized headers" do
+      assert_raise ArgumentError, ~r/unable to convert topic to binary/, fn ->
+        fastlane!(@serializer, %Phoenix.Socket.Broadcast{
+          topic: String.duplicate("t", 256),
+          event: "event",
+          payload: {:binary, <<101, 102, 103>>}
+        })
+      end
+
+      assert_raise ArgumentError, ~r/unable to convert event to binary/, fn ->
+        fastlane!(@serializer, %Phoenix.Socket.Broadcast{
+          topic: "topic",
+          event: String.duplicate("e", 256),
+          payload: {:binary, <<101, 102, 103>>}
+        })
+      end
+
+    end
+  end
+
+  describe "binary decode" do
+    test "pushed message" do
+      assert decode!(@serializer, @client_push, opcode: :binary) == %Phoenix.Socket.Message{
+               join_ref: "12",
+               ref: "123",
+               topic: "topic",
+               event: "event",
+               payload: {:binary, <<101, 102, 103>>}
+             }
+    end
   end
 end
