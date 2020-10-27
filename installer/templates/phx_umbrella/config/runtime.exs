7c997c3ceb8fc952009ec82db6db140248e84c23
Landon Grindheim <landon.grindheim@gmail.com>
2020-10-27 14:13:29 -0400

    Reorder entries in umbrella config/runtime.exs

--- installer/templates/phx_umbrella/config/runtime.exs
+++ installer/templates/phx_umbrella/config/runtime.exs
@@ -1,7 +1,5 @@
-import Config
 # In this file, we load production configuration and secrets
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
-if config_env() == :prod do
-end
+import Config
