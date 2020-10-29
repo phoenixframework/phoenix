d208bc040eef5e6a895c337a50cb5329a9de15bd
José Valim <jose.valim@gmail.com>
2020-10-29 08:01:16 +0100

    Update runtime.exs

--- installer/templates/phx_umbrella/config/runtime.exs
+++ installer/templates/phx_umbrella/config/runtime.exs
@@ -1,8 +1,10 @@
-# In this file, we load production configuration and secrets
-# from environment variables. You can also hardcode secrets,
-# although such is generally not recommended and you have to
-# remember to add this file to your .gitignore.
 import Config
 
+# config/runtime.exs is executed for all environments, including
+# during releases. It is executed after compilation and before the
+# system starts, so it typically used load production configuration
+# and secrets from environment variables or elsewhere. Do not define
+# any compile-time configuration in here, as it won't be applied.
+# The block below contains prod specific runtime configuration.
 if config_env() == :prod do
 end
