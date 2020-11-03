d94ae677217b702848c89a803567f7bf53b1847d
levistovall <levi_stovall@yahoo.com>
2020-11-03 11:16:24 -0600

    Use Array filter in all cases until IE11 support is removed

--- assets/js/phoenix.js
+++ assets/js/phoenix.js
@@ -1241,27 +1241,11 @@ export class Socket {
     })
   }
 
-  getInternetExplorerVersion() {
-    let rv = -1
-    if (navigator.appName == 'Microsoft Internet Explorer') {
-      let ua = navigator.userAgent;
-      let re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})")
-      if (re.exec(ua) != null)
-         rv = parseFloat(RegExp.$1)
-    }
-    return rv;
-  }
-
   leaveOpenTopic(topic) {
-    let ver = this.getInternetExplorerVersion()
     let dupChannelArrowExpr = c => c.topic === topic && (c.isJoined() || c.isJoining())
-    let dupChannel
-    if(ver>=11.0 && ver<12.0) {
-      dupChannel = Array.prototype.filter(dupChannelArrowExpr)[0]
-    }
-    else {
-      dupChannel = this.channels.find(dupChannelArrowExpr)
-    }
+
+    // When IE11 support is removed, update to use this.channel.find(dupChannelArrowExpr) instead
+    let dupChannel = Array.prototype.filter(dupChannelArrowExpr)[0]
     if(dupChannel){
       if(this.hasLogger()) this.log("transport", `leaving duplicate topic "${topic}"`)
       dupChannel.leave()
