17408c6293fd151e8956ce640002a4f2ca8eb5e8
Max Veytsman <maxim@ontoillogical.com>
2020-12-17 00:54:26 -0500

    Use `input_name` in LiveView error helpers
    This updates the generators in line with https://github.com/phoenixframework/phoenix_live_view/pull/1278.

--- installer/templates/phx_web/views/error_helpers.ex
+++ installer/templates/phx_web/views/error_helpers.ex
@@ -12,7 +12,7 @@ defmodule <%= @web_namespace %>.ErrorHelpers do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
       content_tag(:span, translate_error(error),
         class: "invalid-feedback",
-        phx_feedback_for: input_id(form, field)
+        phx_feedback_for: input_name(form, field)
       )
     end)
   end<% end %><%= if @gettext do %>
