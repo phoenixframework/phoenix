f2a93aa1f3915c328c4503c8e1e5f352495c67cd
Nathan Long <him@nathanmlong.com>
2021-01-05 11:00:16 -0500

    Link to details on IP address selection (#4127)

--- installer/templates/phx_single/config/runtime.exs
+++ installer/templates/phx_single/config/runtime.exs
@@ -18,6 +18,8 @@ if config_env() == :prod do
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: String.to_integer(System.get_env("PORT") || "4000")
     ],
