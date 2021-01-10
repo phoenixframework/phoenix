2ada2fe4324e27018b91dddd6add6905cf33ace6
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2021-01-10 09:06:27 +0100

    Remove HardSourceWebpackPlugin to prepare for Webpack 5 upgrade
    - It makes asset compilation crash when used with latest mini-css-extract-plugin
    - HardSourceWebpackPlugin is no longer needed with Webpack 5: https://github.com/mzgoddard/hard-source-webpack-plugin/issues/514#issuecomment-687621600

--- installer/templates/phx_assets/webpack.config.js
+++ installer/templates/phx_assets/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const glob = require('glob');
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
@@ -48,6 +47,5 @@ module.exports = (env, options) => {
       new MiniCssExtractPlugin({ filename: '../css/app.css' }),
       new CopyWebpackPlugin({ patterns: [{ from: "static/", to: "../" }] }),
     ]
-    .concat(devMode ? [new HardSourceWebpackPlugin()] : [])
   }
 };
