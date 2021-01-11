d0324efc440d562a618825a5f769ddc9f7009a4d
Aaron Renner <aaron.renner@gmail.com>
2021-01-11 06:09:59 -0700

    Remove duplicate test fixture module templates (#4145)

--- priv/templates/phx.gen.auth/context_fixtures.ex
+++
@@ -1,6 +0,0 @@
-defmodule <%= inspect context.module %>Fixtures do
-  @moduledoc """
-  This module defines test helpers for creating
-  entities via the `<%= inspect context.module %>` context.
-  """
-end
