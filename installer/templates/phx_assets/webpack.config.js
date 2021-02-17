7dfc362fdf9e4f54d5b1c6f8e29914739d7f12c4
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2021-02-17 08:30:11 +0100

    Fix double-quotes in webpack.config.js

--- installer/templates/phx_assets/webpack.config.js
+++ installer/templates/phx_assets/webpack.config.js
@@ -45,7 +45,7 @@ module.exports = (env, options) => {
     },
     plugins: [
       new MiniCssExtractPlugin({ filename: '../css/app.css' }),
-      new CopyWebpackPlugin({ patterns: [{ from: "static/", to: "../" }] }),
+      new CopyWebpackPlugin({ patterns: [{ from: 'static/', to: '../' }] }),
     ]
   }
 };
