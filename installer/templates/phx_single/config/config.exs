7fb23694adfeefdff2b2bdd00b6ee8f5129fbfcd
Metal <ted.thetnaungsoe@gmail.com>
2021-03-19 14:32:36 +0700

    Update comments to reflect changes from Mix.Config to Config

--- installer/templates/phx_single/config/config.exs
+++ installer/templates/phx_single/config/config.exs
@@ -1,5 +1,5 @@
 # This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
+# and its dependencies with the aid of the Config module.
 #
 # This configuration file is loaded before any dependency and
 # is restricted to this project.
