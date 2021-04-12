8a15bf1a00008cab731c02b19155d942b21cc7f6
Kurt <mrkurt@gmail.com>
2021-04-12 10:04:27 -0500

    comma

--- installer/templates/phx_ecto/repo.ex
+++ installer/templates/phx_ecto/repo.ex
@@ -1,6 +1,6 @@
 defmodule <%= @app_module %>.Repo do
   use Ecto.Repo,
     otp_app: :<%= @app_name %>,
-    adapter: <%= inspect @adapter_module %>
+    adapter: <%= inspect @adapter_module %>,
     socket_options: [:inet6]
 end
