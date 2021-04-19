17fdccec90de2ba3054fcdaf3def34ccfbab26d2
Kevin Lang <me@gnalck.com>
2021-04-19 12:47:01 -0400

    add missing comma

--- installer/lib/phx_new/generator.ex
+++ installer/lib/phx_new/generator.ex
@@ -353,7 +353,7 @@ defmodule Phx.New.Generator do
       prod_config: """
       # ssl: true,
       url: database_url,
-      pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+      pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
       socket_options: [:inet6]
       """
     ]
