5d4265e106abe2a654d1aa6f32d92f89fc50347a
Chris McCord <chris@chrismccord.com>
2021-05-10 11:55:17 -0400

    Release 1.5.9

--- installer/mix.exs
+++ installer/mix.exs
@@ -1,7 +1,7 @@
 defmodule Phx.New.MixProject do
   use Mix.Project
 
-  @version "1.5.8"
+  @version "1.5.9"
   @github_path "phoenixframework/phoenix"
   @url "https://github.com/#{@github_path}"
 
