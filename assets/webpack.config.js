e6781a082d8724d0ed9de42ca1f20e244e9e10b8
Brian Cardarella <bcardarella@gmail.com>
2021-05-29 14:21:05 -0400

    build phoenix.js for production, strip sourcemaps

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,27 +1,31 @@
 const path = require('path')
 
-module.exports = {
-  entry: './js/phoenix/index.js',
-  output: {
-    filename: 'phoenix.js',
-    path: path.resolve(__dirname, '../priv/static'),
-    library: {
-      name: 'phoenix',
-      type: 'umd'
+module.exports = (env, options) => {
+  const devMode = options.mode !== 'production';
+
+  return {
+    entry: './js/phoenix/index.js',
+    output: {
+      filename: 'phoenix.js',
+      path: path.resolve(__dirname, '../priv/static'),
+      library: {
+        name: 'phoenix',
+        type: 'umd'
+      },
+      globalObject: 'this'
     },
-    globalObject: 'this'
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: require.resolve('phoenix'),
-        loader: 'expose-loader',
-        options: {
-          exposes: ['Phoenix']
+    devtool: devMode ? 'source-map' : undefined,
+    module: {
+      rules: [
+        {
+          test: require.resolve('phoenix'),
+          loader: 'expose-loader',
+          options: {
+            exposes: ['Phoenix']
+          }
         }
-      }
-    ]
-  },
-  plugins: []
+      ]
+    },
+    plugins: []
+  }
 }
