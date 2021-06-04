2c41afe865e5cc4c131b5bc8cec105816c3b9374
Richard Michael <rmichael@edgeofthenet.org>
2021-06-04 15:10:46 +0200

    Group argument descriptions
    Argument descriptions are scattered, group them and include their
    respective explanations.  (An alternative would be to tersely list the
    arguments, and follow the entire list with explanations.)

--- lib/mix/tasks/phx.gen.live.ex
+++ lib/mix/tasks/phx.gen.live.ex
@@ -6,13 +6,17 @@ defmodule Mix.Tasks.Phx.Gen.Live do
 
       mix phx.gen.live Accounts User users name:string age:integer
 
-  The first argument is the context module followed by the schema module
-  and its plural name (used as the schema table name).
+  The first argument is the context module.  The context is an Elixir module
+  that serves as an API boundary for the given resource. A context often holds
+  many related resources.  Therefore, if the context already exists, it will be
+  augmented with functions for the given resource.
 
-  The context is an Elixir module that serves as an API boundary for
-  the given resource. A context often holds many related resources.
-  Therefore, if the context already exists, it will be augmented with
-  functions for the given resource.
+  The second argument is the schema module.  The schema is responsible for
+  mapping the database fields into an Elixir struct.
+
+  The remaining arguments are the schema module plural name (used as the schema
+  table name), and an optional list of attributes as their respective names and
+  types.  See `mix help phx.gen.schema` for more information on attributes.
 
   When this command is run for the first time, a `ModalComponent` and
   `LiveHelpers` module will be created, along with the resource level
@@ -21,11 +25,6 @@ defmodule Mix.Tasks.Phx.Gen.Live do
 
   > Note: A resource may also be split
   > over distinct contexts (such as `Accounts.User` and `Payments.User`).
-
-  The schema is responsible for mapping the database fields into an
-  Elixir struct. It is followed by an optional list of attributes,
-  with their respective names and types. See `mix help phx.gen.schema`
-  for more information on attributes.
 
   Overall, this generator will add the following files:
 
