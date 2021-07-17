36fbd7f473b5682074ec71c066235f3aac5026d2
José Valim <jose.valim@dashbit.co>
2021-07-17 15:18:48 +0200

    Make watcher a MFA

--- test/phoenix/endpoint/watcher_test.exs
+++ test/phoenix/endpoint/watcher_test.exs
@@ -14,7 +14,7 @@ defmodule Phoenix.Endpoint.WatcherTest do
 
   test "starts watching and writes to stdio with fun" do
     assert capture_io(fn ->
-      {:ok, pid} = Watcher.start_link({"echo", fn -> IO.puts("hello") end})
+      {:ok, pid} = Watcher.start_link({"echo", {IO, :puts, ["hello"]}})
       ref = Process.monitor(pid)
       assert_receive {:DOWN, ^ref, :process, ^pid, :normal}, 1000
     end) == "hello\n"
