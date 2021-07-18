b88b17461feaf065dcc143f38d63c0534fef0578
José Valim <jose.valim@dashbit.co>
2021-07-19 01:23:11 +0200

    Update installer/templates/phx_assets/app.js
    Co-authored-by: Max Veytsman <maxim@ontoillogical.com>

--- installer/templates/phx_assets/app.js
+++ installer/templates/phx_assets/app.js
@@ -1,4 +1,5 @@
 // We import the CSS which is extracted to its own file by esbuild.
+// Remove this line if you add a your own CSS build pipeline (e.g postcss)
 import "../css/app.css"
 
 // You can bring dependencies in two ways.
