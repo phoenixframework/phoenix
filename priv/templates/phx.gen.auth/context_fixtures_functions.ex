e038f618809298212826d3952affdade6ca988b4
José Valim <jose.valim@gmail.com>
2021-07-19 18:45:53 +0200

    Update priv/templates/phx.gen.auth/context_fixtures_functions.ex

--- priv/templates/phx.gen.auth/context_fixtures_functions.ex
+++ priv/templates/phx.gen.auth/context_fixtures_functions.ex
@@ -19,6 +19,6 @@
 
   def extract_<%= schema.singular %>_token(fun) do
     {:ok, captured_email} = fun.(&"[TOKEN]#{&1}[TOKEN]")
-    [_, token, _] = String.split(captured_email.text_body, "[TOKEN]")
+    [_, token | _] = String.split(captured_email.text_body, "[TOKEN]")
     token
   end
