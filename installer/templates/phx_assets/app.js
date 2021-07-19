f2b9a32a5a84526210eb4af89e65fdae4866e7a2
José Valim <jose.valim@dashbit.co>
2021-07-19 15:19:05 +0200

    Add more comments

--- installer/templates/phx_assets/app.js
+++ installer/templates/phx_assets/app.js
@@ -1,6 +1,10 @@
 // We import the CSS which is extracted to its own file by esbuild.
-// Remove this line if you add a your own CSS build pipeline (e.g postcss)
+// Remove this line if you add a your own CSS build pipeline (e.g postcss).
 import "../css/app.css"
+
+// If you want to use Phoenix channels, run `mix help phx.gen.channel`
+// to get started and then uncomment the line below.
+// import "./user_socket.js"
 
 // You can include dependencies in two ways.
 //
