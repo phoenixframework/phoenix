b6310d0ebf5e7bae66a35ee523b33853358c0d7d
Philip Sampaio <philip.sampaio@gmail.com>
2021-07-23 04:03:47 -0300

    Add phx.gen.socket to generate a socket file (#4383)
    The socket file was removed from the installer, so this task is needed
    when the user wants to work with websockets outside LiveView.
    
    This commit also changes the `mix phx.gen.channel` task to include
    instructions regarding this new task when the default socket -
    `user_socket.ex` does not exist under `/channels`.

---
+++ priv/templates/phx.gen.socket/socket.ex
@@ -0,0 +1,54 @@
+defmodule <%= module %>Socket do
+  use Phoenix.Socket
+
+  # A Socket handler
+  #
+  # It's possible to control the websocket connection and
+  # assign values that can be accessed by your channel topics.
+
+  ## Channels<%= if existing_channel do %>
+
+  channel "<%= existing_channel[:singular] %>:*", <%= existing_channel[:module] %>Channel
+<% else %>
+  # Uncomment the following line to define a "room:*" topic
+  # pointing to the `<%= web_module %>.RoomChannel`:
+  #
+  # channel "room:*", <%= web_module %>.RoomChannel
+  #
+  # To create a channel file, use the mix task:
+  #
+  #     mix phx.gen.channel Room
+  #
+  # See the [`Channels guide`](https://hexdocs.pm/phoenix/channels.html)
+  # for futher details.
+
+<% end %>
+  # Socket params are passed from the client and can
+  # be used to verify and authenticate a user. After
+  # verification, you can put default assigns into
+  # the socket that will be set for all channels, ie
+  #
+  #     {:ok, assign(socket, :user_id, verified_user_id)}
+  #
+  # To deny connection, return `:error`.
+  #
+  # See `Phoenix.Token` documentation for examples in
+  # performing token verification on connect.
+  @impl true
+  def connect(_params, socket, _connect_info) do
+    {:ok, socket}
+  end
+
+  # Socket id's are topics that allow you to identify all sockets for a given user:
+  #
+  #     def id(socket), do: "user_socket:#{socket.assigns.user_id}"
+  #
+  # Would allow you to broadcast a "disconnect" event and terminate
+  # all active sockets and channels for a given user:
+  #
+  #     <%= endpoint_module %>.broadcast("user_socket:#{user.id}", "disconnect", %{})
+  #
+  # Returning `nil` makes this socket anonymous.
+  @impl true
+  def id(_socket), do: nil
+end
