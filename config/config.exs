a74fdee11bc86a28cc192216ea80e33cf5f777cf
José Valim <jose.valim@dashbit.co>
2021-08-01 19:15:39 +0200

    Only config esbuild in dev

--- config/config.exs
+++ config/config.exs
@@ -8,13 +8,24 @@ config :phoenix, :json_library, Jason
 
 config :phoenix, :trim_on_html_eex_engine, false
 
-esbuild_base =  [
-  cd: Path.expand("../assets", __DIR__),
-  env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
-]
+if Mix.env() == :dev do
+  esbuild = fn args ->
+    [
+      args: ~w(./js/phoenix --bundle) ++ args,
+      cd: Path.expand("../assets", __DIR__),
+      env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+    ]
+  end
 
-config :esbuild,
-  version: "0.12.15",
-  module: esbuild_base ++ [args: ~w(./js/phoenix --bundle --format=esm --sourcemap --outfile=../priv/static/phoenix.esm.js)],
-  cdn: esbuild_base ++ [args: ~w(./js/phoenix --bundle --target=es2016 --format=iife --global-name=Phoenix --outfile=../priv/static/phoenix.js)],
-  cdn_min: esbuild_base ++ [args: ~w(./js/phoenix --bundle --target=es2016 --format=iife --global-name=Phoenix --minify --outfile=../priv/static/phoenix.min.js)]
+  config :esbuild,
+    version: "0.12.17",
+    module: esbuild.(~w(--format=esm --sourcemap --outfile=../priv/static/phoenix.esm.js)),
+    cdn:
+      esbuild.(
+        ~w(--target=es2016 --format=iife --global-name=Phoenix --outfile=../priv/static/phoenix.js)
+      ),
+    cdn_min:
+      esbuild.(
+        ~w(--target=es2016 --format=iife --global-name=Phoenix --minify --outfile=../priv/static/phoenix.min.js)
+      )
+end
