0da9b5c1865946eaeb3a66670955e95b1abce8bf
José Valim <jose.valim@dashbit.co>
2021-08-07 14:40:35 +0200

    Add more docs to phx.gen.auth tokens (#4405)

--- priv/templates/phx.gen.auth/schema_token.ex
+++ priv/templates/phx.gen.auth/schema_token.ex
@@ -27,6 +27,20 @@ defmodule <%= inspect schema.module %>Token do
   Generates a token that will be stored in a signed place,
   such as session or cookie. As they are signed, those
   tokens do not need to be hashed.
+
+  The reason why we store session tokens in the database, even
+  though Phoenix already provides a session cookie, is because
+  Phoenix' default session cookies are not persisted, they are
+  simply signed and potentially encrypted. This means they are
+  valid indefinitely, unless you change the signing/encryption
+  salt.
+
+  Therefore, storing them allows individual <%= schema.singular %>
+  sessions to be expired. The token system can also be extended
+  to store additional data, such as the device used for logging in.
+  You could then use this information to display all valid sessions
+  and devices in the UI and allow users to explicitly expire any
+  session they deem invalid.
   """
   def build_session_token(<%= schema.singular %>) do
     token = :crypto.strong_rand_bytes(@rand_size)
@@ -36,7 +50,10 @@ defmodule <%= inspect schema.module %>Token do
   @doc """
   Checks if the token is valid and returns its underlying lookup query.
 
-  The query returns the <%= schema.singular %> found by the token.
+  The query returns the <%= schema.singular %> found by the token, if any.
+
+  The token is valid if it matches the value in the database and it has
+  not expired (after @session_validity_in_days).
   """
   def verify_session_token_query(token) do
     query =
@@ -49,12 +66,17 @@ defmodule <%= inspect schema.module %>Token do
   end
 
   @doc """
-  Builds a token with a hashed counter part.
+  Builds a token and its hash to be delivered to the <%= schema.singular %>'s email.
 
   The non-hashed token is sent to the <%= schema.singular %> email while the
-  hashed part is stored in the database, to avoid reconstruction.
-  The token is valid for a week as long as <%= schema.singular %>s don't change
-  their email.
+  hashed part is stored in the database. The original token cannot be reconstructed,
+  which means anyone with read-only access to the database cannot directly use
+  the token in the application to gain access. Furthermore, if the user changes
+  their email in the system, the tokens sent to the previous email are no longer
+  valid.
+
+  Users can easily adapt the existing code to provide other types of delivery methods,
+  for example, by phone numbers.
   """
   def build_email_token(<%= schema.singular %>, context) do
     build_hashed_token(<%= schema.singular %>, context, <%= schema.singular %>.email)
@@ -76,7 +98,15 @@ defmodule <%= inspect schema.module %>Token do
   @doc """
   Checks if the token is valid and returns its underlying lookup query.
 
-  The query returns the <%= schema.singular %> found by the token.
+  The query returns the <%= schema.singular %> found by the token, if any.
+
+  The given token is valid if it matches its hashed counterpart in the
+  database and the user email has not changed. This function also checks
+  if the token is being used within a certain period, depending on the
+  context. The default contexts supported by this function are either
+  "confirm", for account confirmation emails, and "reset_password",
+  for resetting the password. For verifying requests to change the email,
+  see `verify_change_email_token_query/2`.
   """
   def verify_email_token_query(token, context) do
     case Base.url_decode64(token, padding: false) do
@@ -103,9 +133,18 @@ defmodule <%= inspect schema.module %>Token do
   @doc """
   Checks if the token is valid and returns its underlying lookup query.
 
-  The query returns the <%= schema.singular %> token record.
+  The query returns the <%= schema.singular %> found by the token, if any.
+
+  This is used to validate requests to change the <%= schema.singular %>
+  email. It is different from `verify_email_token_query/2` precisely because
+  `verify_email_token_query/2` validates the email has not changed, which is
+  the starting point by this function.
+
+  The given token is valid if it matches its hashed counterpart in the
+  database and if it has not expired (after @change_email_validity_in_days).
+  The context must always start with "change:".
   """
-  def verify_change_email_token_query(token, context) do
+  def verify_change_email_token_query(token, "change:" <> _ = context) do
     case Base.url_decode64(token, padding: false) do
       {:ok, decoded_token} ->
         hashed_token = :crypto.hash(@hash_algorithm, decoded_token)
@@ -122,7 +161,7 @@ defmodule <%= inspect schema.module %>Token do
   end
 
   @doc """
-  Returns the given token with the given context.
+  Returns the token struct for the given token value and context.
   """
   def token_and_context_query(token, context) do
     from <%= inspect schema.module %>Token, where: [token: ^token, context: ^context]
