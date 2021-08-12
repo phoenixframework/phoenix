0466d02b85091b13b1f79ce9a6ef870dc4783d5d
mad <mad42@users.noreply.github.com>
2021-08-12 10:39:51 +0000

    use on_mount/1

--- priv/templates/phx.gen.auth/live_auth.ex
+++ priv/templates/phx.gen.auth/live_auth.ex
@@ -5,49 +5,57 @@ defmodule <%= inspect context.web_module %>.<%= if schema.web_path, do: "#{Phoen
   @doc """
   Attaches current_<%= schema.singular %> to `socket` assigns based on <%= schema.singular %>_token or nil if it doesn't.
 
-  ## Example
-      # In the LiveView file
-      import <%= inspect context.web_module %>.<%= if schema.web_path, do: "#{Phoenix.Naming.humanize(schema.web_path)}.", else: "" %><%= Phoenix.Naming.humanize(schema.singular) %>LiveAuth
+  ## Examples
 
-      # LiveView mount
-      def mount(_params, session, socket) do
-        socket = mount_current_<%= schema.singular %>(socket, session)
-        {:ok, socket}
+      # In the LiveView file
+      defmodule DemoWeb.PageLive do
+        use Phoenix.LiveView
+
+        on_mount {<%= inspect context.web_module %>.<%= if schema.web_path, do: "#{Phoenix.Naming.humanize(schema.web_path)}.", else: "" %><%= Phoenix.Naming.humanize(schema.singular) %>LiveAuth, :mount_current_<%= schema.singular %>}
       end
 
   """
   def mount_current_<%= schema.singular %>(socket, %{"<%=schema.singular %>_token" => <%= schema.singular %>_token}) do
-    assign_new(socket, :current_<%= schema.singular %>, fn ->
+    socket = assign_new(socket, :current_<%= schema.singular %>, fn ->
       <%= inspect context.module %>.get_<%= schema.singular %>_by_session_token(<%= schema.singular %>_token)
     end)
+
+    {:cont, socket}
   end
 
   def mount_current_<%= schema.singular %>(socket, _session) do
-    assign_new(socket, :current_<%= schema.singular %>, fn -> nil end)
+    {:cont, assign_new(socket, :current_<%= schema.singular %>, fn -> nil end)}
   end
 
   @doc """
   Attaches current_<%= schema.singular %> to `socket` assigns based on <%= schema.singular %>_token if the token exists.
   Redirect to login page if not.
 
-  ## Example
-      # In the LiveView file
-      import <%= inspect context.web_module %>.<%= if schema.web_path, do: "#{Phoenix.Naming.humanize(schema.web_path)}.", else: "" %><%= Phoenix.Naming.humanize(schema.singular) %>LiveAuth
+  ## Examples
 
-      # LiveView mount
-      def mount(_params, session, socket) do
-        socket = ensure_mounted_current_<%= schema.singular %>(socket, session)
-        {:ok, socket}
+      # In the LiveView file
+      defmodule DemoWeb.PageLive do
+        use Phoenix.LiveView
+
+        on_mount {<%= inspect context.web_module %>.<%= if schema.web_path, do: "#{Phoenix.Naming.humanize(schema.web_path)}.", else: "" %><%= Phoenix.Naming.humanize(schema.singular) %>LiveAuth, :ensure_mounted_current_<%= schema.singular %>}
       end
 
   """
   def ensure_mounted_current_<%= schema.singular %>(socket, %{"<%=schema.singular %>_token" => <%= schema.singular %>_token}) do
-    assign_new(socket, :current_<%= schema.singular %>, fn ->
+    socket = assign_new(socket, :current_<%= schema.singular %>, fn ->
       <%= inspect context.module %>.get_<%= schema.singular %>_by_session_token(<%= schema.singular %>_token)
     end)
+
+    case socket.assigns.current_<%= schema.singular %> do
+      nil ->
+        {:halt, push_redirect(socket, to: Routes.<%= schema.route_helper %>_session_path(socket, :new))}
+
+      _ ->
+        {:cont, socket}
+    end
   end
 
   def ensure_mounted_current_<%= schema.singular %>(socket, _session) do
-    push_redirect(socket, to: Routes.<%= schema.route_helper %>_session_path(socket, :new))
+    {:halt, push_redirect(socket, to: Routes.<%= schema.route_helper %>_session_path(socket, :new))}
   end
 end
