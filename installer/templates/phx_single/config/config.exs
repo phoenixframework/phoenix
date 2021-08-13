8704b735fbd0d65ce4ae169db351b7b7f29c5e87
Luke Strickland <luke@axxim.net>
2021-08-13 18:04:46 -0400

    Include Phoenix as an external to the esbuild args

--- installer/templates/phx_single/config/config.exs
+++ installer/templates/phx_single/config/config.exs
@@ -24,7 +24,7 @@ config :<%= @app_name %>, <%= @endpoint_module %>,
 config :esbuild,
   version: "0.12.18",
   default: [
-    args: ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets),
+    args: ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets --external:phoenix),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]<% end %><%= if @mailer do %>
