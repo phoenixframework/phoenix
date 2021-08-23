fb3e09b7dd88f6c977561c069d64947db103c2f5
José Valim <jose.valim@dashbit.co>
2021-08-23 11:38:56 +0200

    More docs on --all

--- lib/mix/tasks/phx.digest.ex
+++ lib/mix/tasks/phx.digest.ex
@@ -33,6 +33,9 @@ defmodule Mix.Tasks.Phx.Digest do
     * app-eb0a5b9302e8d32828d8a73f137cc8f0.js.gz
     * cache_manifest.json
 
+  You can use `mix phx.digest.clean` to prune stale versions of the assets.
+  If you want to remove all produced files, run `mix phx.digest.clean --all`.
+
   ## vsn
 
   It is possible to digest the stylesheet asset references without the query
