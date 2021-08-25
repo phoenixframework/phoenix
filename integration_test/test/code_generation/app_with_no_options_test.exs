724ca43ff21f451fbed5850c9342896f83c43ec6
Chris McCord <chris@chrismccord.com>
2021-08-25 15:09:19 -0400

    No live

--- integration_test/test/code_generation/app_with_no_options_test.exs
+++ integration_test/test/code_generation/app_with_no_options_test.exs
@@ -12,7 +12,8 @@ defmodule Phoenix.Integration.CodeGeneration.AppWithNoOptionsTest do
           "--no-ecto",
           "--no-gettext",
           "--no-mailer",
-          "--no-dashboard"
+          "--no-dashboard",
+          "--no-live"
         ])
 
       assert_no_compilation_warnings(app_root_path)
