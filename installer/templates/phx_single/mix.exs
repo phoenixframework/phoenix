73874ac4c55e0f8984ad46ec7a20120b7ae0a5dc
Aswin Mohan <dev.aswinmohan@gmail.com>
2021-09-03 18:43:40 +0530

    Add --no-runtime-config option to esbuild
    Running `assets.deploy` in a CI/CD pipeline fails because `esbuild` reads the `config/runtime.exs` file which raises because the CI environment doesn't have access to the environment variables. The flag `--no-runtime-config` was added in https://github.com/phoenixframework/esbuild/commit/9779f2bc445dc2ae8413e0345bd35050f6cb9872 which would mitigate this issue.

--- installer/templates/phx_single/mix.exs
+++ installer/templates/phx_single/mix.exs
@@ -68,7 +68,7 @@ defmodule <%= @app_module %>.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]<% end %><%= if @assets do %>,
-      "assets.deploy": ["esbuild default --minify", "phx.digest"]<% end %>
+      "assets.deploy": ["esbuild --no-runtime-config default --minify", "phx.digest"]<% end %>
     ]
   end
 end
