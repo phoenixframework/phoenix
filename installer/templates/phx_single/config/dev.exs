731876994f3f3eced478bfc84db15bd7e8b47bf9
José Valim <jose.valim@dashbit.co>
2021-09-07 18:10:11 +0200

    No secret key base fallback, closes #4465

--- installer/templates/phx_single/config/dev.exs
+++ installer/templates/phx_single/config/dev.exs
@@ -10,9 +10,10 @@ config :<%= @app_name %>, <%= @endpoint_module %>,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
   http: [ip: {127, 0, 0, 1}, port: 4000],
-  debug_errors: true,
-  code_reloader: true,
   check_origin: false,
+  code_reloader: true,
+  debug_errors: true,
+  secret_key_base: "<%= @secret_key_base_dev %>",
   watchers: <%= if @assets do %>[
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
     esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
--- installer/templates/phx_umbrella/apps/app_name_web/config/config.exs
+++ installer/templates/phx_umbrella/apps/app_name_web/config/config.exs
@@ -7,7 +7,6 @@ config :<%= @web_app_name %><%= if @namespaced? do %>,
 <% end %># Configures the endpoint
 config :<%= @web_app_name %>, <%= @endpoint_module %>,
   url: [host: "localhost"],
-  secret_key_base: "<%= @secret_key_base %>",
   render_errors: [view: <%= @web_namespace %>.ErrorView, accepts: ~w(<%= if @html do %>html <% end %>json), layout: false],
   pubsub_server: <%= @app_module %>.PubSub,
   live_view: [signing_salt: "<%= @lv_signing_salt %>"]<%= if @assets do %>
