61a24e41d594482279cb25f52e86f97f7f8b1151
Barbaric Corgi <BarbaricCorgi@users.noreply.github.com>
2021-09-24 15:12:24 -0400

    Update phx.gen.ex
    Added markdown table to explain the relationship between the CRUD related generators

--- lib/mix/tasks/phx.gen.ex
+++ lib/mix/tasks/phx.gen.ex
@@ -5,6 +5,19 @@ defmodule Mix.Tasks.Phx.Gen do
 
   @moduledoc """
   Lists all available Phoenix generators.
+  
+  ## CRUD related generators
+
+Sometimes you just want to create an schema to validate an external entity and other times you just need a REST API or an HTML CRUD. check the following table to get an idea of how each of the CRUD related generators relate to each other
+
+| Tasks | Migration | Schema | Context | Controller | View | CRUD Templates | LiveView views/components |
+| ------ | ------ | ------ | ------ | ------ | ------ | ------ | ------ |
+| `phx.gen.schema` | Included | Included |  |  |  |  |  |
+| `phx.gen.embedded` |  | Included |  |  |  |  |  |
+| `phx.gen.context` | Included | Included | Included |  |  |  |  |
+| `phx.gen.json` | Included | Included | Included | Included | Included |  |  |
+| `phx.gen.html` | Included | Included | Included | Included | Included | Included |  |
+| `phx.gen.live` | Included | Included |  |  |  |  | Included |
   """
 
   def run(_args) do
