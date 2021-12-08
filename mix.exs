73bd3e6673dd544d4d85fd28b0566fd8db233a41
Chris McCord <chris@chrismccord.com>
2021-12-08 12:55:26 -0500

    Release 1.6.3

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Phoenix.MixProject do
     end
   end
 
-  @version "1.6.2"
+  @version "1.6.3"
   @scm_url "https://github.com/phoenixframework/phoenix"
 
   # If the elixir requirement is updated, we need to make the installer
