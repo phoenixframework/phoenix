92195b9137ff8c7b1cfd4e1922bf53175786a4d3
Leandro Cesquini Pereira <leandro@leandro.io>
2022-01-05 16:06:43 -0500

    Leverage MIX_ENV on the runner image (#4628)
    Make it explicit which env is used to run the app,
    the same way we use it to build the image.

--- test/mix/tasks/phx.gen.release_test.exs
+++ test/mix/tasks/phx.gen.release_test.exs
@@ -74,8 +74,6 @@ defmodule Mix.Tasks.Phx.Gen.ReleaseTest do
     end)
   end
 
-
-
   test "generates release and docker files", config do
     in_tmp_project(config.test, fn ->
       Gen.Release.run(["--docker", "--ecto"])
@@ -86,12 +84,7 @@ defmodule Mix.Tasks.Phx.Gen.ReleaseTest do
       end)
 
       assert_file("Dockerfile", fn file ->
-        assert file =~ ~S|COPY --from=builder --chown=nobody:root /app/_build/prod/rel/phoenix ./|
-        assert file =~ ~S|CMD /app/bin/server|
-      end)
-
-      assert_file("Dockerfile", fn file ->
-        assert file =~ ~S|COPY --from=builder --chown=nobody:root /app/_build/prod/rel/phoenix ./|
+        assert file =~ ~S|COPY --from=builder --chown=nobody:root /app/_build/${MIX_ENV}/rel/phoenix ./|
         assert file =~ ~S|CMD /app/bin/server|
       end)
 
