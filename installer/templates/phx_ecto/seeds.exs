ea5381e0a9255814ff5813a0a2739b5805f04453
Nelson Estevão <nelsonmestevao@gmail.com>
2022-02-03 22:25:35 +0000

    Add ecto.seed alias to generator
    I create an `ecto.seed` alias in every app that I use, I think it is very
    useful to make it available in the aliases because a lot of the time we
    want to add more seeding data without necessary running any migration.
    
    It is also much more easy to type.

--- installer/templates/phx_ecto/seeds.exs
+++ installer/templates/phx_ecto/seeds.exs
@@ -1,6 +1,6 @@
 # Script for populating the database. You can run it as:
 #
-#     mix run priv/repo/seeds.exs
+#     mix ecto.seed
 #
 # Inside the script, you can read and write to any of your
 # repositories directly:
--- installer/templates/phx_single/mix.exs
+++ installer/templates/phx_single/mix.exs
@@ -65,7 +65,8 @@ defmodule <%= @app_module %>.MixProject do
   defp aliases do
     [
       setup: ["deps.get"<%= if @ecto do %>, "ecto.setup"<% end %>]<%= if @ecto do %>,
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.seed": ["run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "ecto.seed"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]<% end %><%= if @assets do %>,
       "assets.deploy": ["esbuild default --minify", "phx.digest"]<% end %>
