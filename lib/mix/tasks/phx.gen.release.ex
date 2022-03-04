f4c7df3306d887cfbf7119e286159c8737c0159d
Aaron Renner <aaron.renner@gmail.com>
2022-03-04 13:03:06 -0700

    [phx.gen.release] Skip generating migration files when ecto_sql is not installed (#4711)

--- lib/mix/tasks/phx.gen.release.ex
+++ lib/mix/tasks/phx.gen.release.ex
@@ -18,6 +18,11 @@ defmodule Mix.Tasks.Phx.Gen.Release do
   Note, the `rel/overlays` directory is copied into the release build by default when
   running `mix release`.
 
+  To skip generating the migration-related files, use the `--no-ecto` flag. To
+  force these migration-related files to be generated, the use `--ecto` flag.
+
+  ## Docker
+
   When the `--docker` flag is passed, the following docker files are generated:
 
     * `Dockerfile` - The Dockerfile for use in any standard docker deployment
@@ -32,8 +37,7 @@ defmodule Mix.Tasks.Phx.Gen.Release do
 
   @doc false
   def run(args) do
-    docker? = "--docker" in args
-    ecto? = "--ecto" in args || Code.ensure_loaded?(Ecto)
+    opts = parse_args(args)
 
     if Mix.Project.umbrella?() do
       Mix.raise("""
@@ -60,7 +64,7 @@ defmodule Mix.Tasks.Phx.Gen.Release do
       {:eex, "rel/server.bat.eex", "rel/overlays/bin/server.bat"}
     ])
 
-    if ecto? do
+    if opts.ecto do
       Mix.Phoenix.copy_from(paths(), "priv/templates/phx.gen.release", binding, [
         {:eex, "rel/migrate.sh.eex", "rel/overlays/bin/migrate"},
         {:eex, "rel/migrate.bat.eex", "rel/overlays/bin/migrate.bat"},
@@ -68,7 +72,7 @@ defmodule Mix.Tasks.Phx.Gen.Release do
       ])
     end
 
-    if docker? do
+    if opts.docker do
       Mix.Phoenix.copy_from(paths(), "priv/templates/phx.gen.release", binding, [
         {:eex, "Dockerfile.eex", "Dockerfile"},
         {:eex, "dockerignore.eex", ".dockerignore"}
@@ -78,7 +82,7 @@ defmodule Mix.Tasks.Phx.Gen.Release do
     File.chmod!("rel/overlays/bin/server", 0o755)
     File.chmod!("rel/overlays/bin/server.bat", 0o755)
 
-    if ecto? do
+    if opts.ecto do
       File.chmod!("rel/overlays/bin/migrate", 0o755)
       File.chmod!("rel/overlays/bin/migrate.bat", 0o755)
     end
@@ -88,7 +92,7 @@ defmodule Mix.Tasks.Phx.Gen.Release do
     Your application is ready to be deployed in a release!
 
     See https://hexdocs.pm/mix/Mix.Tasks.Release.html for more information about Elixir releases.
-    #{if docker?, do: docker_instructions()}
+    #{if opts.docker, do: docker_instructions()}
     Here are some useful release commands you can run in any release environment:
 
         # To build a release
@@ -96,7 +100,7 @@ defmodule Mix.Tasks.Phx.Gen.Release do
 
         # To start your system with the Phoenix server running
         _build/dev/rel/#{app}/bin/server
-    #{if ecto?, do: ecto_instructions(app)}
+    #{if opts.ecto, do: ecto_instructions(app)}
     Once the release is running you can connect to it remotely:
 
         _build/dev/rel/#{app}/bin/#{app} remote
@@ -106,7 +110,7 @@ defmodule Mix.Tasks.Phx.Gen.Release do
         _build/dev/rel/#{app}/bin/#{app}
     """)
 
-    if ecto? do
+    if opts.ecto do
       post_install_instructions("config/runtime.exs", ~r/ECTO_IPV6/, """
       [warn] Conditional IPV6 support missing from runtime configuration.
 
@@ -141,6 +145,15 @@ defmodule Mix.Tasks.Phx.Gen.Release do
           ...,
           url: [host: host, port: 443]
     """)
+  end
+
+  defp parse_args(args) do
+    args
+    |> OptionParser.parse!(strict: [ecto: :boolean, docker: :boolean])
+    |> elem(0)
+    |> Keyword.put_new_lazy(:ecto, &ecto_sql_installed?/0)
+    |> Keyword.put_new(:docker, false)
+    |> Map.new()
   end
 
   defp ecto_instructions(app) do
@@ -189,4 +202,6 @@ defmodule Mix.Tasks.Phx.Gen.Release do
         "#{major}.0"
     end
   end
+
+  defp ecto_sql_installed?, do: Application.loaded_applications() |> List.keymember?(:ecto_sql, 0)
 end
