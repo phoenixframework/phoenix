61c410d03c681ad98ad4f9d118891567d7bb8637
Leandro Cesquini Pereira <leandro@leandro.io>
2022-04-18 12:39:29 -0400

    Add HTMLFormatter in formatter.exs templates (#4793)

--- installer/templates/phx_umbrella/apps/app_name/formatter.exs
+++ installer/templates/phx_umbrella/apps/app_name/formatter.exs
@@ -1,6 +1,6 @@
 [<%= if @ecto do %>
   import_deps: [:ecto],
-  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  subdirectories: ["priv/*/migrations"]<% else %>
-  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"]<% end %>
+  subdirectories: ["priv/*/migrations"],<% end %><%= if @html and Version.match?(System.version(), ">= 1.13.4") do %>
+  plugins: [Phoenix.LiveView.HTMLFormatter],<% end %>
+  inputs: [<%= if @html and Version.match?(System.version(), ">= 1.13.4") do %>"*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}"<% else %>"*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"<% end %><%= if @ecto do %>, "priv/*/seeds.exs"<% end %>]
 ]
