7bc17619add4bfa41b4b6582ea531b0519a28251
Chris McCord <chris@chrismccord.com>
2022-06-10 13:08:03 -0400

    Resolve conflicts

--- installer/templates/phx_assets/tailwind.config.js
+++ installer/templates/phx_assets/tailwind.config.js
@@ -1,6 +1,8 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
 
+const plugin = require("tailwindcss/plugin")
+
 module.exports = {
   content: [
     "./js/**/*.js",
@@ -11,6 +13,10 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require("@tailwindcss/forms")
+    require("@tailwindcss/forms"),
+    plugin(({addVariant}) => addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])),
+    plugin(({addVariant}) => addVariant("phx-click-loading", [".phx-click-loading&", ".phx-click-loading &"])),
+    plugin(({addVariant}) => addVariant("phx-submit-loading", [".phx-submit-loading&", ".phx-submit-loading &"])),
+    plugin(({addVariant}) => addVariant("phx-change-loading", [".phx-change-loading&", ".phx-change-loading &"]))
   ]
 }
