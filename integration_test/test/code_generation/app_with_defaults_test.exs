1c42da1232bf2de85db815a1805a28dc6e122a0a
Berenice Medel <bemesa21@gmail.com>
2022-07-14 15:46:43 -0500

    update integration tests

--- integration_test/test/code_generation/app_with_defaults_test.exs
+++ integration_test/test/code_generation/app_with_defaults_test.exs
@@ -174,11 +174,22 @@ defmodule Phoenix.Integration.CodeGeneration.AppWithDefaultsTest do
   end
 
   describe "phx.gen.auth + bcrypt" do
-    test "has no compilation or formatter warnings" do
+    test "has no compilation or formatter warnings (--live)" do
       with_installer_tmp("new with defaults", fn tmp_dir ->
         {app_root_path, _} = generate_phoenix_app(tmp_dir, "phx_blog")
 
-        mix_run!(~w(phx.gen.auth Accounts User users), app_root_path)
+        mix_run!(~w(phx.gen.auth Accounts User users --live), app_root_path)
+
+        assert_no_compilation_warnings(app_root_path)
+        assert_passes_formatter_check(app_root_path)
+      end)
+    end
+
+    test "has no compilation or formatter warnings (--no-live)" do
+      with_installer_tmp("new with defaults", fn tmp_dir ->
+        {app_root_path, _} = generate_phoenix_app(tmp_dir, "phx_blog")
+
+        mix_run!(~w(phx.gen.auth Accounts User users --no-live), app_root_path)
 
         assert_no_compilation_warnings(app_root_path)
         assert_passes_formatter_check(app_root_path)
@@ -186,11 +197,23 @@ defmodule Phoenix.Integration.CodeGeneration.AppWithDefaultsTest do
     end
 
     @tag database: :postgresql
-    test "has a passing test suite" do
+    test "has a passing test suite (--live)" do
       with_installer_tmp("app_with_defaults", fn tmp_dir ->
         {app_root_path, _} = generate_phoenix_app(tmp_dir, "default_app")
 
-        mix_run!(~w(phx.gen.auth Accounts User users), app_root_path)
+        mix_run!(~w(phx.gen.auth Accounts User users --live), app_root_path)
+
+        drop_test_database(app_root_path)
+        assert_tests_pass(app_root_path)
+      end)
+    end
+
+    @tag database: :postgresql
+    test "has a passing test suite (--no-live)" do
+      with_installer_tmp("app_with_defaults", fn tmp_dir ->
+        {app_root_path, _} = generate_phoenix_app(tmp_dir, "default_app")
+
+        mix_run!(~w(phx.gen.auth Accounts User users --no-live), app_root_path)
 
         drop_test_database(app_root_path)
         assert_tests_pass(app_root_path)
--- integration_test/test/code_generation/app_with_mssql_adapter_test.exs
+++ integration_test/test/code_generation/app_with_mssql_adapter_test.exs
@@ -85,7 +85,7 @@ defmodule Phoenix.Integration.CodeGeneration.AppWithMSSQLAdapterTest do
   end
 
   describe "phx.gen.auth + pbkdf2 + existing context" do
-    test "has no compilation or formatter warnings" do
+    test "has no compilation or formatter warnings (--live)" do
       with_installer_tmp("new with defaults", fn tmp_dir ->
         {app_root_path, _} = generate_phoenix_app(tmp_dir, "phx_blog", ["--database", "mssql", "--live"])
 
@@ -102,7 +102,31 @@ defmodule Phoenix.Integration.CodeGeneration.AppWithMSSQLAdapterTest do
           """)
         end)
 
-        mix_run!(~w(phx.gen.auth Accounts User users --hashing-lib pbkdf2 --merge-with-existing-context), app_root_path)
+        mix_run!(~w(phx.gen.auth Accounts User users --hashing-lib pbkdf2 --merge-with-existing-context --live), app_root_path)
+
+        assert_no_compilation_warnings(app_root_path)
+        assert_passes_formatter_check(app_root_path)
+      end)
+    end
+
+    test "has no compilation or formatter warnings (--no-live)" do
+      with_installer_tmp("new with defaults", fn tmp_dir ->
+        {app_root_path, _} = generate_phoenix_app(tmp_dir, "phx_blog", ["--database", "mssql", "--live"])
+
+        mix_run!(~w(phx.gen.html Accounts Group groups name), app_root_path)
+
+        modify_file(Path.join(app_root_path, "lib/phx_blog_web/router.ex"), fn file ->
+          inject_before_final_end(file, """
+
+            scope "/", PhxBlogWeb do
+              pipe_through [:browser]
+
+              resources "/groups", GroupController
+            end
+          """)
+        end)
+
+        mix_run!(~w(phx.gen.auth Accounts User users --hashing-lib pbkdf2 --merge-with-existing-context --no-live), app_root_path)
 
         assert_no_compilation_warnings(app_root_path)
         assert_passes_formatter_check(app_root_path)
@@ -111,6 +135,31 @@ defmodule Phoenix.Integration.CodeGeneration.AppWithMSSQLAdapterTest do
 
     @tag database: :mssql
     test "has a passing test suite" do
+      with_installer_tmp("app_with_defaults (--live)", fn tmp_dir ->
+        {app_root_path, _} = generate_phoenix_app(tmp_dir, "phx_blog", ["--database", "mssql", "--live"])
+
+        mix_run!(~w(phx.gen.html Accounts Group groups name), app_root_path)
+
+        modify_file(Path.join(app_root_path, "lib/phx_blog_web/router.ex"), fn file ->
+          inject_before_final_end(file, """
+
+            scope "/", PhxBlogWeb do
+              pipe_through [:browser]
+
+              resources "/groups", GroupController
+            end
+          """)
+        end)
+
+        mix_run!(~w(phx.gen.auth Accounts User users --hashing-lib pbkdf2 --merge-with-existing-context --live), app_root_path)
+
+        drop_test_database(app_root_path)
+        assert_tests_pass(app_root_path)
+      end)
+    end
+
+    @tag database: :mssql
+    test "has a passing test suite (--no-live)" do
       with_installer_tmp("app_with_defaults", fn tmp_dir ->
         {app_root_path, _} = generate_phoenix_app(tmp_dir, "phx_blog", ["--database", "mssql", "--live"])
 
@@ -127,7 +176,7 @@ defmodule Phoenix.Integration.CodeGeneration.AppWithMSSQLAdapterTest do
           """)
         end)
 
-        mix_run!(~w(phx.gen.auth Accounts User users --hashing-lib pbkdf2 --merge-with-existing-context), app_root_path)
+        mix_run!(~w(phx.gen.auth Accounts User users --hashing-lib pbkdf2 --merge-with-existing-context --no-live), app_root_path)
 
         drop_test_database(app_root_path)
         assert_tests_pass(app_root_path)
