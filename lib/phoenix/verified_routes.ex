5b400a7f514eab81451b180933711f7a2bfbce6a
Chris McCord <chris@chrismccord.com>
2022-07-14 23:46:19 -0400

    WIP

--- lib/phoenix/verified_routes.ex
+++ lib/phoenix/verified_routes.ex
@@ -11,6 +11,7 @@ defmodule Phoenix.VerifiedRoutes do
       router = Keyword.fetch!(opts, :router)
       endpoint = Keyword.fetch!(opts, :endpoint)
       import unquote(__MODULE__)
+      # TODO quoted_literal?
       @phoenix_verified %{router: router, endpoint: endpoint}
     end
   end
@@ -24,20 +25,20 @@ defmodule Phoenix.VerifiedRoutes do
     %{router: router, endpoint: endpoint} =
       Module.get_attribute(__CALLER__.module, :phoenix_verified)
 
-    verify_route(endpoint, router, route)
+    verify_rewrite(endpoint, router, route)
   end
 
   defmacro path(other), do: raise_invalid_route(other)
 
   defmacro path(endpoint, router, {:<<>>, _meta, _segments} = route) do
-    verify_route(endpoint, router, route)
+    verify_rewrite(endpoint, router, route)
   end
 
   defmacro path(_endpoint, _router, other), do: raise_invalid_route(other)
 
   defmacro path(conn_or_socket_or_endpoint_or_uri, {:<<>>, _meta, _segments} = route) do
     %{router: router} = Module.get_attribute(__CALLER__.module, :phoenix_verified)
-    verify_route(conn_or_socket_or_endpoint_or_uri, router, route)
+    verify_rewrite(conn_or_socket_or_endpoint_or_uri, router, route)
   end
 
   defmacro path(_conn_or_socket_or_endpoint_or_uri, other), do: raise_invalid_route(other)
@@ -46,12 +47,12 @@ defmodule Phoenix.VerifiedRoutes do
     %{router: router, endpoint: endpoint} =
       Module.get_attribute(__CALLER__.module, :phoenix_verified)
 
-    verify_route(endpoint, router, route)
+    verify_rewrite(endpoint, router, route)
   end
 
   defmacro url(conn_or_socket_or_endpoint_or_uri, {:<<>>, _meta, _segments} = route) do
     %{router: router} = Module.get_attribute(__CALLER__.module, :phoenix_verified)
-    rewrite_route = verify_route(conn_or_socket_or_endpoint_or_uri, router, route)
+    rewrite_route = verify_rewrite(conn_or_socket_or_endpoint_or_uri, router, route)
 
     quote do
       unuqote(__MODULE__).__runtime_url__(
@@ -92,10 +93,7 @@ defmodule Phoenix.VerifiedRoutes do
 
   @doc false
   def __runtime_path__(%Plug.Conn{} = conn, router, path) do
-    conn
-    |> build_own_forward_path(router, path)
-    |> Kernel.||(build_conn_forward_path(conn, router, path))
-    |> Kernel.||(path_with_script(path, conn.script_name))
+    Phoenix.Router.Helpers.path(router, conn, path)
   end
 
   def __runtime_path__(%URI{} = uri, _router, path) do
@@ -116,92 +114,104 @@ defmodule Phoenix.VerifiedRoutes do
             "or a Phoenix.Endpoint when building path for #{inspect(router)} at #{path}, got: #{inspect(other)}"
   end
 
-  defp verify_route(endpoint_ctx, router, {:<<>>, meta, segments} = _route) do
-    rewrite =
-      Enum.map(segments, fn
-        "/" ->
-          "/"
+  defp verify_segment(["/" | rest], route, acc), do: verify_segment(rest, route, ["/" | acc])
 
-        "/" <> _ = segment ->
-          segment
+  defp verify_segment(["/" <> _ = segment | rest], route, acc) do
+    case {String.split(segment, "?"), rest} do
+      {[segment], _} ->
+        verify_segment(rest, route, [segment | acc])
 
-        segment when is_binary(segment) ->
-          raise ArgumentError, "path segments must begin with /"
+      {[segment, ""], [{:"::", m1, [{{:., m2, [Kernel, :to_string]}, m2, [query_arg]}, {:binary, m4, nil}]}]} ->
+        rewrite = {:"::", m1, [{{:., m2, [__MODULE__, :__encode_query__]}, m2, [query_arg]}, {:binary, m4, nil}]}
+        {Enum.reverse([segment | acc]), [rewrite]}
 
-        {:"::", m1, [{{:., m2, [Kernel, :to_string]}, m3, [dynamic]}, {:binary, m4, nil}]} ->
-          {:"::", m1, [{{:., m2, [Phoenix.Param, :to_param]}, m3, [dynamic]}, {:binary, m4, nil}]}
+      {[_segment, query], []} ->
+        {Enum.reverse([segment | acc]), [query]}
 
-        other ->
-          raise ArgumentError,
-                "verified routes require a compile-time string, got: #{inspect(other)}"
-      end)
+      {[_segment, _], _} ->
+        raise ArgumentError, "expected query string param to be compile-time map or keyword list, got: #{Macro.to_string(route)}"
+    end
+  end
+
+  defp verify_segment([segment | _], route, _acc) when is_binary(segment) do
+    raise ArgumentError, "path segments must begin with /, got: #{Macro.to_string(route)}"
+  end
+
+  defp verify_segment(
+         [{:"::", m1, [{{:., m2, [Kernel, :to_string]}, m3, [dynamic]}, {:binary, m4, nil}]} | rest],
+         route,
+         acc
+       ) do
+    rewrite = {:"::", m1, [{{:., m2, [Phoenix.Param, :to_param]}, m3, [dynamic]}, {:binary, m4, nil}]}
+    verify_segment(rest, route, [rewrite | acc])
+  end
+
+  defp verify_segment([_other | _], route, _acc) do
+    raise ArgumentError,
+          "verified routes require a compile-time string, got: #{Macro.to_string(route)}"
+  end
+
+  defp verify_segment([], _route, acc), do: {Enum.reverse(acc), _query = []}
+
+  @doc false
+  def __encode_query__(dict) when is_list(dict) or is_map(dict) do
+    case Plug.Conn.Query.encode(dict, &to_param/1) do
+      "" -> ""
+      query_str -> "?" <> query_str
+    end
+  end
+
+  defp to_param(int) when is_integer(int), do: Integer.to_string(int)
+  defp to_param(bin) when is_binary(bin), do: bin
+  defp to_param(false), do: "false"
+  defp to_param(true), do: "true"
+  defp to_param(data), do: Phoenix.Param.to_param(data)
+
+  defp verify_rewrite(endpoint_ctx, router, {:<<>>, meta, segments} = route) do
+    {path_rewrite, query_rewrite} = verify_segment(segments, route, [])
+      # Enum.map_reduce(segments, @segments, fn
+      #   "/", @segments ->
+      #     {"/", @segments}
+
+      #   "/" <> _ = segment, @segments ->
+      #     if String.ends_with?(segment, "?") do
+      #       {"todo", :query}
+      #     else
+      #       {segment, @segments}
+      #     end
+
+      #   segment when is_binary(segment), @segments ->
+      #     raise ArgumentError, "path segments must begin with /"
+
+      #   {:"::", m1, [{{:., m2, [Kernel, :to_string]}, m3, [dynamic]}, {:binary, m4, nil}]},
+      #   @segments ->
+      #     ast =
+      #       {:"::", m1, [{{:., m2, [Phoenix.Param, :to_param]}, m3, [dynamic]}, {:binary, m4, nil}]}
+
+      #     {ast, @segments}
+
+      #   other, @segments ->
+      #     raise ArgumentError,
+      #           "verified routes require a compile-time string, got: #{inspect(other)}"
+      # end)
 
     test_path =
-      Enum.map_join(rewrite, fn
+      Enum.map_join(path_rewrite, fn
         segment when is_binary(segment) -> segment
         _other -> "..."
       end)
 
     case Phoenix.Router.route_info(router, "GET", test_path, _host = nil) do
-      %{} ->
-        :ok
-
-      :error ->
-        IO.warn("""
-        no route path for #{inspect(router)} matches "#{test_path}"
-
-        Available routes:
-
-        #{Phoenix.Router.ConsoleFormatter.format(router)}
-        """)
+      %{} -> :ok
+      :error -> IO.warn("no route path for #{inspect(router)} matches #{Macro.to_string(route)}")
     end
 
     quote do
       unquote(__MODULE__).__runtime_path__(
         unquote(endpoint_ctx),
         unquote(router),
-        unquote({:<<>>, meta, rewrite})
+        unquote({:<<>>, meta, path_rewrite ++ query_rewrite})
       )
     end
-  end
-
-  defp build_own_forward_path(conn, router, path) do
-    case Map.fetch(conn.private, router) do
-      {:ok, {local_script, _}} ->
-        path_with_script(path, local_script)
-
-      :error ->
-        nil
-    end
-  end
-
-  defp build_conn_forward_path(
-         %Plug.Conn{private: %{phoenix_router: phx_router}} = conn,
-         router,
-         path
-       ) do
-    case Map.fetch(conn.private, phx_router) do
-      {:ok, {script_name, forwards}} ->
-        case Map.fetch(forwards, router) do
-          {:ok, local_script} ->
-            path_with_script(path, script_name ++ local_script)
-
-          :error ->
-            nil
-        end
-
-      :error ->
-        nil
-    end
-  end
-
-  defp build_conn_forward_path(_conn, _router, _path), do: nil
-
-  defp path_with_script(path, []) do
-    path
-  end
-
-  defp path_with_script(path, script) do
-    "/" <> Enum.join(script, "/") <> path
   end
 end
