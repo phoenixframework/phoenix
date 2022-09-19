63111c3f4e087da4dae3e9e5983945de7c01b32a
Chris McCord <chris@chrismccord.com>
2022-09-19 16:38:57 -0400

    Add heroicons reference

--- priv/templates/phx.gen.live/components.ex
+++ priv/templates/phx.gen.live/components.ex
@@ -2,9 +2,12 @@ defmodule <%= @web_namespace %>.Components do
   @moduledoc """
   Provides core UI components.
 
-  The components in this module use tailwindcss, a utility-first CSS framework.
+  The components in this module use Tailwind CSS, a utility-first CSS framework.
   See the [Tailwind CSS documentation](https://tailwindcss.com) to learn how to
   customize the generated components in this module.
+
+  Icons are provided by [heroicons](https://heroicons.com), using the
+  [heroicons_elixir](https://github.com/mveytsman/) project.
   """
   use Phoenix.Component
 
