22ef871312bd06fa591c2da04d32949ebc77f750
Justin Smestad <justin.smestad@gmail.com>
2022-09-21 12:00:24 -0600

    Add note about topbar modifications (#4966)
    Fixes #4965

--- installer/templates/phx_live/assets/topbar.js
+++ installer/templates/phx_live/assets/topbar.js
@@ -1,10 +1,12 @@
 /**
  * @license MIT
  * topbar 1.0.0, 2021-01-06
+ * Modifications:
+ *   - add delayedShow(time) (2022-09-21)
  * http://buunguyen.github.io/topbar
  * Copyright (c) 2021 Buu Nguyen
  */
- (function (window, document) {
+(function (window, document) {
   "use strict";
 
   // https://gist.github.com/paulirish/1579671
