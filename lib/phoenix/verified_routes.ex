9b767b8e17fa4d2cfe43916acb9890448ed21b65
Max Veytsman <maxim@ontoillogical.com>
2022-09-27 13:07:08 -0400

    Don't encode structs as dictionaries

--- lib/phoenix/verified_routes.ex
+++ lib/phoenix/verified_routes.ex
@@ -642,7 +642,7 @@ defmodule Phoenix.VerifiedRoutes do
   end
 
   @doc false
-  def __encode_query__(dict) when is_list(dict) or is_map(dict) do
+  def __encode_query__(dict) when is_list(dict) or (is_map(dict) and not is_struct(dict)) do
     case Plug.Conn.Query.encode(dict, &to_param/1) do
       "" -> ""
       query_str -> query_str
