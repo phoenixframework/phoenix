f07b02d89921c432ba7e6d6d9debc1e98debfca4
Chris McCord <chris@chrismccord.com>
2022-10-10 11:11:24 -0400

    Update installer mix

--- installer/mix.exs
+++ installer/mix.exs
@@ -1,12 +1,32 @@
 defmodule Phx.New.Mixfile do
   use Mix.Project
 
+  @scm_url "https://github.com/phoenixframework/phoenix"
+
   def project do
     [
       app: :phx_new,
       start_permanent: Mix.env == :prod,
       version: "1.3.5",
-      elixir: "~> 1.3 or ~> 1.4"
+      elixir: "~> 1.3 or ~> 1.4",
+      package: [
+        maintainers: [
+          "Chris McCord",
+          "José Valim",
+          "Gary Rennie",
+          "Jason Stiebs"
+        ],
+        licenses: ["MIT"],
+        links: %{"GitHub" => @scm_url},
+        files: ~w(lib templates mix.exs README.md)
+      ],
+      source_url: @scm_url,
+      homepage_url: "https://www.phoenixframework.org",
+      description: """
+      Phoenix framework project generator.
+      Provides a `mix phx.new` task to bootstrap a new Elixir application
+      with Phoenix dependencies.
+      """
     ]
   end
 
