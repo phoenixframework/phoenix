ff44eafefba40e80c1f187e7506e4bf472a08946
Kevin <moogle19@users.noreply.github.com>
2022-10-26 21:35:46 +0200

    Add tailwind config and watcher in umbrella (#5018)
    Co-authored-by: José Valim <jose.valim@dashbit.co>

--- installer/templates/phx_umbrella/apps/app_name_web/config/dev.exs
+++ installer/templates/phx_umbrella/apps/app_name_web/config/dev.exs
@@ -14,7 +14,8 @@ config :<%= @web_app_name %>, <%= @endpoint_module %>,
   secret_key_base: "<%= @secret_key_base_dev %>",
   watchers: <%= if @assets do %>[
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]<% else %>[]<% end %>
 
 # ## SSL Support
