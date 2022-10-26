fa9a9f64157ff3ee574fd636800515c42e2fd974
José Valim <jose.valim@dashbit.co>
2022-10-26 21:17:12 +0200

    Unify dashboard/mailer blocks, closes #5048

--- installer/templates/phx_web/router.ex
+++ installer/templates/phx_web/router.ex
@@ -30,26 +30,20 @@ defmodule <%= @web_namespace %>.Router do
   end<% end %><%= if @dashboard || @mailer do %>
 
   # Enable <%= [@dashboard && "LiveDashboard", @mailer && "Swoosh mailbox preview"] |> Enum.filter(&(&1)) |> Enum.join(" and ") %> in development
-  if Application.compile_env(:<%= @app_name %>, :dev_routes) do<% end %><%= if @dashboard do %>
+  if Application.compile_env(:<%= @app_name %>, :dev_routes) do<%= if @dashboard do %>
     # If you want to use the LiveDashboard in production, you should put
     # it behind authentication and allow only admins to access it.
     # If your application does not have an admins-only section yet,
     # you can use Plug.BasicAuth to set up some basic authentication
     # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
+    import Phoenix.LiveDashboard.Router<% end %>
 
-    scope "/" do<%= if @html do %>
-      pipe_through :browser<% else %>
-      pipe_through [:fetch_session, :protect_from_forgery]<% end %>
-
-      live_dashboard "/dashboard", metrics: <%= @web_namespace %>.Telemetry
-    end
-<% end %><%= if @mailer do %>
     scope "/dev" do<%= if @html do %>
       pipe_through :browser<% else %>
       pipe_through [:fetch_session, :protect_from_forgery]<% end %>
-
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+<%= if @dashboard do %>
+      live_dashboard "/dashboard", metrics: <%= @web_namespace %>.Telemetry<% end %><%= if @mailer do %>
+      forward "/mailbox", Plug.Swoosh.MailboxPreview<% end %>
     end
   end<% end %>
 end
