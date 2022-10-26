e6ae1bf5a995b940dc798c3a0d57c4a5ac99aea9
José Valim <jose.valim@dashbit.co>
2022-10-26 21:26:50 +0200

    Move SSL configuration to runtime, closes #5045

--- installer/templates/phx_umbrella/config/prod.exs
+++ installer/templates/phx_umbrella/config/prod.exs
@@ -2,3 +2,6 @@ import Config
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Runtime production configuration, including reading
+# of environment variables, is done on config/runtime.exs.
