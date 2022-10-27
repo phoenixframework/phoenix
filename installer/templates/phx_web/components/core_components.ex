646281a4ff14fc265326ed4485c616c5207508ef
Leandro Cesquini Pereira <leandro@leandro.io>
2022-10-27 13:38:59 -0400

    app_with_no_options

--- installer/templates/phx_web/components/core_components.ex
+++ installer/templates/phx_web/components/core_components.ex
@@ -72,7 +72,7 @@ defmodule <%= @web_namespace %>.CoreComponents do
                   phx-click={hide_modal(@on_cancel, @id)}
                   type="button"
                   class="-m-3 flex-none p-3 opacity-20 hover:opacity-40"
-                  aria-label={gettext("close")}
+                  aria-label="close"
                 >
                   <Heroicons.x_mark solid class="h-5 w-5 stroke-current" />
                 </button>
@@ -153,12 +153,7 @@ defmodule <%= @web_namespace %>.CoreComponents do
         <%%= @title %>
       </p>
       <p class="mt-2 text-[0.8125rem] leading-5"><%%= msg %></p>
-      <button
-        :if={@close}
-        type="button"
-        class="group absolute top-2 right-1 p-2"
-        aria-label={gettext("close")}
-      >
+      <button :if={@close} type="button" class="group absolute top-2 right-1 p-2" aria-label="close">
         <Heroicons.x_mark solid class="h-5 w-5 stroke-current opacity-40 group-hover:opacity-70" />
       </button>
     </div>
@@ -443,7 +438,7 @@ defmodule <%= @web_namespace %>.CoreComponents do
         <thead class="text-left text-[0.8125rem] leading-6 text-zinc-500">
           <tr>
             <th :for={col <- @col} class="p-0 pb-4 pr-6 font-normal"><%%= col[:label] %></th>
-            <th class="relative p-0 pb-4"><span class="sr-only"><%%= gettext("Actions") %></span></th>
+            <th class="relative p-0 pb-4"><span class="sr-only">Actions</span></th>
           </tr>
         </thead>
         <tbody class="relative divide-y divide-zinc-100 border-t border-zinc-200 text-sm leading-6 text-zinc-700">
@@ -580,7 +575,7 @@ defmodule <%= @web_namespace %>.CoreComponents do
 
   @doc """
   Translates an error message using gettext.
-  """
+  """<%= if @gettext do %>
   def translate_error({msg, opts}) do
     # When using gettext, we typically pass the strings we want
     # to translate as a static argument:
@@ -604,7 +599,19 @@ defmodule <%= @web_namespace %>.CoreComponents do
     else
       Gettext.dgettext(<%= @web_namespace %>.Gettext, "errors", msg, opts)
     end
-  end
+  end<% else %>
+  def translate_error({msg, _opts}) do
+    # You can make use of gettext to translate error messages by
+    # uncommenting and adjusting the following code:
+
+    # if count = opts[:count] do
+    #   Gettext.dngettext(<%= @web_namespace %>.Gettext, "errors", msg, msg, count, opts)
+    # else
+    #   Gettext.dgettext(<%= @web_namespace %>.Gettext, "errors", msg, opts)
+    # end
+
+    msg
+  end<% end %>
 
   @doc """
   Translates the errors for a field from a keyword list of errors.
