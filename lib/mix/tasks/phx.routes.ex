811d3c4ac01f1e20059b807619379f131de7bd9e
Chris McCord <chris@chrismccord.com>
2022-11-02 09:51:43 -0400

    Do not print socket information if only router is passed. Closes #4370

--- lib/mix/tasks/phx.routes.ex
+++ lib/mix/tasks/phx.routes.ex
@@ -38,27 +38,26 @@ defmodule Mix.Tasks.Phx.Routes do
     Mix.Task.run("compile", args)
     Mix.Task.reenable("phx.routes")
 
-    {router_mod, opts} =
-      case OptionParser.parse(args, switches: [endpoint: :string, router: :string]) do
-        {opts, [passed_router], _} -> {router(passed_router, base), opts}
-        {opts, [], _} -> {router(opts[:router], base), opts}
-      end
+    case OptionParser.parse(args, switches: [endpoint: :string, router: :string]) do
+      {opts, [passed_router], _} ->
+        ConsoleFormatter.format(passed_router, opts[:endpoint])
 
-    router_mod
-    |> ConsoleFormatter.format(endpoint(opts[:endpoint], base))
-    |> Mix.shell().info()
+      {opts, [], _} ->
+        ConsoleFormatter.format(router(opts[:router], base), endpoint(opts[:endpoint], base))
+    end
   end
 
   defp endpoint(nil, base) do
     loaded(web_mod(base, "Endpoint"))
   end
+
   defp endpoint(module, _base) do
     loaded(Module.concat([module]))
   end
 
   defp router(nil, base) do
     if Mix.Project.umbrella?() do
-      Mix.raise """
+      Mix.raise("""
       umbrella applications require an explicit router to be given to phx.routes, for example:
 
           $ mix phx.routes MyAppWeb.Router
@@ -67,26 +66,29 @@ defmodule Mix.Tasks.Phx.Routes do
 
           "phx.routes": "phx.routes MyAppWeb.Router"
 
-      """
+      """)
     end
+
     web_router = web_mod(base, "Router")
     old_router = app_mod(base, "Router")
 
-    loaded(web_router) || loaded(old_router) || Mix.raise """
-    no router found at #{inspect web_router} or #{inspect old_router}.
-    An explicit router module may be given to phx.routes, for example:
+    loaded(web_router) || loaded(old_router) ||
+      Mix.raise("""
+      no router found at #{inspect(web_router)} or #{inspect(old_router)}.
+      An explicit router module may be given to phx.routes, for example:
 
-        $ mix phx.routes MyAppWeb.Router
+          $ mix phx.routes MyAppWeb.Router
 
-    An alias can be added to mix.exs aliases to automate this:
+      An alias can be added to mix.exs aliases to automate this:
 
-        "phx.routes": "phx.routes MyAppWeb.Router"
+          "phx.routes": "phx.routes MyAppWeb.Router"
 
-    """
+      """)
   end
+
   defp router(router_name, _base) do
     arg_router = Module.concat([router_name])
-    loaded(arg_router) || Mix.raise "the provided router, #{inspect(arg_router)}, does not exist"
+    loaded(arg_router) || Mix.raise("the provided router, #{inspect(arg_router)}, does not exist")
   end
 
   defp loaded(module) do
