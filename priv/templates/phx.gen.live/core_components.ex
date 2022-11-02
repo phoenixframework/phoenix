7dcecf686608c9ab161c6e0321ee28cfe5120ea4
Chris McCord <chris@chrismccord.com>
2022-11-02 08:08:11 -0400

    Sync core components

--- priv/templates/phx.gen.live/core_components.ex
+++ priv/templates/phx.gen.live/core_components.ex
@@ -383,7 +383,7 @@ defmodule <%= @web_namespace %>.CoreComponents do
     ~H"""
     <p class="phx-no-feedback:hidden mt-3 flex gap-3 text-sm leading-6 text-rose-600">
       <Heroicons.exclamation_circle mini class="mt-0.5 h-5 w-5 flex-none fill-rose-500" />
-      <%%= @message %>
+      <%%= render_slot(@inner_block) %>
     </p>
     """
   end
