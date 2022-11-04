4289a1c8859be72db9c8a36bd8dd576cf1c398ec
Kevin <moogle19@users.noreply.github.com>
2022-11-04 23:53:28 +0100

    Update umbrella files

--- installer/templates/phx_umbrella/apps/app_name_web/config/config.exs
+++ installer/templates/phx_umbrella/apps/app_name_web/config/config.exs
@@ -7,6 +7,7 @@ config :<%= @web_app_name %><%= if @namespaced? do %>,
 <% end %># Configures the endpoint
 config :<%= @web_app_name %>, <%= @endpoint_module %>,
   url: [host: "localhost"],
+  adapter: <%= inspect @web_adapter_module %>,
   render_errors: [
     formats: [<%= if @html do%>html: <%= @web_namespace %>.ErrorHTML, <% end %>json: <%= @web_namespace %>.ErrorJSON],
     layout: false
