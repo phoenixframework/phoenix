252b3036f01438305e84505c29435bc33bc80ec8
Chulki Lee <chulki.lee@gmail.com>
2022-11-12 11:17:48 -0800

    use the latest tailwind

--- installer/templates/phx_single/config/config.exs
+++ installer/templates/phx_single/config/config.exs
@@ -43,7 +43,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "3.1.8",
+  version: "3.2.4",
   default: [
     args: ~w(
       --config=tailwind.config.js
