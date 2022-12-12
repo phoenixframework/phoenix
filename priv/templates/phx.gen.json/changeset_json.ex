3ba35b78175497f04f30b37ec2a5a2fcd6b2955b
Justin Smestad <justin.smestad@gmail.com>
2022-12-12 10:51:38 -0700

    Update changeset_json.ex (#5158)

--- priv/templates/phx.gen.json/changeset_json.ex
+++ priv/templates/phx.gen.json/changeset_json.ex
@@ -3,7 +3,7 @@ defmodule <%= inspect context.web_module %>.ChangesetJSON do
   Traverses and translates changeset errors.
 
   See `Ecto.Changeset.traverse_errors/2` and
-  `<%= inspect context.web_module %>.CoreCompponents.translate_error/1` for more details.
+  `<%= inspect context.web_module %>.CoreComponents.translate_error/1` for more details.
   """
   def translate_errors(changeset) do
     Ecto.Changeset.traverse_errors(changeset, &<%= inspect context.web_module %>.CoreComponents.translate_error/1)
