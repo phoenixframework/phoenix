a3a58c1ff9fe2ccff678e1164f728a70cc60f9d1
José Valim <jose.valim@dashbit.co>
2023-01-01 11:26:21 -0300

    Fix ChangesetJSON generator

--- priv/templates/phx.gen.json/changeset_json.ex
+++ priv/templates/phx.gen.json/changeset_json.ex
@@ -1,17 +1,18 @@
 defmodule <%= inspect context.web_module %>.ChangesetJSON do
   @doc """
-  Traverses and translates changeset errors.
-
-  See `Ecto.Changeset.traverse_errors/2` and
-  `<%= inspect context.web_module %>.CoreComponents.translate_error/1` for more details.
-  """<%= if core_components? do %>) do
-  def translate_errors(changeset) do
-    Ecto.Changeset.traverse_errors(changeset, &<%= inspect context.web_module %>.CoreComponents.translate_error/1)
-  end<% else %><%= if gettext? do %>
-  def translate_errors(changeset) do
-    Ecto.Changeset.traverse_errors(changeset, &translate_error/1)
+  Renders changeset errors.
+  """<%= if core_components? do %>
+  def error(%{changeset: changeset}) do
+    # When encoded, the changeset returns its errors
+    # as a JSON object. So we just pass it forward.
+    %{errors: Ecto.Changeset.traverse_errors(changeset, &<%= inspect context.web_module %>.CoreComponents.translate_error/1)}
+  end<% else %>
+  def error(%{changeset: changeset}) do
+    # When encoded, the changeset returns its errors
+    # as a JSON object. So we just pass it forward.
+    %{errors: Ecto.Changeset.traverse_errors(changeset, &translate_error/1)}
   end
-
+<%= if gettext? do %>
   defp translate_error({msg, opts}) do
     # set by Ecto and indicates we should also apply plural rules.
     if count = opts[:count] do
@@ -19,11 +20,8 @@ defmodule <%= inspect context.web_module %>.ChangesetJSON do
     else
       Gettext.dgettext(<%= inspect context.web_module %>.Gettext, "errors", msg, opts)
     end
-  end<% else %>
-  def translate_errors(changeset) do
-    Ecto.Changeset.traverse_errors(changeset, &translate_error/1)
   end
-
+<% else %>
   defp translate_error({msg, opts}) do
     # You can make use of gettext to translate error messages by
     # uncommenting and adjusting the following code:
@@ -38,13 +36,4 @@ defmodule <%= inspect context.web_module %>.ChangesetJSON do
       String.replace(acc, "%{#{key}}", fn _ -> to_string(value) end)
     end)
   end<% end %><% end %>
-
-  @doc """
-  Renders changeset errors.
-  """
-  def error(%{changeset: changeset}) do
-    # When encoded, the changeset returns its errors
-    # as a JSON object. So we just pass it forward.
-    %{errors: translate_errors(changeset)}
-  end
 end
