ec14995c7044602a88ef7b2e3493b480092c4007
José Valim <jose.valim@dashbit.co>
2023-01-01 12:34:53 -0300

    Synchronize components for textarea

--- installer/templates/phx_web/components/core_components.ex
+++ installer/templates/phx_web/components/core_components.ex
@@ -338,7 +338,8 @@ defmodule <%= @web_namespace %>.CoreComponents do
           "phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400 phx-no-feedback:focus:ring-zinc-800/5"
         ]}
         {@rest}
-      ><%%= @value %></textarea>
+      >
+    <%%= @value %></textarea>
       <.error :for={msg <- @errors}><%%= msg %></.error>
     </div>
     """
