96e687ecc61c3f859977128cc9175b8dcb90039f
Chris King <chris.aa.king@gmail.com>
2023-01-02 10:38:07 +0100

    fixes missing :references to phx.gen.embedded

--- priv/templates/phx.gen.embedded/embedded_schema.ex
+++ priv/templates/phx.gen.embedded/embedded_schema.ex
@@ -5,7 +5,8 @@ defmodule <%= inspect schema.module %> do
 
   embedded_schema do
 <%= Mix.Phoenix.Schema.format_fields_for_schema(schema) %>
-  end
+<%= for {_, k, _, _} <- schema.assocs do %>    field <%= inspect k %>, <%= if schema.binary_id do %>:binary_id<% else %>:id<% end %>
+<% end %>  end
 
   @doc false
   def changeset(%<%= inspect schema.alias %>{} = <%= schema.singular %>, attrs) do
