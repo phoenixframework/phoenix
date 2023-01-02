2b5705846c07aa89621ae1e86bd8e3fc60295f5c
Chris King <70202379+Morzaram@users.noreply.github.com>
2023-01-02 14:59:12 +0100

    Fixes missing :references generation to phx.gen.embedded (#5186)
    * fixes missing :references to phx.gen.embedded
    
    * fixes blank space if no non-references are called

--- priv/templates/phx.gen.embedded/embedded_schema.ex
+++ priv/templates/phx.gen.embedded/embedded_schema.ex
@@ -3,9 +3,10 @@ defmodule <%= inspect schema.module %> do
   import Ecto.Changeset
   alias <%= inspect schema.module %>
 
-  embedded_schema do
-<%= Mix.Phoenix.Schema.format_fields_for_schema(schema) %>
-  end
+  embedded_schema do <%= if !Map.equal?(schema.types, %{}) do %>
+<%= Mix.Phoenix.Schema.format_fields_for_schema(schema) %><% end %>
+<%= for {_, k, _, _} <- schema.assocs do %>    field <%= inspect k %>, <%= if schema.binary_id do %>:binary_id<% else %>:id<% end %>
+<% end %>  end
 
   @doc false
   def changeset(%<%= inspect schema.alias %>{} = <%= schema.singular %>, attrs) do
