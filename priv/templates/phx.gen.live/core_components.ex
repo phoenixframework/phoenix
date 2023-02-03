2aff0e73dd46cb0c2f7f8fd67b820b877bf5995d
Benjamin Milde <benni@kobrakai.de>
2023-02-03 22:31:33 +0100

    Cross browser rounded hover effect

--- priv/templates/phx.gen.live/core_components.ex
+++ priv/templates/phx.gen.live/core_components.ex
@@ -452,8 +452,12 @@ defmodule <%= @web_namespace %>.CoreComponents do
       <table class="mt-11 w-[40rem] sm:w-full">
         <thead class="text-left text-[0.8125rem] leading-6 text-zinc-500">
           <tr>
-            <th :for={col <- @col} class="p-0 pb-4 pr-6 font-normal"><%%= col[:label] %></th>
-            <th class="relative p-0 pb-4"><span class="sr-only"><%= if @gettext do %><%%= gettext("Actions") %><% else %>Actions<% end %></span></th>
+            <th :for={col <- @col} class="p-0 pb-4 pr-6 font-normal">
+              <%%= col[:label] %>
+            </th>
+            <th :if={@action != []} class="relative p-0 pb-4">
+              <span class="sr-only"><%= if @gettext do %><%%= gettext("Actions") %><% else %>Actions<% end %></span>
+            </th>
           </tr>
         </thead>
         <tbody class="relative divide-y divide-zinc-100 border-t border-zinc-200 text-sm leading-6 text-zinc-700">
@@ -465,18 +469,21 @@ defmodule <%= @web_namespace %>.CoreComponents do
             <td
               :for={{col, i} <- Enum.with_index(@col)}
               phx-click={@row_click && @row_click.(row)}
-              class={["relative p-0", @row_click && "hover:cursor-pointer"]}
+              class={[
+                "relative p-0",
+                rounded_start(),
+                @action == [] && rounded_end(),
+                @row_click && "hover:cursor-pointer"
+              ]}
             >
               <div class="block py-4 pr-6">
-                <span class="absolute -inset-y-px right-0 -left-4 group-hover:bg-zinc-50 sm:rounded-l-xl" />
                 <span class={["relative", i == 0 && "font-semibold text-zinc-900"]}>
                   <%%= render_slot(col, row) %>
                 </span>
               </div>
             </td>
-            <td :if={@action != []} class="relative p-0 w-14">
-              <div class="relative whitespace-nowrap py-4 text-right text-sm font-medium">
-                <span class="absolute -inset-y-px -right-4 left-0 group-hover:bg-zinc-50 sm:rounded-r-xl" />
+            <td :if={@action != []} class={["relative p-0 w-14", rounded_end()]}>
+              <div class="whitespace-nowrap py-4 text-right text-sm font-medium">
                 <span
                   :for={action <- @action}
                   class="relative ml-4 font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
@@ -490,6 +497,14 @@ defmodule <%= @web_namespace %>.CoreComponents do
       </table>
     </div>
     """
+  end
+
+  defp rounded_start do
+    "first:before:absolute first:before:h-full first:before:w-4 first:before:top-0 first:before:-left-4 first:before:group-hover:bg-zinc-50 first:before:sm:rounded-l-xl"
+  end
+
+  defp rounded_end do
+    "last:after:absolute last:after:h-full last:after:w-4 last:after:top-0 last:after:-right-4 last:after:group-hover:bg-zinc-50 last:after:sm:rounded-r-xl"
   end
 
   @doc """
