0ae273b23f6075ce581efe5011488215aff21de0
Chris McCord <chris@chrismccord.com>
2023-02-09 15:01:04 -0500

    Fix formatting

--- priv/templates/phx.gen.live/core_components.ex
+++ priv/templates/phx.gen.live/core_components.ex
@@ -282,13 +282,13 @@ defmodule <%= @web_namespace %>.CoreComponents do
   attr :id, :any, default: nil
   attr :name, :any
   attr :label, :string, default: nil
+  attr :value, :any
 
   attr :type, :string,
     default: "text",
     values: ~w(checkbox color date datetime-local email file hidden month number password
                range radio search select tel text textarea time url week)
 
-  attr :value, :any
   attr :field, Phoenix.HTML.FormField,
     doc: "a form field struct retrieved from the form, for example: @form[:email]"
 
