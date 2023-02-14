e4285c3385032409a65ce58d955140a8e3305573
Chris McCord <chris@chrismccord.com>
2023-02-14 15:06:14 -0500

    Fix CI

--- installer/templates/phx_web/components/core_components.ex
+++ installer/templates/phx_web/components/core_components.ex
@@ -495,11 +495,7 @@ defmodule <%= @web_namespace %>.CoreComponents do
           phx-update={match?(%Phoenix.LiveView.LiveStream{}, @rows) && "stream"}
           class="relative divide-y divide-zinc-100 border-t border-zinc-200 text-sm leading-6 text-zinc-700"
         >
-          <tr
-            :for={row <- @rows}
-            id={@row_id && @row_id.(row)}
-            class="group hover:bg-zinc-50"
-          >
+          <tr :for={row <- @rows} id={@row_id && @row_id.(row)} class="group hover:bg-zinc-50">
             <td
               :for={{col, i} <- Enum.with_index(@col)}
               phx-click={@row_click && @row_click.(row)}
