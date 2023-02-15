0405ecf27c6226fc2f5abf4a2693e30464d66da2
Chris McCord <chris@chrismccord.com>
2023-02-15 09:56:08 -0500

    Bump LV

--- installer/templates/phx_umbrella/apps/app_name_web/mix.exs
+++ installer/templates/phx_umbrella/apps/app_name_web/mix.exs
@@ -40,7 +40,7 @@ defmodule <%= @web_namespace %>.MixProject do
       {:phoenix_ecto, "~> 4.4"},<% end %><%= if @html do %>
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.18.12"},
+      {:phoenix_live_view, "~> 0.18.14"},
       {:heroicons, "~> 0.5"},
       {:floki, ">= 0.30.0", only: :test},<% end %><%= if @dashboard do %>
       {:phoenix_live_dashboard, "~> 0.7.2"},<% end %><%= if @assets do %>
