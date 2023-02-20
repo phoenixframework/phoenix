e16cbaed2986ddd7fa4dc07f4844e77b68693f36
José Valim <jose.valim@gmail.com>
2023-02-20 14:13:36 +0100

    Apply suggestions from code review

--- priv/templates/phx.gen.auth/schema.ex
+++ priv/templates/phx.gen.auth/schema.ex
@@ -54,7 +54,7 @@ defmodule <%= inspect schema.module %> do
     changeset
     |> validate_required([:password])
     |> validate_length(:password, min: 12, max: 72)
-    # other examples of password validation; adapt to your needs
+    # Examples of additional password validation:
     # |> validate_format(:password, ~r/[a-z]/, message: "at least one lower case character")
     # |> validate_format(:password, ~r/[A-Z]/, message: "at least one upper case character")
     # |> validate_format(:password, ~r/[!?@#$%^&*_0-9]/, message: "at least one digit or punctuation character")
