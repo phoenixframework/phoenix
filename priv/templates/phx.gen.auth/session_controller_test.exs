5a9103ced470d04bade13184c8d75018f76716aa
Chris McCord <chris@chrismccord.com>
2023-02-24 13:41:14 -0500

    Apply suggestions from code review

--- priv/templates/phx.gen.auth/session_controller_test.exs
+++ priv/templates/phx.gen.auth/session_controller_test.exs
@@ -36,8 +36,8 @@ defmodule <%= inspect context.web_module %>.<%= inspect Module.concat(schema.web
       conn = get(conn, ~p"/")
       response = html_response(conn, 200)
       assert response =~ <%= schema.singular %>.email
-      assert response =~ "/users/settings"
-      assert response =~ "/users/log_out"
+      assert response =~ ~p"/users/settings"
+      assert response =~ ~p"/users/log_out"
     end
 
     test "logs the <%= schema.singular %> in with remember me", %{conn: conn, <%= schema.singular %>: <%= schema.singular %>} do
