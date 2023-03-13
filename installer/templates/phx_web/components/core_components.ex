87f35a9cdb62f3bb04dfbe6f8bc5823bc57c703c
José Valim <jose.valim@gmail.com>
2023-03-13 21:19:19 +0100

    Apply suggestions from code review

--- installer/templates/phx_web/components/core_components.ex
+++ installer/templates/phx_web/components/core_components.ex
@@ -124,7 +124,7 @@ defmodule <%= @web_namespace %>.CoreComponents do
         <%%= @title %>
       </p>
       <p class="mt-2 text-sm leading-5"><%%= msg %></p>
-      <button type="button" class="group absolute top-1 right-1 p-2" aria-label={gettext("close")}>
+      <button type="button" class="group absolute top-1 right-1 p-2" aria-label=<%= if @gettext do %>{gettext("close")}<% else %>"close"<% end %>>
         <.icon name="hero-x-mark-solid" class="h-5 w-5 opacity-40 group-hover:opacity-70" />
       </button>
     </div>
