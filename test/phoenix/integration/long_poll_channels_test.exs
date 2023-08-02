210771986a629ef92e028e7de1b05be7a86ad44b
Chris McCord <chris@chrismccord.com>
2023-08-01 23:06:22 -0400

    Async

--- test/phoenix/integration/long_poll_channels_test.exs
+++ test/phoenix/integration/long_poll_channels_test.exs
@@ -1,7 +1,7 @@
 Code.require_file("../../support/http_client.exs", __DIR__)
 
 defmodule Phoenix.Integration.LongPollChannelsTest do
-  use ExUnit.Case, async: false
+  use ExUnit.Case
   import ExUnit.CaptureLog
 
   alias Phoenix.Integration.HTTPClient
