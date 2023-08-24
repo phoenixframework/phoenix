deea9d64cf5704543ec683e1d06639bcbe2b5689
Rafał Studnicki <studzien@gmail.com>
2023-08-24 16:13:55 +0200

    Dynamic socket drainer configuration (#5543)

--- lib/phoenix/socket.ex
+++ lib/phoenix/socket.ex
@@ -452,7 +452,14 @@ defmodule Phoenix.Socket do
     opts = Keyword.merge(socket_options, opts)
 
     if drainer = Keyword.get(opts, :drainer, []) do
-      {Phoenix.Socket.PoolDrainer, {endpoint, handler, drainer}}
+      drainer =
+        case drainer do
+          {module, function, arguments} ->
+            apply(module, function, arguments)
+          _ ->
+            drainer
+        end
+        {Phoenix.Socket.PoolDrainer, {endpoint, handler, drainer}}
     else
       :ignore
     end
