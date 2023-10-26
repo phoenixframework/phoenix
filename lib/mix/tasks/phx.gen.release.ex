34b65cc4a7d1efe59daf735422684339b824476d
Gary Rennie <gazler@gmail.com>
2023-10-26 12:13:33 +0100

    Improve the error message for mix phx.gen.release
    If a suitable image is not found, the user is now given a URL to find
    a version that matches the erlang version.

--- lib/mix/tasks/phx.gen.release.ex
+++ lib/mix/tasks/phx.gen.release.ex
@@ -228,7 +228,11 @@ defmodule Mix.Tasks.Phx.Gen.Release do
         ])
 
       :error ->
-        raise "unable to fetch supported Docker image for Elixir #{elixir_vsn} and Erlang #{otp_vsn}"
+        raise """
+          unable to fetch supported Docker image for Elixir #{elixir_vsn} and Erlang #{otp_vsn}.
+          Please check https://hub.docker.com/r/hexpm/elixir/tags?page=1&name=#{otp_vsn}\
+          for a suitable Elixir version
+        """
     end
   end
 
