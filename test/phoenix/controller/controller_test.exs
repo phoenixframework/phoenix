35de2094a222710b92511bb169792b05a33fde70
Tangui Le Pense <29804907+tanguilp@users.noreply.github.com>
2023-12-15 21:55:14 +0300

    Fix flaky test

--- test/phoenix/controller/controller_test.exs
+++ test/phoenix/controller/controller_test.exs
@@ -788,7 +788,7 @@ defmodule Phoenix.Controller.ControllerTest do
 
     test "current_path/2 allows custom nested query params" do
       conn = build_conn_for_path("/")
-      assert current_path(conn, %{foo: %{bar: [:baz], baz: :qux}}) == "/?foo[bar][]=baz&foo[baz]=qux"
+      assert current_path(conn, [foo: [bar: [:baz], baz: :qux]]) == "/?foo[bar][]=baz&foo[baz]=qux"
     end
 
     test "current_url/1 with root path includes trailing slash" do
