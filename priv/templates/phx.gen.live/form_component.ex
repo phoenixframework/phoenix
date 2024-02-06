322d445dec7bcb9f912c56736b0abf52edaab449
Chris McCord <chris@chrismccord.com>
2024-02-06 11:43:04 -0500

    Rework form component

--- priv/templates/phx.gen.live/form_component.ex
+++ priv/templates/phx.gen.live/form_component.ex
@@ -30,18 +30,18 @@ defmodule <%= inspect context.web_module %>.<%= inspect Module.concat(schema.web
 
   @impl true
   def update(%{<%= schema.singular %>: <%= schema.singular %>} = assigns, socket) do
-    changeset = <%= inspect context.alias %>.change_<%= schema.singular %>(<%= schema.singular %>)
-
     {:ok,
      socket
      |> assign(assigns)
-     |> assign_form(changeset, :validate)}
+     |> assign_new(:form, fn ->
+       to_form(<%= inspect context.alias %>.change_<%= schema.singular %>(<%= schema.singular %>))
+     end)}
   end
 
   @impl true
   def handle_event("validate", %{"<%= schema.singular %>" => <%= schema.singular %>_params}, socket) do
     changeset = <%= inspect context.alias %>.change_<%= schema.singular %>(socket.assigns.<%= schema.singular %>, <%= schema.singular %>_params)
-    {:noreply, assign_form(socket, changeset, :validate)}
+    {:noreply, assign(socket, form: to_form(changeset, action: :validate))}
   end
 
   def handle_event("save", %{"<%= schema.singular %>" => <%= schema.singular %>_params}, socket) do
@@ -59,7 +59,7 @@ defmodule <%= inspect context.web_module %>.<%= inspect Module.concat(schema.web
          |> push_patch(to: socket.assigns.patch)}
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign_form(socket, changeset, :save)}
+        {:noreply, assign(socket, form: to_form(changeset))}
     end
   end
 
@@ -74,12 +74,8 @@ defmodule <%= inspect context.web_module %>.<%= inspect Module.concat(schema.web
          |> push_patch(to: socket.assigns.patch)}
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign_form(socket, changeset, :save)}
+        {:noreply, assign(socket, form: to_form(changeset))}
     end
-  end
-
-  defp assign_form(socket, %Ecto.Changeset{} = changeset, action) do
-    assign(socket, :form, to_form(changeset, action: action))
   end
 
   defp notify_parent(msg), do: send(self(), {__MODULE__, msg})
