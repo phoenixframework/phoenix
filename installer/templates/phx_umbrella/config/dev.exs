ee884f81dd83f2ed016d33a6d6601360040f44d8
Steffen Deusch <steffen@deusch.me>
2024-03-11 19:55:52 +0100

    enable runtime checks for new lv projects in dev env (#5746)
    See https://github.com/phoenixframework/phoenix_live_view/pull/3161

--- installer/templates/phx_umbrella/config/dev.exs
+++ installer/templates/phx_umbrella/config/dev.exs
@@ -6,8 +6,11 @@ config :logger, :console, format: "[$level] $message\n"
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime<%= if @html do %>
 
-# Include HEEx debug annotations as HTML comments in rendered markup
-config :phoenix_live_view, :debug_heex_annotations, true<% end %><%= if @mailer do %>
+config :phoenix_live_view,
+  # Include HEEx debug annotations as HTML comments in rendered markup
+  debug_heex_annotations: true,
+  # Enable helpful, but potentially expensive runtime checks
+  enable_expensive_runtime_checks: true<% end %><%= if @mailer do %>
 
 # Disable swoosh api client as it is only required for production adapters.
 config :swoosh, :api_client, false<% end %>
