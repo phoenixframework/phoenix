2c5da88a2642d2249dbba4e6845222a294ba90d3
Juan Barrios <03juan@users.noreply.github.com>
2024-04-02 17:59:40 +0200

    Fix phx.gen.socket moduledoc grammar and example path (#5769)
    * Fix grammar and punctuation
    
    * Fix incorrect example path for umbrella app
    
    ---------
    
    Co-authored-by: Juan P Barrios <juan@ogpss.com>

--- lib/mix/tasks/phx.gen.socket.ex
+++ lib/mix/tasks/phx.gen.socket.ex
@@ -6,7 +6,7 @@ defmodule Mix.Tasks.Phx.Gen.Socket do
 
       $ mix phx.gen.socket User
 
-  Accepts the module name for the socket
+  Accepts the module name for the socket.
 
   The generated files will contain:
 
@@ -18,9 +18,9 @@ defmodule Mix.Tasks.Phx.Gen.Socket do
   For an umbrella application:
 
     * a client in `apps/my_app_web/assets/js`
-    * a socket in `apps/my_app_web/lib/app_name_web/channels`
+    * a socket in `apps/my_app_web/lib/my_app_web/channels`
 
-  You can then generated channels with `mix phx.gen.channel`.
+  You can then generate channels with `mix phx.gen.channel`.
   """
   use Mix.Task
 
