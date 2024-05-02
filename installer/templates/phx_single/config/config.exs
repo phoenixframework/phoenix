70aa952bf3d8b7be1eca742eda7a870037e87f0a
Rodolfo Carvalho <rhcarvalho@gmail.com>
2024-05-02 15:14:01 +0200

    Upgrade Tailwind to 3.4.3 (#5800)

--- installer/templates/phx_single/config/config.exs
+++ installer/templates/phx_single/config/config.exs
@@ -45,7 +45,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "3.4.0",
+  version: "3.4.3",
   <%= @app_name %>: [
     args: ~w(
       --config=tailwind.config.js
--- integration_test/config/config.exs
+++ integration_test/config/config.exs
@@ -4,4 +4,4 @@ config :phoenix, :json_library, Jason
 
 config :swoosh, api_client: false
 
-config :tailwind, :version, "3.4.0"
+config :tailwind, :version, "3.4.3"
