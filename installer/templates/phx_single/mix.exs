ed193386dc604eedae4326dd33be92b491b93c63
Chris McCord <chris@chrismccord.com>
2024-05-03 13:20:34 -0400

    Replace phx-feedback-for in favor of used_input?/1 (#5713)

--- installer/templates/phx_single/mix.exs
+++ installer/templates/phx_single/mix.exs
@@ -37,10 +37,10 @@ defmodule <%= @app_module %>.MixProject do
   defp deps do
     [
       <%= @phoenix_dep %>,<%= if @ecto do %>
-      {:phoenix_ecto, "~> 4.4"},
+      {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
       {<%= inspect @adapter_app %>, ">= 0.0.0"},<% end %><%= if @html do %>
-      {:phoenix_html, "~> 4.0"},
+      {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.20.2"},
       {:floki, ">= 0.30.0", only: :test},<% end %><%= if @dashboard do %>
--- installer/templates/phx_umbrella/apps/app_name_web/mix.exs
+++ installer/templates/phx_umbrella/apps/app_name_web/mix.exs
@@ -37,8 +37,8 @@ defmodule <%= @web_namespace %>.MixProject do
   defp deps do
     [
       <%= @phoenix_dep %>,<%= if @ecto do %>
-      {:phoenix_ecto, "~> 4.4"},<% end %><%= if @html do %>
-      {:phoenix_html, "~> 4.0"},
+      {:phoenix_ecto, "~> 4.5"},<% end %><%= if @html do %>
+      {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.20.2"},
       {:floki, ">= 0.30.0", only: :test},<% end %><%= if @dashboard do %>
