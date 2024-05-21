999fe4e31319cf1757554c4e859ab702e02ed94b
Steffen Deusch <steffen@deusch.me>
2024-05-21 23:55:10 +0200

    do not duplicate integration test code

--- integration_test/docker.sh
+++ integration_test/docker.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh -e
 
-ELIXIR="1.16.2"
-ERLANG="26.2.5"
-SUFFIX="alpine-3.19.1"
+# adapt with versions from .github/versions/ci.yml if necessary;
+# you can also override these with environment variables
+ELIXIR="${ELIXIR:-1.16.2}"
+ERLANG="${ERLANG:-26.2.5}"
+SUFFIX="${SUFFIX:-alpine-3.19.1}"
 
 # Get the directory of the script
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
@@ -25,23 +27,9 @@ fi
 # Start databases
 $COMPOSE_CMD up -d
 
-# Run test commands (adapt from .github/workflows/ci.yml if necessary)
+# Run test script in container
 docker run --rm --network=integration_test_default \
     -w $PARENT_DIR -v $PARENT_DIR:$PARENT_DIR \
-    -it hexpm/elixir:$ELIXIR-erlang-$ERLANG-$SUFFIX sh -c '
-mix local.rebar --force
-mix local.hex --force
-apk add --no-progress --update git socat make gcc libc-dev
-socat TCP-LISTEN:5432,fork TCP-CONNECT:postgres:5432&
-socat TCP-LISTEN:3306,fork TCP-CONNECT:mysql:3306&
-socat TCP-LISTEN:1433,fork TCP-CONNECT:mssql:1433&
-echo "Running installer tests"
-cd installer
-mix test
-echo "Running integration tests"
-cd ../integration_test
-mix deps.get
-mix test --include database
-'
+    -it hexpm/elixir:$ELIXIR-erlang-$ERLANG-$SUFFIX sh integration_test/test.sh
 
 $COMPOSE_CMD down
---
+++ integration_test/test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -e
+
+mix local.rebar --force
+mix local.hex --force
+
+# Install Dependencies
+apk add --no-progress --update git socat make gcc libc-dev
+
+# Set up local proxies
+socat TCP-LISTEN:5432,fork TCP-CONNECT:postgres:5432&
+socat TCP-LISTEN:3306,fork TCP-CONNECT:mysql:3306&
+socat TCP-LISTEN:1433,fork TCP-CONNECT:mssql:1433&
+
+# Run installer tests
+echo "Running installer tests"
+cd installer
+mix test
+
+echo "Running integration tests"
+cd ../integration_test
+mix deps.get
+mix test --include database
