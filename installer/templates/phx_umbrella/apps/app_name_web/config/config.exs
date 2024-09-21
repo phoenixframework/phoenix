22d9b5772d9e11ed5ced49c288e740c6a5e1a729
Andrew Stewart <andrewianstewart@gmail.com>
2024-09-21 12:28:46 -0400

    Update config.exs

--- installer/templates/phx_umbrella/apps/app_name_web/config/config.exs
+++ installer/templates/phx_umbrella/apps/app_name_web/config/config.exs
@@ -22,7 +22,7 @@ config :esbuild,
   version: "0.17.11",
   <%= @web_app_name %>: [
     args:
-      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+      ~w(js/app.js --bundle --target=es2022 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../apps/<%= @web_app_name %>/assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]<% end %><%= if @css do %>
