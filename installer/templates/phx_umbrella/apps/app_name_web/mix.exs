65d2507ccd8619fc0beb0c6339a6c3f6967cdd16
Jonatan Kłosko <jonatanklosko@gmail.com>
2024-10-22 10:29:57 +0200

    Implement mix listener to respect concurrent compilations (#5955)

--- installer/templates/phx_umbrella/apps/app_name_web/mix.exs
+++ installer/templates/phx_umbrella/apps/app_name_web/mix.exs
@@ -13,7 +13,8 @@ defmodule <%= @web_namespace %>.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      listeners: [Phoenix.CodeReloader]
     ]
   end
 
