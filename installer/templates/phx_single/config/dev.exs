f5452d066361c411ecc353e21d400c972a48b620
Steffen Deusch <steffen@deusch.me>
2024-11-26 17:42:28 +0100

    Revert "Enable web console logger for new projects (#5910)"
    This reverts commit f586988de87ef88e482675960427941eb6ae96df.
    
    I didn't see https://github.com/phoenixframework/phoenix/pull/5799;
    will go with that instead!

--- installer/templates/phx_single/config/dev.exs
+++ installer/templates/phx_single/config/dev.exs
@@ -45,7 +45,6 @@ config :<%= @app_name %>, <%= @endpoint_module %>,
 # Watch static and templates for browser reloading.
 config :<%= @app_name %>, <%= @endpoint_module %>,
   live_reload: [
-    web_console_logger: true,
     patterns: [
       ~r"priv/static/(?!uploads/).*(js|css|png|jpeg|jpg|gif|svg)$",<%= if @gettext do %>
       ~r"priv/gettext/.*(po)$",<% end %>
