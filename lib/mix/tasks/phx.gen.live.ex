ac10bf4e3550143ab2c9ca7d1ae18063c58a7a4d
Steffen Deusch <steffen@deusch.me>
2024-12-09 17:46:53 +0100

    backport new interpolation syntax (#6008)

--- lib/mix/tasks/phx.gen.live.ex
+++ lib/mix/tasks/phx.gen.live.ex
@@ -107,6 +107,8 @@ defmodule Mix.Tasks.Phx.Gen.Live do
       )
     end
 
+    Mix.Phoenix.ensure_live_view_compat!(__MODULE__)
+
     {context, schema} = Gen.Context.build(args)
     Gen.Context.prompt_for_code_injection(context)
 
