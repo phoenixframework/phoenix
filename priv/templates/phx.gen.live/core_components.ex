984789fc38bea73a1d9f929544dcb2d63e442c93
Sucipto <sucipto@pm.me>
2025-01-12 12:51:20 +0700

    Update inconsistent core_component select input class

--- priv/templates/phx.gen.live/core_components.ex
+++ priv/templates/phx.gen.live/core_components.ex
@@ -267,7 +267,11 @@ defmodule <%= @web_namespace %>.CoreComponents do
       <select
         id={@id}
         name={@name}
-        class="mt-2 block w-full rounded-md border border-zinc-300 bg-white shadow-sm focus:border-zinc-400 focus:ring-0 sm:text-sm"
+        class={[
+          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
+          @errors == [] && "border-zinc-300 focus:border-zinc-400",
+          @errors != [] && "border-rose-400 focus:border-rose-400"
+        ]}
         multiple={@multiple}
         {@rest}
       >
