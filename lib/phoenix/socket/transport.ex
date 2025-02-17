7a99c9c0ebe1e7a4d31d0c57deced98c68d21d39
Steffen Deusch <steffen@deusch.me>
2025-02-17 18:28:53 +0100

    support :sec_websocket_headers instead of :sec_websocket_protocol
    Supersedes #5904.

--- lib/phoenix/socket/transport.ex
+++ lib/phoenix/socket/transport.ex
@@ -280,7 +280,7 @@ defmodule Phoenix.Socket.Transport do
 
         other ->
           raise ArgumentError,
-                ":connect_info keys are expected to be one of :peer_data, :trace_context_headers, :x_headers, :user_agent, :sec_websocket_protocol, :uri, or {:session, config}, " <>
+                ":connect_info keys are expected to be one of :peer_data, :trace_context_headers, :x_headers, :user_agent, :sec_websocket_headers, :uri, or {:session, config}, " <>
                   "optionally followed by custom keyword pairs, got: #{inspect(other)}"
       end)
 
@@ -470,7 +470,7 @@ defmodule Phoenix.Socket.Transport do
 
     * `:user_agent` - the value of the "user-agent" request header
 
-    * `:sec_websocket_protocol` - the value of the "sec-websocket-protocol" header
+    * `:sec_websocket_headers` - a list of all request headers that have a "sec-websocket-" prefix
 
   The CSRF check can be disabled by setting the `:check_csrf` option to `false`.
   """
@@ -484,7 +484,7 @@ defmodule Phoenix.Socket.Transport do
           {:trace_context_headers, fetch_trace_context_headers(conn)}
 
         :x_headers ->
-          {:x_headers, fetch_x_headers(conn)}
+          {:x_headers, fetch_headers(conn, "x-")}
 
         :uri ->
           {:uri, fetch_uri(conn)}
@@ -492,8 +492,8 @@ defmodule Phoenix.Socket.Transport do
         :user_agent ->
           {:user_agent, fetch_user_agent(conn)}
 
-        :sec_websocket_protocol ->
-          {:sec_websocket_protocol, Plug.Conn.get_req_header(conn, "sec-websocket-protocol")}
+        :sec_websocket_headers ->
+          {:sec_websocket_headers, fetch_headers(conn, "sec-websocket-")}
 
         {:session, session} ->
           {:session, connect_session(conn, endpoint, session, opts)}
@@ -532,9 +532,9 @@ defmodule Phoenix.Socket.Transport do
     end
   end
 
-  defp fetch_x_headers(conn) do
+  defp fetch_headers(conn, prefix) do
     for {header, _} = pair <- conn.req_headers,
-        String.starts_with?(header, "x-"),
+        String.starts_with?(header, prefix),
         do: pair
   end
 
