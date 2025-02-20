ada36b54a35d7373db7afa2c9d8466b8c175bbea
Steffen Deusch <steffen@deusch.me>
2025-02-20 15:06:18 +0100

    fix fixtures indent

--- priv/templates/phx.gen.context/fixtures.ex
+++ priv/templates/phx.gen.context/fixtures.ex
@@ -5,13 +5,13 @@
 <% end %>  @doc """
   Generate a <%= schema.singular %>.
   """
-  def <%= schema.singular %>_fixture(<%= if scope do %>scope, <% end %>attrs \\ %{}) do
-    <%= if scope do %>attrs = Enum.into(attrs, %{
-      <%= schema.fixture_params |> Enum.map(fn {key, code} -> "        #{key}: #{code}" end) |> Enum.join(",\n") %>
-            })
+  def <%= schema.singular %>_fixture(<%= if scope do %>scope, <% end %>attrs \\ %{}) do<%= if scope do %>
+    attrs =
+      Enum.into(attrs, %{
+<%= schema.fixture_params |> Enum.map(fn {key, code} -> "        #{key}: #{code}" end) |> Enum.join(",\n") %>
+      })
 
-    {:ok, <%= schema.singular %>} = <%= inspect context.module %>.create_<%= schema.singular %>(scope, attrs)
-    <% else %>{:ok, <%= schema.singular %>} =
+    {:ok, <%= schema.singular %>} = <%= inspect context.module %>.create_<%= schema.singular %>(scope, attrs)<% else %>{:ok, <%= schema.singular %>} =
       attrs
       |> Enum.into(%{
 <%= schema.fixture_params |> Enum.map(fn {key, code} -> "        #{key}: #{code}" end) |> Enum.join(",\n") %>
