563ccc56bbf165ca8c6860333c4ea179d0617eae
José Valim <jose.valim@dashbit.co>
2025-04-07 01:32:21 +0200

    Fixes for umbrella, closes #6165

--- installer/templates/phx_assets/heroicons.js
+++ installer/templates/phx_assets/heroicons.js
@@ -3,7 +3,7 @@ const fs = require("fs")
 const path = require("path")
 
 module.exports = plugin(function({matchComponents, theme}) {
-  let iconsDir = path.join(__dirname, "..<%= if @in_umbrella, do: "/.." %>/../deps/heroicons/optimized")
+  let iconsDir = path.join(__dirname, "..<%= if @in_umbrella, do: "/../.." %>/../deps/heroicons/optimized")
   let values = {}
   let icons = [
     ["", "/24/outline"],
