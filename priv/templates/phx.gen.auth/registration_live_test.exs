5a2ea96e655db0531b9f93a71c5423833ad924f4
Steffen Deusch <steffen@deusch.me>
2025-04-09 17:16:03 +0200

    don't rely on floki specific selectors (#6179)

--- priv/templates/phx.gen.auth/registration_live_test.exs
+++ priv/templates/phx.gen.auth/registration_live_test.exs
@@ -72,7 +72,7 @@ defmodule <%= inspect context.web_module %>.<%= inspect Module.concat(schema.web
 
       {:ok, _login_live, login_html} =
         lv
-        |> element(~s|main a:fl-contains("Log in")|)
+        |> element("main a", "Log in")
         |> render_click()
         |> follow_redirect(conn, ~p"<%= schema.route_prefix %>/log-in")
 
