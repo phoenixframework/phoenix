ea572c1335d2ec3c5b8e589918f96ecbf61a33c2
Benjamin Milde <benni@kobrakai.de>
2025-04-23 11:42:07 +0200

    Update installer/templates/phx_web/components/layouts.ex
    Co-authored-by: Steffen Deusch <steffen@deusch.me>

--- installer/templates/phx_web/components/layouts.ex
+++ installer/templates/phx_web/components/layouts.ex
@@ -22,6 +22,7 @@ defmodule <%= @web_namespace %>.Layouts do
       
   """
   attr :flash, :map, required: true, doc: "the map of flash messages"
+  attr :current_scope, :map, default: nil, doc: "the current [scope](https://hexdocs.pm/phoenix/scopes.html)"
 
   slot :inner_block, required: true
 
