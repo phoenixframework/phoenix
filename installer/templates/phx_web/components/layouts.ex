1d52cdf2890de6bc75908f0f1b00133de4d5868e
Philipp A. <phips4.yt@gmail.com>
2025-04-23 14:38:27 +0200

    Add w-fit to logo to limit is clickable area (#6213)

--- installer/templates/phx_web/components/layouts.ex
+++ installer/templates/phx_web/components/layouts.ex
@@ -33,7 +33,7 @@ defmodule <%= @web_namespace %>.Layouts do
     ~H"""
     <header class="navbar px-4 sm:px-6 lg:px-8">
       <div class="flex-1">
-        <a href="/" class="flex-1 flex items-center gap-2">
+        <a href="/" class="flex-1 flex w-fit items-center gap-2">
           <img src={~p"/images/logo.svg"} width="36" />
           <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
         </a>
