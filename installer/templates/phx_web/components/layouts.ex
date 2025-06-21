ab2b63af7adcabbf22b8de8db5cceee5c32af173
José Valim <jose.valim@dashbit.co>
2025-06-20 18:25:23 -0700

    Improve layouts example

--- installer/templates/phx_web/components/layouts.ex
+++ installer/templates/phx_web/components/layouts.ex
@@ -1,18 +1,22 @@
 defmodule <%= @web_namespace %>.Layouts do
   @moduledoc """
-  This module holds different layouts used by your application.
-
-  See the `layouts` directory for all templates available.
-  The "root" layout is a skeleton rendered as part of the
-  application router. The "app" layout is rendered as component
-  in regular views and live views.
+  This module holds layouts and related functionality
+  used by your application.
   """
   use <%= @web_namespace %>, :html
 
+  # Embed all files in layouts/* within this module.
+  # The default root.html.heex file contains the HTML
+  # skeleton of your application, namely HTML headers
+  # and other static content.
   embed_templates "layouts/*"
 
   @doc """
-  Renders the app layout
+  Renders your app layout.
+
+  This function is typically invoked from every template,
+  and it often contains your application menu, sidebar,
+  or similar.
 
   ## Examples
 
