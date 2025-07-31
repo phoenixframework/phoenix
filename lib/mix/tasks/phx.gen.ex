54e7b17b83325ce3e654de2e2d9ea30ec5f6bf15
Obie <20772607+obieewan@users.noreply.github.com>
2025-08-01 03:19:45 +0800

    Add guide for live_view generator customization (#6387)
    * Add example and warning on overriding phx.gen templates in docs
    
    Co-authored-by: José Valim <jose.valim@gmail.com>
    Co-authored-by: Steffen Deusch <steffen@deusch.me>

--- lib/mix/tasks/phx.gen.ex
+++ lib/mix/tasks/phx.gen.ex
@@ -18,6 +18,32 @@ defmodule Mix.Tasks.Phx.Gen do
   | `phx.gen.live`     | x | x | x |   |   | x |
   | `phx.gen.json`     | x | x | x | x | x |   |
   | `phx.gen.html`     | x | x | x | x | x |   |
+
+  ## Customizing generators
+  
+  You can override the default templates used by generators.
+  
+  For example, to customize `phx.gen.live`, you can copy and edit the generator templates
+  to your own project's priv folder:
+  
+  First, create the directory for your custom `phx.gen.live` templates:
+
+  ```console
+  $ mkdir -p priv/templates/phx.gen.live
+  ```
+
+  Next, copy the default phx.gen.live generator templates into your project so you can customize them:
+
+  ```console
+  $ cp -r deps/phoenix/priv/templates/phx.gen.live/* priv/templates/phx.gen.live/
+  ```
+
+  Phoenix generators will look for templates in your project's `priv/templates` directory first.
+  If a matching template is found, it will be used instead of the default.
+
+  Note generator templates may change between minor or even patch Phoenix releases,
+  so custom templates may require updates after upgrading. Use this mechanism at your
+  own risk.
   """
 
   def run(_args) do
