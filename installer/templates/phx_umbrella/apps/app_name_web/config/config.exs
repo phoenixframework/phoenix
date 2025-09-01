97cbe097f2ae7d1655a7c260ff3c5be7309d0aa1
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2025-09-01 12:52:42 +0330

    Update tailwind version to fix creating empty utility class
    In Tailwind 4.1.7 when you use some class like `space-y-10` , you can see in your browser it is created like
    
    ```css
    .space-y-10 {}
    ```
    After updating to last version it fixed!

--- installer/templates/phx_umbrella/apps/app_name_web/config/config.exs
+++ installer/templates/phx_umbrella/apps/app_name_web/config/config.exs
@@ -29,7 +29,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "4.1.7",
+  version: "4.1.12",
   <%= @web_app_name %>: [
     args: ~w(
       --input=assets/css/app.css
--- integration_test/config/config.exs
+++ integration_test/config/config.exs
@@ -4,6 +4,6 @@ config :phoenix, :json_library, Jason
 
 config :swoosh, api_client: false
 
-config :tailwind, :version, "4.1.7"
+config :tailwind, :version, "4.1.12"
 
 config :phoenix_live_view, enable_expensive_runtime_checks: true
