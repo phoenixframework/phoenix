f1f2b03f7cb59881151ccf65f467aea005202310
Steffen Deusch <steffen@deusch.me>
2025-09-18 17:04:09 +0200

    update dependencies

--- installer/templates/phx_single/mix.exs
+++ installer/templates/phx_single/mix.exs
@@ -66,7 +66,7 @@ defmodule <%= @app_module %>.MixProject do
       {:req, "~> 0.5"},<% end %>
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},<%= if @gettext do %>
-      {:gettext, "~> 0.26"},<% end %>
+      {:gettext, "~> 1.0"},<% end %>
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {<%= inspect @web_adapter_app %>, "<%= @web_adapter_vsn %>"}
--- integration_test/mix.exs
+++ integration_test/mix.exs
@@ -48,7 +48,7 @@ defmodule Phoenix.Integration.MixProject do
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.26"},
+      {:gettext, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:swoosh, "~> 1.16"},
       {:bandit, "~> 1.0"},
