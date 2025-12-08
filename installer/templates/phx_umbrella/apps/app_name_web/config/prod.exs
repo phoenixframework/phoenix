8b80f26b06376666d9a652d5c0bcd6fb77a7812f
José Valim <jose.valim@dashbit.co>
2025-12-08 14:23:25 +0100

    Add documentation to exclude paths (#6556)

--- installer/templates/phx_umbrella/apps/app_name_web/config/prod.exs
+++ installer/templates/phx_umbrella/apps/app_name_web/config/prod.exs
@@ -10,5 +10,11 @@ config :<%= @web_app_name %>, <%= @endpoint_module %>,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Force using SSL in production. This also sets the "strict-security-transport" header,
-# also known as HSTS. `:force_ssl` is required to be set at compile-time.
-config :<%= @web_app_name %>, <%= @endpoint_module %>, force_ssl: [rewrite_on: [:x_forwarded_proto]]
+# known as HSTS. If you have a health check endpoint, you may want to exclude it below.
+# Note `:force_ssl` is required to be set at compile-time.
+config :<%= @web_app_name %>, <%= @endpoint_module %>,
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  exclude: [
+    # paths: ["/health"],
+    hosts: ["localhost", "127.0.0.1"]
+  ]
