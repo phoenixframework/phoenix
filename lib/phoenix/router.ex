0671fd94682de3eb3fea59097763141c88daf805
t3chw <mohit.kr.connect@gmail.com>
2026-02-08 19:44:08 +0530

    Add host constraints to mix phx.routes output

--- lib/phoenix/router.ex
+++ lib/phoenix/router.ex
@@ -539,7 +539,7 @@ defmodule Phoenix.Router do
         quote line: line, do: _ = &(unquote(module).unquote(function) / unquote(arity))
       end)
 
-    keys = [:verb, :path, :plug, :plug_opts, :helper, :metadata]
+    keys = [:verb, :path, :plug, :plug_opts, :helper, :metadata, :hosts]
     routes = Enum.map(routes, &Map.take(&1, keys))
 
     quote do
@@ -1286,7 +1286,9 @@ defmodule Phoenix.Router do
               verb: nested_route.verb
           }
 
-          Map.put(route, :label, nested_route.label)
+          route
+          |> Map.put(:label, nested_route.label)
+          |> Map.put(:hosts, Map.get(nested_route, :hosts, route.hosts))
         end)
       else
         plug =
@@ -1302,6 +1304,7 @@ defmodule Phoenix.Router do
             helper: route.helper,
             verb: route.verb,
             path: route.path,
+            hosts: route.hosts,
             label: label
           }
         ]
