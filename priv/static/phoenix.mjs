97d48c9408b2cf2a7b1cbbf452da807dae58d062
chrismccord <576796+chrismccord@users.noreply.github.com>
2026-02-23 17:00:53 +0000

    Update assets

--- priv/static/phoenix.mjs
+++ priv/static/phoenix.mjs
@@ -224,14 +224,12 @@ var Channel = class {
     });
     this.onClose(() => {
       this.rejoinTimer.reset();
-      if (this.socket.hasLogger())
-        this.socket.log("channel", `close ${this.topic} ${this.joinRef()}`);
+      if (this.socket.hasLogger()) this.socket.log("channel", `close ${this.topic} ${this.joinRef()}`);
       this.state = CHANNEL_STATES.closed;
       this.socket.remove(this);
     });
     this.onError((reason) => {
-      if (this.socket.hasLogger())
-        this.socket.log("channel", `error ${this.topic}`, reason);
+      if (this.socket.hasLogger()) this.socket.log("channel", `error ${this.topic}`, reason);
       if (this.isJoining()) {
         this.joinPush.reset();
       }
@@ -241,8 +239,7 @@ var Channel = class {
       }
     });
     this.joinPush.receive("timeout", () => {
-      if (this.socket.hasLogger())
-        this.socket.log("channel", `timeout ${this.topic} (${this.joinRef()})`, this.joinPush.timeout);
+      if (this.socket.hasLogger()) this.socket.log("channel", `timeout ${this.topic} (${this.joinRef()})`, this.joinPush.timeout);
       let leavePush = new Push(this, CHANNEL_EVENTS.leave, closure({}), this.timeout);
       leavePush.send();
       this.state = CHANNEL_STATES.errored;
@@ -388,8 +385,7 @@ var Channel = class {
     this.joinPush.cancelTimeout();
     this.state = CHANNEL_STATES.leaving;
     let onClose = () => {
-      if (this.socket.hasLogger())
-        this.socket.log("channel", `leave ${this.topic}`);
+      if (this.socket.hasLogger()) this.socket.log("channel", `leave ${this.topic}`);
       this.trigger(CHANNEL_EVENTS.close, "leave");
     };
     let leavePush = new Push(this, CHANNEL_EVENTS.leave, closure({}), timeout);
@@ -423,8 +419,7 @@ var Channel = class {
       return false;
     }
     if (joinRef && joinRef !== this.joinRef()) {
-      if (this.socket.hasLogger())
-        this.socket.log("channel", "dropping outdated message", { topic, event, payload, joinRef });
+      if (this.socket.hasLogger()) this.socket.log("channel", "dropping outdated message", { topic, event, payload, joinRef });
       return false;
     } else {
       return true;
@@ -767,7 +762,7 @@ var LongPoll = class {
 };
 
 // js/phoenix/presence.js
-var Presence = class {
+var Presence = class _Presence {
   constructor(channel, opts = {}) {
     let events = opts.events || { state: "presence_state", diff: "presence_diff" };
     this.state = {};
@@ -785,9 +780,9 @@ var Presence = class {
     this.channel.on(events.state, (newState) => {
       let { onJoin, onLeave, onSync } = this.caller;
       this.joinRef = this.channel.joinRef();
-      this.state = Presence.syncState(this.state, newState, onJoin, onLeave);
+      this.state = _Presence.syncState(this.state, newState, onJoin, onLeave);
       this.pendingDiffs.forEach((diff) => {
-        this.state = Presence.syncDiff(this.state, diff, onJoin, onLeave);
+        this.state = _Presence.syncDiff(this.state, diff, onJoin, onLeave);
       });
       this.pendingDiffs = [];
       onSync();
@@ -797,7 +792,7 @@ var Presence = class {
       if (this.inPendingSyncState()) {
         this.pendingDiffs.push(diff);
       } else {
-        this.state = Presence.syncDiff(this.state, diff, onJoin, onLeave);
+        this.state = _Presence.syncDiff(this.state, diff, onJoin, onLeave);
         onSync();
       }
     });
@@ -812,7 +807,7 @@ var Presence = class {
     this.caller.onSync = callback;
   }
   list(by) {
-    return Presence.list(this.state, by);
+    return _Presence.list(this.state, by);
   }
   inPendingSyncState() {
     return !this.joinRef || this.joinRef !== this.channel.joinRef();
@@ -1376,8 +1371,7 @@ var Socket = class {
     clearTimeout(this.heartbeatTimeoutTimer);
   }
   onConnOpen() {
-    if (this.hasLogger())
-      this.log("transport", `${this.transportName(this.transport)} connected to ${this.endPointURL()}`);
+    if (this.hasLogger()) this.log("transport", `${this.transportName(this.transport)} connected to ${this.endPointURL()}`);
     this.closeWasClean = false;
     this.disconnecting = false;
     this.establishedConnections++;
@@ -1454,12 +1448,10 @@ var Socket = class {
     }, 150 * tries);
   }
   onConnClose(event) {
-    if (this.conn)
-      this.conn.onclose = () => {
-      };
+    if (this.conn) this.conn.onclose = () => {
+    };
     let closeCode = event && event.code;
-    if (this.hasLogger())
-      this.log("transport", "close", event);
+    if (this.hasLogger()) this.log("transport", "close", event);
     this.triggerChanError();
     this.clearHeartbeats();
     if (!this.closeWasClean && closeCode !== 1e3) {
@@ -1471,8 +1463,7 @@ var Socket = class {
    * @private
    */
   onConnError(error) {
-    if (this.hasLogger())
-      this.log("transport", error);
+    if (this.hasLogger()) this.log("transport", error);
     let transportBefore = this.transport;
     let establishedBefore = this.establishedConnections;
     this.stateChangeCallbacks.error.forEach(([, callback]) => {
@@ -1596,8 +1587,7 @@ var Socket = class {
         this.pendingHeartbeatRef = null;
         this.heartbeatTimer = setTimeout(() => this.sendHeartbeat(), this.heartbeatIntervalMs);
       }
-      if (this.hasLogger())
-        this.log("receive", `${payload.status || ""} ${topic} ${event} ${ref && "(" + ref + ")" || ""}`, payload);
+      if (this.hasLogger()) this.log("receive", `${payload.status || ""} ${topic} ${event} ${ref && "(" + ref + ")" || ""}`, payload);
       for (let i = 0; i < this.channels.length; i++) {
         const channel = this.channels[i];
         if (!channel.isMember(topic, event, payload, join_ref)) {
@@ -1614,8 +1604,7 @@ var Socket = class {
   leaveOpenTopic(topic) {
     let dupChannel = this.channels.find((c) => c.topic === topic && (c.isJoined() || c.isJoining()));
     if (dupChannel) {
-      if (this.hasLogger())
-        this.log("transport", `leaving duplicate topic "${topic}"`);
+      if (this.hasLogger()) this.log("transport", `leaving duplicate topic "${topic}"`);
       dupChannel.leave();
     }
   }
