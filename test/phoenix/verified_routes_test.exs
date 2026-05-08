c06dc4c2dfd9ba44c69b36ac9560e9c07b8b7c5e
José Valim <jose.valim@gmail.com>
2026-05-08 12:12:57 +0200

    Apply suggestion from @josevalim

--- test/phoenix/verified_routes_test.exs
+++ test/phoenix/verified_routes_test.exs
@@ -306,14 +306,11 @@ defmodule Phoenix.VerifiedRoutesTest do
     assert_raise ArgumentError,
                  ~r|attempted to use Phoenix.VerifiedRoutes without calling|,
                  fn ->
-                   defmodule ImportedVerifiedRoutes do
+                   defmodule BadImportedVerifiedRoutes do
                      import Phoenix.VerifiedRoutes
                      def test, do: ~p"/posts/1"
                    end
                  end
-  after
-    :code.purge(__MODULE__.ImportedVerifiedRoutes)
-    :code.delete(__MODULE__.ImportedVerifiedRoutes)
   end
 
   test "path arities" do
